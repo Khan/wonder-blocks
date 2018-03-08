5fef5476ee164dd65ade03287502048cb95a3e78
Kevin Barabash <kevinb7@gmail.com>
2018-03-08 19:21:35 +0100

    Cleanup repo and enable travis-ci (#1)
    * Cleanup repo and enable travis-ci
    
    Summary:
    The cleanup involved:
    - removing duplicate files
    - preferring .test.js and .stories.js to the _ versions
    - fixing regex in .storybook/config.js so that we were only loading stories in packages/wonder-blocks-*
    - add explicit "dependencies" in each sub package's package.json to point to relative directories
    - add script to run "npm install" in each sub package
    
    Test Plan: npm test
    
    Reviewers: miguel idr john
    
    * add coverage reporting
    
    * fix dependencies in wonder-blocks-button

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,7 @@
 import { configure } from '@storybook/react';
 
 function loadStories() {
-    const req = require.context('../', true, /\_stories\.js$/);
+    const req = require.context('../packages', true, /\.stories\.js$/);
     req.keys().forEach(filename => req(filename));
 }
 
