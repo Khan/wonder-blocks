4907b6ee101758aeed25aafa6c4803e04952ecea
Alex Lopatin <alex@khanacademy.org>
2018-03-20 13:41:36 -0700

    Disable subpixel antialiasing for all text
    To verify the impact of setting this property, I added "light on dark"
    rendering of each typography component to the storybook. I also set the
    default text color to off-black (our primary text color) because it's
    hard to tell the difference between that and the browser default
    (pure black) at a glance.
    
    Test Plan:
    npm test
    
    Reviewers: john

--- packages/wonder-blocks-core/src/text.js
+++ packages/wonder-blocks-core/src/text.js
@@ -1,5 +1,7 @@
 // @flow
 import React, {Component} from "react";
+import {StyleSheet} from "aphrodite";
+import Color from "wonder-blocks-color";
 
 import {processStyleList} from "./util.js";
 
@@ -11,6 +13,27 @@ type Props = {
 
 const isHeaderRegex = /^h[1-6]$/;
 
+const styles = StyleSheet.create({
+    text: {
+        // Our primary text color, off-black, looks a lot like pure black.
+        // We set it to be the default text color so that we don't ever
+        // have to worry about trying to visually distinguish the two.
+        color: Color.offBlack,
+
+        // Disable subpixel antialiasing on Mac desktop for consistency of
+        // rendering with mobile and Sketch (neither of which support it).
+        // See https://bjango.com/articles/subpixeltext/ for more details.
+        WebkitFontSmoothing: "antialiased",
+        MozOsxFontSmoothing: "grayscale",
+    },
+    header: {
+        // User agent stylesheets add vertical margins to header tags by
+        // default. We prefer to be more deliberate in our spacing instead.
+        marginTop: 0,
+        marginBottom: 0,
+    },
+});
+
 export default class Text extends Component {
     props: Props;
 
@@ -21,9 +44,8 @@ export default class Text extends Component {
     render() {
         const isHeader = isHeaderRegex.test(this.props.tag);
         const {className, style} = processStyleList([
-            // User agent stylesheets add vertical margins to header tags by
-            // default. We prefer to be more deliberate in our spacing instead.
-            isHeader && {marginTop: 0, marginBottom: 0},
+            styles.text,
+            isHeader && styles.header,
             this.props.style,
         ]);
 
