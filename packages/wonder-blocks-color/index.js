e51f19ac33defe2147064ed42d33ab27a0b491de
Brian Genisio <BrianGenisio@Gmail.com>
2018-03-21 15:42:38 -0400

    Updating the secondary color to the accessible color

--- packages/wonder-blocks-color/index.js
+++ packages/wonder-blocks-color/index.js
@@ -8,7 +8,7 @@ const Color = {
 
     // Neutral
     offBlack: "#21242C",
-    offBlackSecondary: "rgba(33, 36, 44, 0.5)",
+    offBlackSecondary: "rgba(33, 36, 44, 0.64)",
     offBlackBorder: "rgba(33, 36, 44, 0.1)",
     offWhite: "#F7F8FA",
     white: "#FFFFFF",
