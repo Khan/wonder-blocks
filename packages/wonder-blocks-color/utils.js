2b28c50cfa39930bc60cd77b33f7ee07fe0a5728
John Resig <jeresig@gmail.com>
2018-04-11 15:54:26 -0400

    Add color mixing utility to wonder-blocks-color.
    This is a new utility, based on our existing one in webapp, that allows us to mix colors easily. It introduces two new public functions: `fade` and `mix` which you can get from the `wonder-blocks-color` package.
    
    I made a couple changes from the version in Webapp:
    
    1) I no longer export `format` and `parse` and instead have all the functions take color strings and return color strings. While this does incur additional overhead sometimes it drastically reduces the complexity of using these functions.
    2) If a color has no opacity I return the color as a hex string (which more closely matches the Wonder Blocks specs).
    3) I changed the logic in `format` from `Math.floor` to `Math.round`, which seems generally more accurate to me? The colors were almost all wrong when using `Math.floor`.
    4) I no longer divide all the numbers by 255 and then multiple by 255 again upon formatting. This was just busy-work that we didn't need anymore.
    5) I added in support for parsing `rgb(...)` and `rgba(...)` color strings.
    
    I additionally changed the formatting of the Color storybook to handle wider color names and added a border around the color to make it more visible.
    
    Finally, I changed the exported colors to be all lowercase to match what we see in Zeplin, making comparisons easier.
    
    {F1149958}
    
    Test Plan:
    I compared the generated colors against the Button v1 spec and, for the most part, they matched. There were a few exceptions however:
    
    ```
    White (XTinted): #f2bab4 -> #f3bbb4
    Tint (Dark): #9e271c -> #9e271d
    Tint (Dark) on Tint: #c62717 -> #c62818
    Tint (Dark) on White: #eedad8 -> #efdddb
    ```
    
    I'm not entirely sure what might be going wrong here. Possibly something having to do with the rounding? Or maybe the color blending in Sketch is a bit smarter? Either way I'd love to know if you can spot an error with the logic that I'm not seeing.

---
+++ packages/wonder-blocks-color/utils.js
@@ -0,0 +1,79 @@
+// @flow
+/**
+ * A color manipulation library useful for dynamically
+ * mixing colors together.
+ */
+
+const colorRegexp = /^#([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})$/i;
+const rgbaRegexp = /^rgba?\(\s*(\d+),\s*(\d+),\s*(\d+)(?:,\s*([\d.]+))?\s*\)$/i;
+
+type Color = {
+    r: number,
+    g: number,
+    b: number,
+    a: number,
+};
+
+// Parse a color in #abcdef, rgb(...), or rgba(...) form into an object
+// with r,g,b,a keys.
+const parse = (color: string) => {
+    if (typeof color !== "string") {
+        throw new Error(`Failed to parse color: ${color}`);
+    }
+
+    const match = color.match(colorRegexp);
+    if (match) {
+        return {
+            r: parseInt(match[1], 16),
+            g: parseInt(match[2], 16),
+            b: parseInt(match[3], 16),
+            a: 1,
+        };
+    } else {
+        const rgbaMatch = color.match(rgbaRegexp);
+
+        if (rgbaMatch) {
+            return {
+                r: parseFloat(rgbaMatch[1]),
+                g: parseFloat(rgbaMatch[2]),
+                b: parseFloat(rgbaMatch[3]),
+                a: rgbaMatch[4] ? parseFloat(rgbaMatch[4]) : 1,
+            };
+        }
+
+        throw new Error(`Failed to parse color: ${color}`);
+    }
+};
+
+// Stringify the color in an `rgba()` or `#abcdef` format.
+const format = (color: Color) => {
+    const r = Math.round(color.r);
+    const g = Math.round(color.g);
+    const b = Math.round(color.b);
+
+    return color.a === 1
+        ? `#${r.toString(16)}${g.toString(16)}${b.toString(16)}`
+        : `rgba(${r},${g},${b},${color.a.toFixed(2)})`;
+};
+
+// Set the alpha value of a color.
+export const fade = (color: string, percentage: number) => {
+    return format({
+        ...parse(color),
+        a: percentage,
+    });
+};
+
+// Mix a color into a background color, using the alpha channel of the base
+// color to determine the linear blend.
+export const mix = (color: string, background: string) => {
+    const colorObj = parse(color);
+    const bgObj = parse(background);
+
+    return format({
+        r: colorObj.r * colorObj.a + bgObj.r * (1 - colorObj.a),
+        g: colorObj.g * colorObj.a + bgObj.g * (1 - colorObj.a),
+        b: colorObj.b * colorObj.a + bgObj.b * (1 - colorObj.a),
+        a: 1,
+    });
+};
