e33d7a16d73b880461bdedb5bd1bdacb1c28157f
Kevin Barabash <kevinbarabash@khanacademy.org>
2018-04-17 19:02:43 -0400

    initial implementation of snapshot testing w/ styleguidist

--- story-shots.test.js
+++
@@ -1,6 +0,0 @@
-const {StyleSheetTestUtils} = require("aphrodite");
-const initStoryshots = require("@storybook/addon-storyshots").default;
-
-StyleSheetTestUtils.suppressStyleInjection();
-
-initStoryshots();
