aac35be3298a61d2fafabb7cc9ae51403403125d
Matt Dunn-Rankin <mattdunnrankin@khanacademy.org>
2018-04-23 16:00:15 -0700

    [modal] Create wonder-blocks-modal package
    In this change, we create the bare-bones `wonder-blocks-modal` package, to set us up for upcoming development work. Hooray!
    
    https://khanacademy.atlassian.net/browse/WB-140
    
    Test Plan:
    Styleguidist now has a section for us!
    https://phabricator-files.khanacademy.org/file/data/lnhi5ovyyikxcwtm6mlo/PHID-FILE-77i2xxe5he3kgrln6ybl/image.png

---
+++ packages/wonder-blocks-modal/components/two-column-modal.js
@@ -0,0 +1,12 @@
+// @flow
+import React from "react";
+import {StyleSheet, css} from "aphrodite";
+
+export default class TwoColumnModal extends React.Component<{}> {
+    render() {
+        return <div>
+            Hello, world!
+            TODO(mdr): Build TwoColumnModal.
+        </div>;
+    }
+}
---
+++ packages/wonder-blocks-modal/docs.md
@@ -0,0 +1,7 @@
+Modals! TODO(mdr): Build modals and write docs.
+
+```js
+const TwoColumnModal = require("./components/two-column-modal.js").default;
+
+<TwoColumnModal />
+```
