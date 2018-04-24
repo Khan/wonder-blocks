dd24963e2a5bcb5ec61b6c9b887f138609d765a5
Matt Dunn-Rankin <mattdunnrankin@khanacademy.org>
2018-04-24 14:03:50 -0700

    [modal] Add two-column layout
    In this change, we create the basic `TwoColumnLayout` component.
    
    This isn't actually a launchable modal yet! This is the standalone component that will someday be launchable, but will also be embeddable directly in a page when appropriate.
    
    Test Plan:
    http://localhost:6060/#!/Modal
    ![Styleguidist screenshot](https://phabricator-files.khanacademy.org/file/data/ekocx2qneruqpjoypfzu/PHID-FILE-3yofa7ujyut2jnun4gxa/image.png)

--- packages/wonder-blocks-modal/components/two-column-modal.js
+++ packages/wonder-blocks-modal/components/two-column-modal.js
@@ -1,12 +1,59 @@
 // @flow
-import React from "react";
+/**
+ * A two-column modal layout.
+ */
+import * as React from "react";
 import {StyleSheet, css} from "aphrodite";
 
-export default class TwoColumnModal extends React.Component<{}> {
+import Color from "wonder-blocks-color";
+
+type Props = {
+    leftContent: React.Node,
+    rightContent: React.Node,
+};
+
+export default class TwoColumnModal extends React.Component<Props> {
     render() {
-        return <div>
-            Hello, world!
-            TODO(mdr): Build TwoColumnModal.
-        </div>;
+        return (
+            <div className={css(styles.container)}>
+                <div className={css(styles.column, styles.leftColumn)}>
+                    {this.props.leftContent}
+                </div>
+                <div className={css(styles.column, styles.rightColumn)}>
+                    {this.props.rightContent}
+                </div>
+            </div>
+        );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "stretch",
+
+        // This minimum height is a fixed number from the spec.
+        minHeight: 464,
+    },
+
+    column: {
+        boxSizing: "border-box",
+        flex: "0 0 50%",
+        paddingTop: 64,
+        paddingBottom: 64,
+    },
+
+    leftColumn: {
+        background: Color.darkBlue,
+        color: Color.white,
+        paddingLeft: 64,
+        paddingRight: 52,
+    },
+
+    rightColumn: {
+        background: Color.white,
+        paddingLeft: 52,
+        paddingRight: 64,
+    },
+});
--- packages/wonder-blocks-modal/generated-snapshot.test.js
+++ packages/wonder-blocks-modal/generated-snapshot.test.js
@@ -7,9 +7,62 @@ import TwoColumnModal from "./components/two-column-modal.js";
 
 describe("wonder-blocks-modal", () => {
     it("example 1", () => {
-        const TwoColumnModal = require("./components/two-column-modal.js").default;
-        
-        const example = <TwoColumnModal />
+        const {StyleSheet, css} = require("aphrodite");
+        const {Title, Body} = require("wonder-blocks-typography");
+        const TwoColumnModal = require("./components/two-column-modal.js")
+            .default;
+
+        const styles = StyleSheet.create({
+            example: {
+                // Checkerboard background
+                backgroundImage:
+                    "linear-gradient(45deg, #ccc 25%, transparent 25%), linear-gradient(-45deg, #ccc 25%, transparent 25%), linear-gradient(45deg, transparent 75%, #ccc 75%), linear-gradient(-45deg, transparent 75%, #ccc 75%)",
+                backgroundSize: "20px 20px",
+                backgroundPosition: "0 0, 0 10px, 10px -10px, -10px 0px",
+
+                padding: 32,
+            },
+
+            title: {
+                marginBottom: 16,
+            },
+        });
+
+        const example = (
+            <div className={css(styles.example)}>
+                <TwoColumnModal
+                    leftContent={
+                        <div>
+                            <Title style={styles.title}>Left column</Title>
+                            <Body>
+                                Lorem ipsum dolor sit amet, consectetur
+                                adipiscing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore magna aliqua. Ut
+                                enim ad minim veniam, quis nostrud exercitation
+                                ullamco laboris.
+                            </Body>
+                        </div>
+                    }
+                    rightContent={
+                        <div>
+                            <Title style={styles.title}>Right column</Title>
+                            <Body>
+                                Lorem ipsum dolor sit amet, consectetur
+                                adipiscing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore magna aliqua. Ut
+                                enim ad minim veniam, quis nostrud exercitation
+                                ullamco laboris nisi ut aliquip ex ea commodo
+                                consequat. Duis aute irure dolor in
+                                reprehenderit in voluptate velit esse cillum
+                                dolore eu fugiat nulla pariatur. Excepteur sint
+                                occaecat cupidatat non proident, sunt in culpa
+                                qui officia deserunt mollit anim id est.
+                            </Body>
+                        </div>
+                    }
+                />
+            </div>
+        );
         const tree = renderer.create(example).toJSON();
         expect(tree).toMatchSnapshot();
     });
