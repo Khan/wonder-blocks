99b428d0f04f9fb99b50dab805a2200e3ba17c5a
Matt Dunn-Rankin <mdunnrankin@gmail.com>
2018-04-25 15:22:47 -0700

    [modal] Add two-column layout (#50)
    In this change, we create the basic `TwoColumnLayout` component.
    
    This isn't actually a launchable modal yet! This is the standalone component that will someday be launchable, but will also be embeddable directly in a page when appropriate.
    
    Test Plan:
    http://localhost:6060/#!/Modal
    ![Styleguidist screenshot](https://phabricator-files.khanacademy.org/file/data/ekocx2qneruqpjoypfzu/PHID-FILE-3yofa7ujyut2jnun4gxa/image.png)

--- packages/wonder-blocks-modal/components/two-column-modal.js
+++ packages/wonder-blocks-modal/components/two-column-modal.js
@@ -1,12 +1,66 @@
 // @flow
-import React from "react";
+/**
+ * A two-column modal layout.
+ */
+import * as React from "react";
 import {StyleSheet, css} from "aphrodite";
 
-export default class TwoColumnModal extends React.Component<{}> {
+import Color from "wonder-blocks-color";
+import {View} from "wonder-blocks-core";
+
+type Props = {
+    leftContent: React.Node,
+    rightContent: React.Node,
+};
+
+export default class TwoColumnModal extends React.Component<Props> {
     render() {
-        return <div>
-            Hello, world!
-            TODO(mdr): Build TwoColumnModal.
-        </div>;
+        return (
+            <View style={styles.container}>
+                <View style={[styles.column, styles.leftColumn]}>
+                    {this.props.leftContent}
+                </View>
+                <View style={[styles.column, styles.rightColumn]}>
+                    {this.props.rightContent}
+                </View>
+            </View>
+        );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "stretch",
+    },
+
+    column: {
+        boxSizing: "border-box",
+        flex: "0 0 50%",
+
+        // TODO(mdr): Some call sites probably won't want built-in padding. How
+        //     should we handle that possibility?
+        paddingTop: 64,
+        paddingBottom: 64,
+    },
+
+    leftColumn: {
+        background: Color.darkBlue,
+        color: Color.white,
+
+        // TODO(mdr): Some call sites probably won't want built-in padding. How
+        //     should we handle that possibility?
+        paddingLeft: 64,
+        paddingRight: 52,
+    },
+
+    rightColumn: {
+        background: Color.white,
+
+        // TODO(mdr): Some call sites probably won't want built-in padding. How
+        //     should we handle that possibility?
+        paddingLeft: 52,
+        paddingRight: 64,
+    },
+});
