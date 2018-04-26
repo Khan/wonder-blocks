385cebe139b5287088dd0c60541064f68771e1d5
John Resig <jeresig@gmail.com>
2018-04-26 13:16:33 -0400

    Remove old file ref.

--- packages/wonder-blocks-grid/index.js
+++ packages/wonder-blocks-grid/index.js
@@ -6,7 +6,7 @@ export * from "./components/grid.js";
 export * from "./components/gutter.js";
 export * from "./components/row.js";
 
-export * from "./util/sizes.js";
+export * from "./util/specs.js";
 
 import type GridSize from "./util/types.js";
 export type {GridSize};
