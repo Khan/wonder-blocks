f5bb9c3410b0f499c3357a7dc6b875c923b9ab52
Yejia Chen <yejia@khanacademy.org>
2018-05-04 14:29:08 -0700

    [Button v1] Create ButtonCore component (#63)
    * [Button v1] Create ButtonCore component
    
    In this change, we create a stateless `ButtonCore` component that will render all the different states of `Button`.
    
    Implemented:
    * Styling for all five states of `Button` (default, disabled, hovered, focused, pressed).
    
    Not yet implemented:
    * Using `a` tags for `Button`s with `href`s
    * Rendering a spinner instead of content
    * Rendering `Icon`s
    * Custom `style`
    * `ButtonBehavior`
    
    Test Plan:
    View on Styleguidist

---
+++ packages/wonder-blocks-button/components/button-core.js
@@ -0,0 +1,176 @@
+// @flow
+import React from "react";
+import {StyleSheet, css} from "aphrodite";
+
+import {LabelLarge} from "wonder-blocks-typography";
+import Color, {mix, fade} from "wonder-blocks-color";
+import type {SharedProps} from "./button.js";
+
+type Props = SharedProps & {
+    hovered: boolean,
+    focused: boolean,
+    pressed: boolean,
+};
+
+export default class ButtonCore extends React.Component<Props> {
+    render() {
+        const {
+            color,
+            kind,
+            light,
+            size,
+            disabled,
+            hovered,
+            focused,
+            pressed,
+            children,
+        } = this.props;
+
+        const buttonStyles = _generateStyles(color, kind, light);
+
+        return (
+            <button
+                className={css(
+                    sharedStyles.shared,
+                    disabled && sharedStyles.disabled,
+                    buttonStyles.default,
+                    disabled && buttonStyles.disabled,
+                    !disabled &&
+                        (pressed
+                            ? buttonStyles.active
+                            : (hovered || focused) && buttonStyles.focus),
+                    size === "small" && sharedStyles.small,
+                )}
+                disabled={disabled}
+            >
+                <LabelLarge>{children}</LabelLarge>
+            </button>
+        );
+    }
+}
+
+const sharedStyles = StyleSheet.create({
+    shared: {
+        position: "relative",
+        height: 40,
+        margin: 0,
+        paddingLeft: 16,
+        paddingRight: 16,
+        border: "none",
+        borderRadius: 4,
+        cursor: "pointer",
+        outline: "none",
+    },
+    disabled: {
+        cursor: "auto",
+    },
+    small: {
+        height: 32,
+    },
+});
+
+const styles = {};
+
+const _generateStyles = (color, kind, light) => {
+    const buttonType = color + kind + light.toString();
+    if (styles[buttonType]) {
+        return styles[buttonType];
+    }
+
+    const {white, white64, offBlack32, offBlack50} = Color;
+
+    let newStyles = {};
+    if (kind === "primary") {
+        newStyles = {
+            default: {
+                background: light ? white : color,
+                color: light ? color : white,
+            },
+            focus: {
+                ":before": {
+                    content: "''",
+                    position: "absolute",
+                    top: -3,
+                    left: -3,
+                    right: -3,
+                    bottom: -3,
+                    borderColor: light ? white : color,
+                    borderWidth: 2,
+                    borderStyle: "solid",
+                    borderRadius: 7,
+                },
+            },
+            active: {
+                background: light
+                    ? mix(fade(color, 0.32), white)
+                    : mix(offBlack32, color),
+                color: light
+                    ? mix(offBlack32, color)
+                    : mix(fade(color, 0.32), white),
+            },
+            disabled: {
+                background: light ? white64 : offBlack32,
+                color: light ? color : white64,
+            },
+        };
+    } else if (kind === "secondary") {
+        newStyles = {
+            default: {
+                background: "none",
+                color: light ? white : color,
+                borderColor: light ? white64 : offBlack50,
+                borderStyle: "solid",
+                borderWidth: 1,
+            },
+            focus: {
+                background: light ? color : white,
+                borderColor: light ? white : color,
+                borderWidth: 2,
+                paddingLeft: 14,
+                paddingRight: 14,
+            },
+            active: {
+                background: light
+                    ? mix(offBlack32, color)
+                    : mix(fade(color, 0.32), white),
+                color: light
+                    ? mix(fade(color, 0.32), white)
+                    : mix(offBlack32, color),
+                borderColor: light
+                    ? mix(fade(color, 0.32), white)
+                    : mix(offBlack32, color),
+            },
+            disabled: {
+                color: light ? mix(white, fade(color, 0.32)) : offBlack32,
+                borderColor: light ? mix(white, fade(color, 0.32)) : offBlack32,
+            },
+        };
+    } else if (kind === "tertiary") {
+        newStyles = {
+            default: {
+                background: "none",
+                color: light ? white : color,
+                paddingLeft: 4,
+                paddingRight: 4,
+            },
+            focus: {
+                borderColor: light ? white : color,
+                borderStyle: "solid",
+                borderWidth: 2,
+            },
+            active: {
+                color: light
+                    ? mix(fade(color, 0.32), white)
+                    : mix(offBlack32, color),
+            },
+            disabled: {
+                color: light ? mix(Color.white, fade(color, 0.32)) : offBlack32,
+            },
+        };
+    } else {
+        throw "Button kind not recognized";
+    }
+
+    styles[buttonType] = StyleSheet.create(newStyles);
+    return styles[buttonType];
+};
--- packages/wonder-blocks-button/components/button.js
+++ packages/wonder-blocks-button/components/button.js
@@ -1,52 +1,135 @@
 // @flow
 import React from "react";
-import {StyleSheet, css} from "aphrodite";
 
 import Color from "wonder-blocks-color";
-import {LabelLarge} from "wonder-blocks-typography";
+import type {ValidTints} from "wonder-blocks-color";
+import ButtonCore from "./button-core.js";
 
-type Props = {
-    color?: "blue" | "green" | "gold" | "red",
+export type SharedProps = {
+    /**
+     * Text to appear on the button.
+     */
+    children: string,
+
+    /**
+     * An icon, displayed to the left of the title.
+     */
+    icon?: string,
+
+    /**
+     * If true, replaces the contents with a spinner.
+     */
+    // TODO(yejia): Implement once spinner is implemented.
+    // spinner: boolean,
+
+    /**
+     * The color of the button, either blue or red.
+     */
+    color: ValidTints,
+
+    /**
+     * The kind of the button, either primary, secondary, or tertiary.
+     *
+     * In default state:
+     * - Primary buttons have background colors
+     * - Secondary buttons have a border and no background color
+     * - Tertiary buttons have no background or border
+     */
+    kind: "primary" | "secondary" | "tertiary",
+
+    /**
+     * Whether the button is on a dark/colored background.
+     *
+     * Sets primary button background color to white, and secondary and
+     * tertiary button title to color.
+     */
+    light: boolean,
+
+    /**
+     * The size of the button. "default" = height: 40; "small" = height: 32
+     */
+    size: "default" | "small",
+
+    /**
+     * Whether the button is disabled.
+     */
+    disabled: boolean,
+
+    /**
+     * Test ID used for e2e testing.
+     */
+    testId?: string,
+
+    /**
+     * The content of the modal, appearing between the titlebar and footer.
+     */
+    style?: any,
+    // TODO(yejia): use this if ADR #47 has been implemented
+    /*
+    style?: Style<Exact<{
+        width?: number | string
+        position: Position,
+        ...MarginStyles,
+        ...FlexItemStyles,
+    }>>,
+    */
+};
+
+type Props = SharedProps & {
+    /**
+     * URL to navigate to.
+     *
+     * Note: Either href or onClick must be defined
+     */
+    href?: string,
+
+    /**
+     * Whether to use client-side navigation.
+     *
+     * If the URL passed to href is local to the client-side, e.g.
+     * /math/algebra/eval-exprs, then use ReactRouter to do a client side
+     * navigation by doing history.push(this.props.href) using
+     * ReactRouter's history object
+     */
+    clientSideNav?: boolean,
+
+    /**
+     * Function to call when button is clicked.
+     *
+     * This callback should be used for things like marking BigBingo
+     * conversions. It should NOT be used to redirect to a different URL or to
+     * prevent navigation via e.preventDefault(). The event passed to this
+     * handler will have its preventDefault() and stopPropagation() methods
+     * stubbed out.
+     *
+     * Note: onClick is optional if href is present, but must be defined if
+     * href is not
+     */
     onClick?: (e: SyntheticEvent<MouseEvent | TouchEvent>) => void,
 };
 
 export default class Button extends React.Component<Props> {
+    static defaultProps = {
+        color: Color.blue,
+        kind: "primary",
+        light: false,
+        size: "default",
+        disabled: false,
+    };
+
     render() {
+        const {onClick, href, children} = this.props;
         return (
-            <a
-                className={css(
-                    styles.button,
-                    styles[this.props.color || "blue"],
-                )}
-                onClick={this.props.onClick}
-            >
-                <LabelLarge>Hello, world!</LabelLarge>
-            </a>
+            <div onClick={onClick} href={href}>
+                <ButtonCore
+                    {...this.props}
+                    hovered={false}
+                    focused={false}
+                    pressed={false}
+                >
+                    {children}
+                </ButtonCore>
+            </div>
         );
     }
 }
-
-const styles = StyleSheet.create({
-    button: {
-        display: "inline-block",
-        minHeight: 40,
-        maxHeight: 40,
-        color: Color.white,
-        padding: "10px 16px",
-        boxSizing: "border-box",
-        borderRadius: 4,
-        cursor: "pointer",
-    },
-    blue: {
-        backgroundColor: Color.blue,
-    },
-    green: {
-        backgroundColor: Color.green,
-    },
-    gold: {
-        backgroundColor: Color.gold,
-    },
-    red: {
-        backgroundColor: Color.red,
-    },
-});
