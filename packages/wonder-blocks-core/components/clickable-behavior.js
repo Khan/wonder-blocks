298adba415ca218dbd7315edb73f1d597a7a67ae
Kevin Barabash <kevinb7@gmail.com>
2018-05-17 16:27:49 -0400

    move description of component to top
    I also removed excess newlines.

--- packages/wonder-blocks-core/components/clickable-behavior.js
+++ packages/wonder-blocks-core/components/clickable-behavior.js
@@ -1,4 +1,52 @@
 // @flow
+/**
+ * Add hover, focus, and active status updates to a clickable component.
+ *
+ * Via mouse:
+ * 1. Hover over button -> hover state
+ * 2. Mouse down -> active state
+ * 3. Mouse up -> default state
+ * 4. Press tab -> focus state
+ *
+ * Via touch:
+ * 1. Touch down -> press state
+ * 2. Touch up -> default state
+ *
+ * Via keyboard:
+ * 1. Tab to focus -> focus state
+ * 2. Keydown (spacebar/enter) -> active state
+ * 3. Keyup (spacebar/enter) -> focus state
+ *
+ * `ClickableBehavior` accepts a function as `children` which is passed state
+ * and an object containing event handlers. The `children` function should
+ * return a clickable React Element of some sort.
+ *
+ * Example:
+ *
+ * ```js
+ * class MyClickableComponent extends React.Component<Props> {
+ *     render() {
+ *         return <ClickableBehavior
+ *             disabled={this.props.disabled}
+ *             onClick={this.props.onClick}
+ *         >
+ *             {({hovered}, handlers) =>
+ *                 <RoundRect
+ *                      textcolor='white'
+ *                      backgroundColor={hovered ? 'red' : 'blue'}}
+ *                      {...handlers}
+ *                 >
+ *                      {this.props.children}
+ *                 </RoundRect>
+ *             }
+ *         </ClickableBehavior>
+ *     }
+ * }
+ * ```
+ *
+ * This follows a pattern call [Function as Child Components]
+ * (https://medium.com/merrickchristensen/function-as-child-components-5f3920a9ace9).
+ */
 import React from "react";
 
 type Props = {
@@ -93,66 +141,6 @@ const keyCodes = {
     space: 32,
 };
 
-/**
- * Add hover, focus, and active status updates to a clickable component.
- *
- * Via mouse:
- *
- * 1. Hover over button -> hover state
- *
- * 2. Mouse down -> active state
- *
- * 3. Mouse up -> default state
- *
- * 4. Press tab -> focus state
- *
- *
- * Via touch:
- *
- * 1. Touch down -> press state
- *
- * 2. Touch up -> default state
- *
- *
- * Via keyboard:
- *
- * 1. Tab to focus -> focus state
- *
- * 2. Keydown (spacebar/enter) -> active state
- *
- * 3. Keyup (spacebar/enter) -> focus state
- *
- *
- * `ClickableBehavior` accepts a function as `children` which is passed state
- * and an object containing event handlers. The `children` function should
- * return a clickable React Element of some sort.
- *
- * Example:
- *
- * ```js
- * class MyClickableComponent extends React.Component<Props> {
- *     render() {
- *         return <ClickableBehavior
- *             disabled={this.props.disabled}
- *             onClick={this.props.onClick}
- *         >
- *             {({hovered}, handlers) =>
- *                 <RoundRect
- *                      textcolor='white'
- *                      backgroundColor={hovered ? 'red' : 'blue'}}
- *                      {...handlers}
- *                 >
- *                      {this.props.children}
- *                 </RoundRect>
- *             }
- *         </ClickableBehavior>
- *     }
- * }
- * ```
- *
- * This follows a pattern call [Function as Child Components]
- * (https://medium.com/merrickchristensen/function-as-child-components-5f3920a9ace9).
- */
 export default class ClickableBehavior extends React.Component<Props, State> {
     waitingForClick: boolean;
     keyboardClick: boolean;
