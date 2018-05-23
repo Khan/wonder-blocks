c463fe02512a3ab62d21de96af7cebd8cc0824c1
Kevin Barabash <kevinbarabash@khanacademy.org>
2018-05-23 18:52:03 -0400

    add README.md for demo/src/third-party, update demo/README.md to explain the purpose of the demo app

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+import registerServiceWorker from "./third-party/registerServiceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
