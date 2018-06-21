001e56895d1d17932e932c651cf1c670ffbdb718
Jeff Yates <jeff@somewhatabstract.com>
2018-06-21 13:22:30 -0700

    Popper.js mock commentary

--- packages/wonder-blocks-tooltip/__mocks__/popper.js.js
+++ packages/wonder-blocks-tooltip/__mocks__/popper.js.js
@@ -1,6 +1,9 @@
 // @flow
-// Mock to allow us to run tests that need react-popper or popper.js
-// From
+/**
+ *  Mock to allow us to run tests that need react-popper or popper.js
+ * Inspired by:
+ *     https://github.com/FezVrasta/popper.js/issues/478#issuecomment-369446079
+ */
 import PopperJs from "popper.js";
 
 export default class Popper {
