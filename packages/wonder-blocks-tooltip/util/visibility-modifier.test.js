a950a853cf924ffb23111efc74fe569b6cb7f188
Jeff Yates <jeff@somewhatabstract.com>
2018-06-22 23:16:47 -0700

    Test coverage for visibility-modifier

--- packages/wonder-blocks-tooltip/util/visibility-modifier.test.js
+++ packages/wonder-blocks-tooltip/util/visibility-modifier.test.js
@@ -1,4 +1,24 @@
 // @flow
+import Popper from "popper.js";
+import visibilityModifierDefaultConfig from "./visibility-modifier.js";
+
 describe("Visibility PopperJS Modifier", () => {
-    test("TODO", () => {});
+    test("returns a modifier configuration", () => {
+        // Arrange
+        // Act
+        const result = visibilityModifierDefaultConfig;
+
+        // Assert
+        expect(result).toMatchObject({
+            enabled: true,
+            fn: expect.any(Function),
+            order: Popper.Defaults.modifiers.hide.order + 1,
+        });
+    });
+
+    describe("#fn", () => {
+        test("TODO", () => {
+
+        });
+    });
 });
