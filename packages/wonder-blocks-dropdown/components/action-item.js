6f2bd899cdefb7de6a79aa9c80501da9cc921a49
Sophie Mori <ksmori14@gmail.com>
2018-06-25 14:08:51 -0700

    Add dropdown package and basic components for menu items

---
+++ packages/wonder-blocks-dropdown/components/action-item.js
@@ -0,0 +1,197 @@
+// @flow
+// For menu items that trigger an action, such as going to a new page or
+// opening a modal.
+
+import * as React from "react";
+import {StyleSheet} from "aphrodite";
+import {Link} from "react-router-dom";
+
+import Color, {mix, fade} from "@khanacademy/wonder-blocks-color";
+import Spacing from "@khanacademy/wonder-blocks-spacing";
+import {LabelLarge} from "@khanacademy/wonder-blocks-typography";
+import {
+    View,
+    addStyle,
+    getClickableBehavior,
+} from "@khanacademy/wonder-blocks-core";
+
+const {blue, white, offBlack, offBlack32} = Color;
+
+type ActionProps = {
+    /**
+     * Display text of the menu item.
+     */
+    label: string,
+
+    /**
+     * Whether this menu item is disabled. A disabled item may not be selected.
+     */
+    disabled?: boolean,
+
+    /**
+     * Whether this item should be indented to have menu items left-align in
+     * text when an ActionItem is used in the same menu as items that have
+     * checks or checkboxes.
+     */
+    indent?: boolean,
+
+    /**
+     * URL to navigate to.
+     *
+     * Note: Either href or onClick must be defined
+     */
+    href?: string,
+
+    /**
+     * Whether to use client-side navigation.
+     *
+     * If the URL passed to href is local to the client-side, e.g.
+     * /math/algebra/eval-exprs, then it uses react-router-dom's Link
+     * component which handles the client-side navigation.
+     *
+     * NOTE: All URLs containing a protocol are considered external, e.g.
+     * https://khanacademy.org/math/algebra/eval-exprs will trigger a full
+     * page reload.
+     */
+    clientNav?: boolean,
+
+    /**
+     * Function to call when button is clicked.
+     *
+     * This callback should be used for things like marking BigBingo
+     * conversions. It should NOT be used to redirect to a different URL or to
+     * prevent navigation via e.preventDefault(). The event passed to this
+     * handler will have its preventDefault() and stopPropagation() methods
+     * stubbed out.
+     *
+     * Note: onClick is optional if href is present, but must be defined if
+     * href is not
+     */
+    onClick?: () => void,
+
+    /**
+     * Custom styles.
+     */
+    style?: any,
+};
+
+const StyledAnchor = addStyle("a");
+const StyledButton = addStyle("button");
+const StyledLink = addStyle(Link);
+
+export default class ActionItem extends React.Component<ActionProps> {
+    static defaultProps = {
+        disabled: false,
+        indent: false,
+    };
+
+    getTag() {
+        const {href, clientNav} = this.props;
+        if (href) {
+            if (clientNav) {
+                return StyledLink;
+            } else {
+                return StyledAnchor;
+            }
+        } else {
+            return StyledButton;
+        }
+    }
+
+    render() {
+        const {
+            clientNav,
+            disabled,
+            href,
+            indent,
+            label,
+            onClick,
+            style,
+        } = this.props;
+
+        const ClickableBehavior = getClickableBehavior(
+            href,
+            clientNav,
+            this.context.router,
+        );
+
+        const Tag = this.getTag();
+
+        return (
+            <ClickableBehavior
+                disabled={disabled}
+                onClick={onClick}
+                href={href}
+            >
+                {(state, handlers) => {
+                    const {pressed, hovered, focused} = state;
+
+                    const defaultStyle = [
+                        styles.shared,
+                        disabled && styles.disabled,
+                        !disabled &&
+                            (pressed
+                                ? styles.active
+                                : (hovered || focused) && styles.focus),
+                    ];
+
+                    const props = {
+                        style: [defaultStyle, style],
+                        disabled,
+                        ...handlers,
+                    };
+
+                    if (href) {
+                        if (clientNav) {
+                            props.to = href;
+                        } else {
+                            props.href = href;
+                        }
+                    }
+
+                    return (
+                        <Tag {...props} role="menuitem">
+                            {indent && <View style={{width: Spacing.medium}} />}
+                            <LabelLarge>{label}</LabelLarge>
+                        </Tag>
+                    );
+                }}
+            </ClickableBehavior>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    shared: {
+        background: white,
+        color: offBlack,
+        cursor: "pointer",
+        textDecoration: "none",
+        border: "none",
+        flexDirection: "row",
+        alignItems: "center",
+        display: "flex",
+        height: 40,
+        paddingLeft: 16,
+        paddingRight: 16,
+        whiteSpace: "nowrap",
+    },
+
+    // hover and focus states
+    focus: {
+        color: white,
+        background: blue,
+    },
+
+    // active and pressed states
+    active: {
+        color: mix(fade(blue, 0.32), white),
+        background: mix(offBlack32, blue),
+    },
+
+    // disabled state
+    disabled: {
+        color: offBlack32,
+        cursor: "default",
+    },
+});
---
+++ packages/wonder-blocks-dropdown/components/select-item.js
@@ -0,0 +1,258 @@
+// @flow
+// For menu items that can be selected, selection denoted either with a
+// check ✔️ or a checkbox ☑️
+
+import * as React from "react";
+import {StyleSheet} from "aphrodite";
+
+import Color, {mix, fade} from "@khanacademy/wonder-blocks-color";
+import Spacing from "@khanacademy/wonder-blocks-spacing";
+import {LabelLarge} from "@khanacademy/wonder-blocks-typography";
+import {
+    View,
+    addStyle,
+    getClickableBehavior,
+} from "@khanacademy/wonder-blocks-core";
+
+const checkIcon = `M10,3.8C10,4,9.9,4.2,9.8,4.3L5.1,8.9L4.3,9.8C4.2,9.9,4,10,
+3.8,10S3.5,9.9,3.4,9.8L2.5,8.9L0.2,6.6C0.1,6.5,0,6.3,0,6.2s0.1-0.3,0.2-0.4
+l0.9-0.9c0.1-0.1,0.3-0.2,0.4-0.2s0.3,0.1,0.4,0.2l1.9,1.9l4.2-4.2c0.1-0.1,
+0.3-0.2,0.4-0.2c0.2,0,0.3,0.1,0.4,0.2l0.9,0.9C9.9,3.5,10,3.7,10,3.8z`;
+
+const {blue, white, offBlack, offBlack32} = Color;
+
+type SelectProps = {
+    /**
+     * Display text of the menu item.
+     */
+    label: string,
+
+    /**
+     * Whether this item is selected.
+     */
+    selected: boolean,
+
+    /**
+     * Value of the item, used as a key of sorts for the parent menu to manage
+     * its menu items, because label/display text may be identical in some
+     * menus. This is the value passed back when the item is pressed.
+     */
+    value: string,
+
+    /**
+     * Whether the item should show a check or checkbox to indicate selection
+     * state.
+     */
+    variant: "check" | "checkbox",
+
+    /**
+     * Callback for when this item is pressed. Passes value of the item and the
+     * new selection state of the item. Should be handled by an implementation
+     * of Menu.
+     */
+    onToggle: (value: string, selectionState: boolean) => void,
+
+    /**
+     * Whether this menu item is disabled. A disabled item may not be selected.
+     */
+    disabled?: boolean,
+
+    /**
+     * Optional client-supplied callback when this item is called.
+     */
+    onClick?: (selectionState: boolean) => void,
+
+    /**
+     * Custom styles.
+     */
+    style?: any,
+};
+
+const StyledLink = addStyle("a");
+
+type CheckProps = {
+    selected: boolean,
+    pressed: boolean,
+    hovered: boolean,
+    focused: boolean,
+};
+
+// TODO: return with Icon once it's been made!
+const Check = (props: CheckProps) => {
+    const {selected, pressed, hovered, focused} = props;
+
+    return (
+        <View style={styles.checkWrapper}>
+            <svg
+                role="img"
+                width="16px"
+                height="16px"
+                viewBox="0 0 11.8 11.8"
+                aria-hidden="true"
+            >
+                {selected && (
+                    <path
+                        fill={pressed || hovered || focused ? white : offBlack}
+                        d={checkIcon}
+                    />
+                )}
+            </svg>
+        </View>
+    );
+};
+
+// TODO: getting replaced with a variant from Choice
+const Checkbox = (props: CheckProps) => {
+    const {selected, pressed, hovered, focused} = props;
+
+    const activeBlue = mix(offBlack32, blue);
+
+    const bgColor = selected && !(pressed || hovered || focused) ? blue : white;
+
+    return (
+        <View
+            style={
+                (styles.checkWrapper,
+                !(selected || pressed || hovered || focused) && styles.checkbox)
+            }
+        >
+            <svg
+                role="img"
+                width="16px"
+                height="16px"
+                viewBox="0 0 11.8 11.8"
+                aria-hidden="true"
+                style={{backgroundColor: bgColor}}
+            >
+                {selected && (
+                    <path
+                        fill={
+                            hovered || focused
+                                ? blue
+                                : pressed
+                                    ? activeBlue
+                                    : white
+                        }
+                        d={checkIcon}
+                    />
+                )}
+            </svg>
+        </View>
+    );
+};
+
+export default class SelectItem extends React.Component<SelectProps> {
+    render() {
+        const {
+            disabled,
+            label,
+            onClick,
+            onToggle,
+            selected,
+            style,
+            value,
+            variant,
+        } = this.props;
+
+        const ClickableBehavior = getClickableBehavior(this.context.router);
+
+        return (
+            <ClickableBehavior
+                disabled={disabled}
+                onClick={() => {
+                    onToggle(value, !selected);
+                    if (onClick) {
+                        onClick(!selected);
+                    }
+                }}
+            >
+                {(state, handlers) => {
+                    const {pressed, hovered, focused} = state;
+
+                    const defaultStyle = [
+                        styles.shared,
+                        disabled && styles.disabled,
+                        !disabled &&
+                            (pressed
+                                ? styles.active
+                                : (hovered || focused) && styles.focus),
+                    ];
+
+                    const allStyles = [defaultStyle, style];
+
+                    return (
+                        <StyledLink style={allStyles} {...handlers}>
+                            <View
+                                style={[styles.itemContainer]}
+                                role="menuitemcheckbox"
+                                aria-checked={selected ? "true" : "false"}
+                            >
+                                {variant === "check" ? (
+                                    <Check selected={selected} {...state} />
+                                ) : (
+                                    <Checkbox selected={selected} {...state} />
+                                )}
+                                <LabelLarge style={styles.label}>
+                                    {label}
+                                </LabelLarge>
+                            </View>
+                        </StyledLink>
+                    );
+                }}
+            </ClickableBehavior>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    shared: {
+        background: white,
+        color: offBlack,
+        cursor: "pointer",
+    },
+
+    label: {
+        paddingLeft: Spacing.xSmall,
+    },
+
+    // hover and focus states
+    focus: {
+        color: white,
+        background: blue,
+    },
+
+    // active and pressed states
+    active: {
+        color: mix(fade(blue, 0.32), white),
+        background: mix(offBlack32, blue),
+    },
+
+    // disabled state
+    disabled: {
+        color: offBlack32,
+        cursor: "default",
+    },
+
+    itemContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        height: 40,
+        paddingLeft: 8,
+        paddingRight: 16,
+        whiteSpace: "nowrap",
+    },
+
+    checkWrapper: {
+        height: 16,
+        width: 16,
+        boxSizing: "border-box",
+    },
+
+    checkbox: {
+        height: 16,
+        width: 16,
+        borderColor: offBlack32,
+        borderStyle: "solid",
+        borderWidth: 1,
+    },
+});
