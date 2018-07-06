4939d778351628bfa2521bf7a1f9055b0d89dd00
Ian Reynolds <idreyn@gmail.com>
2018-07-06 10:15:30 -0500

    [Icon v1] Create and employ Icon component (#133)
    * implement wonder-blocks-icon
    
    * Make sure dropdown is marked as private/prototype (#175)
    
    * Added coverage for mix and fade (#168)
    
    * Moved utils.js
    
    * Added coverage for mix and fade
    
    * Remove duplicate testpoint
    
    * Add Inconsolata to links in styleguide config (#176)
    
    * Add SingleSelectMenu to dropdown package (#153)
    
    * add more code owners (#178)
    
    * add more code owners
    
    * for an @
    
    * removing @matchu from begin the modal owner
    
    * Updating the portal tests (#179)
    
    * Updating the portal tests
    
    * Refactoring to get more coverage, fix edge cases, and improve things in general
    
    * Update based on review comments
    
    * Abstract away react-popper types and idiosyncrasies (#171)
    
    * Abstract away react-popper types and idiosyncracies
    
    * Add test coverage for RefTracker
    
    * Tidy up examples and other tweaks now that inline styles override classes again
    
    Fix an issue with RefTracker where this context was changing
    
    * Remove unused code
    
    * Addressed review comments
    
    * Incorporate multi selection menus! (#169)
    
    * Add UniqueIDProvider (#180)
    
    * Add UniqueIDProvider
    
    * Implement mockOnFirstRender behaviour
    
    * Addressing review comments
    
    Also renamed id -> get to match our original decisions in ADR 58
    
    * Document IIdentifierFactory
    
    * Fixup comments to be styleguidist friendly
    
    * implement wonder-blocks-icon
    
    * fix up IconButton tests
    
    * add correct deps to wonder-blocks-icon and add wonder-blocks-icon as a dep to packages that use it

--- packages/wonder-blocks-modal/components/modal-close-button.test.js
+++ packages/wonder-blocks-modal/components/modal-close-button.test.js
@@ -1,14 +1,13 @@
 // @flow
 import React from "react";
-import {shallow} from "enzyme";
+import {mount} from "enzyme";
 
 import ModalCloseButton from "./modal-close-button.js";
 
 describe("ModalCloseButton", () => {
     test("Clicking the button triggers `onClick`", () => {
         const onClick = jest.fn();
-        const wrapper = shallow(<ModalCloseButton onClick={onClick} />);
-
+        const wrapper = mount(<ModalCloseButton onClick={onClick} />);
         expect(onClick).not.toHaveBeenCalled();
         wrapper.find("button").simulate("click");
         expect(onClick).toHaveBeenCalled();
