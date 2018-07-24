f4a53de3406eacb98c798f652687a938d369acf8
Kevin Barabash <kevinbarabash@khanacademy.org>
2018-07-24 10:55:16 -0400

    add a better comment and change 'styles' to 'overrides'

--- packages/wonder-blocks-core/util/add-style.js
+++ packages/wonder-blocks-core/util/add-style.js
@@ -15,7 +15,8 @@ export default function addStyle<T: Object>(
         props: T & {style: any, mediaSize: MediaSize, mediaSpec: MediaSpec},
     ) {
         const {style, mediaSize, mediaSpec, ...otherProps} = props;
-        const reset = typeof Component === "string" ? styles[Component] : null;
+        const reset =
+            typeof Component === "string" ? overrides[Component] : null;
 
         const {className, style: inlineStyles} = processStyleList(
             [reset, defaultStyle, style],
@@ -34,8 +35,12 @@ export default function addStyle<T: Object>(
     return MediaLayoutWrapper(StyleComponent);
 }
 
-// TODO(kevinb): add more style resets
-const styles = StyleSheet.create({
+/**
+ * These are necessary to override various custom styles that browsers add so that
+ * elements have consistent styles across all browsers.  Only add styles here if
+ * they appear in https://github.com/necolas/normalize.css/blob/master/normalize.css.
+ */
+const overrides = StyleSheet.create({
     button: {
         margin: 0, // Safari adds 2px left/right margins
     },
