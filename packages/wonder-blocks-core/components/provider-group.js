8e3b1450d173d8d601107226bb65cd0de8af3cab
Jeff Yates <jeff@somewhatabstract.com>
2018-08-20 15:26:10 -0500

    Support multiple args

--- packages/wonder-blocks-core/components/provider-group.js
+++ packages/wonder-blocks-core/components/provider-group.js
@@ -18,7 +18,22 @@ export default class ProviderGroup extends React.Component<Props> {
         let i = 0;
         const buildProviders = (...args) => {
             if (i > 0) {
-                provided.push(...args);
+                if (args.length === 0) {
+                    // We need to have something in the array so that consumers
+                    // can correlate the provider definition with the values
+                    // we give.
+                    provided.push(undefined);
+                } else if (args.length === 1) {
+                    // We unpack the array to a single value.
+                    // Is this bad? I don't think so.
+                    // Consumers of the results should know if something is
+                    // providing multiple values, and single values are the
+                    // more common use case so we should make that the easiest.
+                    provided.push(...args);
+                } else {
+                    // We have lots of args so let's push the argument array.
+                    provided.push(args);
+                }
             }
             if (i < providers.length) {
                 const providerEl = React.createElement(providers[i].type, {
