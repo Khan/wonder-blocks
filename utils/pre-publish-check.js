c4aa5a633effcf32b86c83a669c182127afec80a
Kevin Barabash <kevinb7@gmail.com>
2018-08-20 19:46:02 -0400

    build es6 modules to support docuflow (#275)
    * build es6 modules to support docuflow
    
    * remove unused rollup plugins
    
    * update circlci config and publish script to also build es6 modules
    
    * address feedback from code review

--- utils/pre-publish-check.js
+++ utils/pre-publish-check.js
@@ -8,6 +8,56 @@ const {exec} = require("child_process");
 
 const inquirer = require("inquirer");
 
+const checkPublishConfig = (publishConfig) => {
+    if (!publishConfig || publishConfig.access !== "public") {
+        console.error(
+            `ERROR: ${name} is missing a "publishConfig": {"access": "public"} section.`,
+        );
+        process.exit(1);
+    }
+};
+
+const checkPackageField = (pkgJson, field, value) => {
+    if (pkgJson[field] !== value) {
+        console.error(
+            `ERROR: ${
+                pkgJson.name
+            } must have a "${field}" set to ${JSON.stringify(value)}.`,
+        );
+        process.exit(1);
+    }
+};
+
+const checkPackageMain = (pkgJson) =>
+    checkPackageField(pkgJson, "main", "dist/index.js");
+
+const checkPackageModule = (pkgJson) =>
+    checkPackageField(pkgJson, "module", "dist/es/index.js");
+
+const checkPackageSource = (pkgJson) =>
+    checkPackageField(pkgJson, "source", "index.js");
+
+const checkPackagePrivate = (pkgJson) => {
+    if (pkgJson.private) {
+        console.warn(
+            `${pkgJson.name} is private and won't be published to NPM.`,
+        );
+        return true;
+    }
+    return false;
+};
+
+const checkNpmUser = (currentUser) => {
+    if (currentUser.trim() !== "khanacademy") {
+        console.error(
+            `ERROR: You are not logged in to NPM as "khanacademy". ` +
+                `Run "npm login" and use the password from: ` +
+                `https://phabricator.khanacademy.org/K207`,
+        );
+        process.exit(1);
+    }
+};
+
 glob(
     path.join(__dirname, "..", "packages", "**", "package.json"),
     (err, pkgPaths) => {
@@ -15,39 +65,17 @@ glob(
 
         for (const pkgPath of pkgPaths) {
             const pkgJson = require(path.relative(__dirname, pkgPath));
-            const {publishConfig, name} = pkgJson;
+            const {publishConfig} = pkgJson;
 
-            if (!publishConfig || publishConfig.access !== "public") {
-                console.error(
-                    `ERROR: ${name} is missing a "publishConfig": {"access": "public"} section.`,
-                );
-                process.exit(1);
-            }
-
-            if (pkgJson.main !== "dist/index.js") {
-                console.error(
-                    `ERROR: ${name} must have a "main" set to "dist/index.js".`,
-                );
-                process.exit(1);
-            }
-
-            if (pkgJson.private) {
-                console.warn(
-                    `${name} is private and won't be published to NPM.`,
-                );
-                warnings = true;
-            }
+            checkPublishConfig(publishConfig);
+            checkPackageMain(pkgJson);
+            checkPackageModule(pkgJson);
+            checkPackageSource(pkgJson);
+            warnings = checkPackagePrivate(pkgJson);
         }
 
         exec("npm whoami", (err, currentUser) => {
-            if (currentUser.trim() !== "khanacademy") {
-                console.error(
-                    `ERROR: You are not logged in to NPM as "khanacademy". ` +
-                        `Run "npm login" and use the password from: ` +
-                        `https://phabricator.khanacademy.org/K207`,
-                );
-                process.exit(1);
-            }
+            checkNpmUser(currentUser);
 
             if (warnings) {
                 inquirer
