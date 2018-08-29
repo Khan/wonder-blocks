beb3fe2aa52db79b8100427a4b5e5e8a9d473016
Pamela Fox <pamela.fox@gmail.com>
2018-08-29 12:45:50 -0700

    Fix spacing from vim

--- packages/wonder-blocks-toolbar/components/toolbar.js
+++ packages/wonder-blocks-toolbar/components/toolbar.js
@@ -61,8 +61,8 @@ export default class Toolbar extends React.Component<Props> {
 
     renderContent(content: React.Node) {
         let contentArray = Array.isArray(content) ? content : [content];
-	// Remove empty/null/undefined elements, then map to Views
-	return contentArray.filter(c => c).map((content, i) => (
+	    // Remove empty/null/undefined elements, then map to Views
+	    return contentArray.filter(c => c).map((content, i) => (
             <View
                 style={[sharedStyles.content, sharedStyles.verticalAlign]}
                 key={i.toString()}
