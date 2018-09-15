06a3463691b5a6839c3fece02638782d7094820a
John Resig <jeresig@gmail.com>
2018-09-15 00:04:38 -0400

    Get things closer to passing.

--- babel.config.js
+++ babel.config.js
@@ -7,8 +7,8 @@ module.exports = {
     "plugins": [
         "@babel/plugin-proposal-class-properties",
         "@babel/plugin-proposal-object-rest-spread",
-        "@babel/plugin-syntax-dynamic-import",
-        "@babel/plugin-proposal-export-default-from"
+        "@babel/plugin-proposal-export-default-from",
+        "babel-plugin-dynamic-import-node",
     ],
 };
 
