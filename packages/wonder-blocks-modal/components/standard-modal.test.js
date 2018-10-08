4913f520441cdebdbd698ebb6f882464e20d9579
Kevin Barabash <kevinbarabash@khanacademy.org>
2018-10-08 14:07:32 -0400

    add test

--- packages/wonder-blocks-modal/components/standard-modal.test.js
+++ packages/wonder-blocks-modal/components/standard-modal.test.js
@@ -2,7 +2,9 @@
 import * as React from "react";
 import {shallow} from "enzyme";
 
+import expectRenderError from "../../../utils/testing/expect-render-error.js";
 import StandardModal from "./standard-modal.js";
+import ModalContext from "./modal-context.js";
 
 describe("StandardModal", () => {
     test("Ensure the ModalCloseButton isn't inside.", () => {
@@ -11,5 +13,19 @@ describe("StandardModal", () => {
         );
 
         expect(wrapper.find("ModalCloseButton").exists()).toBeFalsy();
+    });
+
+    test("ModalContext.Provider and onClose should warn", () => {
+        expectRenderError(
+            <ModalContext.Provider value={{closeModal: () => {}}}>
+                <StandardModal
+                    title="Title"
+                    content="Content"
+                    footer="Footer"
+                    onClose={() => {}}
+                />,
+            </ModalContext.Provider>,
+            "You've specified 'onClose' on a modal when using ModalLauncher.  Please specify 'onClose' on the ModalLauncher instead",
+        );
     });
 });
