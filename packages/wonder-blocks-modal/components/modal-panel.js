9c6deb3ef6e491d1a3a47caf9db13c5bb34c0187
Kevin Barabash <kevinb7@gmail.com>
2018-10-08 15:52:21 -0400

    Fix close icon on modals and update toolbar to match updated Toolbar spec (#338)
    * fix position of close button in modals
    
    * update Toolbar examples to match updated design spec
    
    * add test
    
    * fix alignment of close button in modals without top toolbar
    
    * extract logic for close button on modals into a new component

--- packages/wonder-blocks-modal/components/modal-panel.js
+++ packages/wonder-blocks-modal/components/modal-panel.js
@@ -3,15 +3,13 @@ import * as React from "react";
 import {StyleSheet} from "aphrodite";
 import Color from "@khanacademy/wonder-blocks-color";
 import {View} from "@khanacademy/wonder-blocks-core";
-import {icons} from "@khanacademy/wonder-blocks-icon";
-import IconButton from "@khanacademy/wonder-blocks-icon-button";
 import Toolbar from "@khanacademy/wonder-blocks-toolbar";
 
 import type {StyleType} from "@khanacademy/wonder-blocks-core";
 import ModalContent from "./modal-content.js";
 import ModalHeader from "./modal-header.js";
 import ModalFooter from "./modal-footer.js";
-import ModalContext from "./modal-context.js";
+import CloseButton from "./close-button.js";
 
 type Props = {|
     /**
@@ -75,29 +73,10 @@ export default class ModalPanel extends React.Component<Props> {
                     //    mediaSize === "small" && styles.smallCloseButton,
                 ]}
             >
-                <ModalContext.Consumer>
-                    {({closeModal}) => {
-                        if (closeModal && onClose) {
-                            throw new Error(
-                                "You've specified 'onClose' on a modal when using ModalLauncher.  Please specify 'onClose' on the ModalLauncher instead",
-                            );
-                        }
-                        return (
-                            <IconButton
-                                icon={icons.dismiss}
-                                // TODO(mdr): Translate this string for i18n.
-                                aria-label="Close modal"
-                                onClick={closeModal || onClose}
-                                kind={
-                                    topBackgroundColor === "dark"
-                                        ? "primary"
-                                        : "tertiary"
-                                }
-                                light={topBackgroundColor === "dark"}
-                            />
-                        );
-                    }}
-                </ModalContext.Consumer>
+                <CloseButton
+                    onClick={onClose}
+                    light={topBackgroundColor === "dark"}
+                />
             </View>
         );
     }
@@ -177,16 +156,16 @@ const styles = StyleSheet.create({
 
     closeButton: {
         position: "absolute",
-        left: 8,
-        top: 8,
+        left: 16,
+        top: 16,
         // This is to allow the button to be tab-ordered before the modal
         // content but still be above the header and content.
         zIndex: 1,
     },
 
     smallCloseButton: {
-        left: 8,
-        top: 8,
+        left: 16,
+        top: 16,
     },
 
     dark: {
