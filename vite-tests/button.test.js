d0c03bc71f9950e8b4845f61a4b9980393f59b9a
Kevin Barabash <kevinbarabash@khanacademy.org>
2018-10-22 22:10:41 -0400

    arrange, act, assert

--- vite-tests/button.test.js
+++ vite-tests/button.test.js
@@ -6,9 +6,14 @@ import Button from "../packages/wonder-blocks-button/components/button.js";
 
 describe("foo", () => {
     test("it should render a button", async () => {
+        // Arrange
         // $FlowFixMe
         const element = await render(<Button>Hello, world!</Button>);
+
+        // Act
         const text = await element.getText();
+
+        // Assert
         expect(text).toBe("Hello, world!");
     });
 });
