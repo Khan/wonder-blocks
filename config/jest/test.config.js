ed64992ff8446e80fe064a081e57bf80f4c2da94
Kevin Barabash <kevinb7@gmail.com>
2018-10-30 22:02:20 -0400

    initial setup of vite test runner (#343)
    * initial setup of vite test runner
    
    * add a simple test for Button
    
    * make sure modules are built before running tests
    
    * compile both cjs and es6 versions of our packages
    
    * prevent ENOMEM when running tests on circleci
    
    * compute coverage across all wonder-blocks packages' source files
    
    * revert change to moduleNameMapper setting since it didn't have the intended effect
    
    * remove foo tests, move test config files into config/test
    
    * revert unnecessary changes
    
    * include shared-unpackaged when running tests
    
    * arrange, act, assert
    
    * avoid missing tests in the future
    
    * add comments

---
+++ config/jest/test.config.js
@@ -0,0 +1,26 @@
+/**
+ * This is the main jest config.  It runs tests using the default
+ * test environment: jest-environment-jsdom.
+ */
+module.exports = {
+    rootDir: "../../",
+    transform: {
+        "^.+\\.jsx?$": "<rootDir>/config/jest/test.transform.js",
+    },
+    testEnvironment: "jest-environment-jsdom",
+    globals: {
+        SNAPSHOT_INLINE_APHRODITE: true,
+    },
+    testMatch: ["<rootDir>/**/*.test.js"],
+    setupTestFrameworkScriptFile: "<rootDir>/config/jest/test-setup.js",
+    moduleNameMapper: {
+        "^@khanacademy/(.*)$":
+            "<rootDir>/node_modules/@khanacademy/$1/index.js",
+    },
+    collectCoverageFrom: [
+        "packages/**/*.js",
+        "!packages/**/dist/**/*.js",
+        "!<rootDir>/node_modules/",
+        "!packages/**/node_modules/",
+    ],
+};
