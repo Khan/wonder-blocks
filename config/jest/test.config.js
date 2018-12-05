8dcb2c5dd2587e9fb2a7ef6b98b779907a9ce0bd
Kevin Barabash <kevinbarabash@khanacademy.org>
2018-12-05 11:33:57 -0500

    use .vscode/setting.json to point jest plugin to the new config location

---
+++ config/jest/test.config.js
@@ -0,0 +1,27 @@
+/**
+ * This is the main jest config.  It runs tests using the default
+ * test environment: jest-environment-jsdom.
+ */
+module.exports = {
+    rootDir: "../../",
+    transform: {
+        "^.+\\.jsx?$": "<rootDir>/config/jest/test.transform.js",
+    },
+    testEnvironment: "jest-environment-jsdom",
+    globals: {
+        SNAPSHOT_INLINE_APHRODITE: true,
+    },
+    testMatch: ["<rootDir>/**/*.test.js"],
+    setupTestFrameworkScriptFile: "<rootDir>/config/jest/test-setup.js",
+    moduleNameMapper: {
+        "^@khanacademy/(.*)$":
+            "<rootDir>/node_modules/@khanacademy/$1/index.js",
+    },
+    collectCoverageFrom: [
+        "packages/**/*.js",
+        "!packages/**/*.vite_test.js",
+        "!packages/**/dist/**/*.js",
+        "!<rootDir>/node_modules/",
+        "!packages/**/node_modules/",
+    ],
+};
