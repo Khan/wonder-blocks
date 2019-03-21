182677f104c94c328e78c7f527d76e4c32fbc9d3
John Resig <jeresig@gmail.com>
2019-03-21 10:55:43 -0400

    Update lint disables.

--- packages/wonder-blocks-tooltip/components/tooltip-popper.js
+++ packages/wonder-blocks-tooltip/components/tooltip-popper.js
@@ -11,8 +11,8 @@ import TooltipBubble from "./tooltip-bubble.js";
 // NOTE(jeff): Here we share some code for use with PopperJS. Long term,
 // we should either contribute this code to the PopperJS component, or its
 // own non-wonder-blocks package.
-// eslint-disable-next-line import/no-restricted-paths $FlowIgnoreMe
-import visibilityModifierDefaultConfig from "../../../shared-unpackaged/visibility-modifier.js";
+// $FlowIgnoreMe
+import visibilityModifierDefaultConfig from "../../../shared-unpackaged/visibility-modifier.js"; // eslint-disable-line import/no-restricted-paths
 import RefTracker from "../util/ref-tracker.js";
 import type {Placement} from "../util/types.js";
 import type {TooltipBubbleProps} from "./tooltip-bubble.js";
