2cffa5455e66ce7be973f2fa576d52d0b62d0374
Kevin Barabash <kevinbarabash@khanacademy.org>
2019-04-11 16:22:18 -0700

    replace Cree with Greek in docs and replace .ttf with .woff2 files

--- packages/wonder-blocks-typography/docs.md
+++ packages/wonder-blocks-typography/docs.md
@@ -1,76 +1,4 @@
-```js
-const {View} = require("@khanacademy/wonder-blocks-core");
-
-<View>
-    <Title dir="rtl">مرحبا</Title>
-    <HeadingLarge dir="rtl">مرحبا</HeadingLarge>
-    <HeadingMedium dir="rtl">مرحبا</HeadingMedium>
-    <HeadingSmall dir="rtl">مرحبا</HeadingSmall>
-    <HeadingXSmall dir="rtl">مرحبا</HeadingXSmall>
-    <BodySerifBlock dir="rtl">مرحبا</BodySerifBlock>
-    <BodySerif dir="rtl">مرحبا</BodySerif>
-    <BodyMonospace dir="rtl">مرحبا</BodyMonospace>
-    <Body dir="rtl">مرحبا</Body>
-    <LabelLarge dir="rtl">مرحبا</LabelLarge>
-    <LabelMedium dir="rtl">مرحبا</LabelMedium>
-    <LabelSmall dir="rtl">مرحبا</LabelSmall>
-    <LabelXSmall dir="rtl">مرحبا</LabelXSmall>
-    <Tagline dir="rtl">مرحبا</Tagline>
-    <Caption dir="rtl">مرحبا</Caption>
-    <Footnote dir="rtl">مرحبا</Footnote>
-
-    <Title>ᑕᓂᓯ</Title>
-    <HeadingLarge>ᑕᓂᓯ</HeadingLarge>
-    <HeadingMedium>ᑕᓂᓯ</HeadingMedium>
-    <HeadingSmall>ᑕᓂᓯ</HeadingSmall>
-    <HeadingXSmall>ᑕᓂᓯ</HeadingXSmall>
-    <BodySerifBlock>ᑕᓂᓯ</BodySerifBlock>
-    <BodySerif>ᑕᓂᓯ</BodySerif>
-    <BodyMonospace>ᑕᓂᓯ</BodyMonospace>
-    <Body>ᑕᓂᓯ</Body>
-    <LabelLarge>ᑕᓂᓯ</LabelLarge>
-    <LabelMedium>ᑕᓂᓯ</LabelMedium>
-    <LabelSmall>ᑕᓂᓯ</LabelSmall>
-    <LabelXSmall>ᑕᓂᓯ</LabelXSmall>
-    <Tagline>ᑕᓂᓯ</Tagline>
-    <Caption>ᑕᓂᓯ</Caption>
-    <Footnote>ᑕᓂᓯ</Footnote>
-
-    <Title dir="rtl">שלום</Title>
-    <HeadingLarge dir="rtl">שלום</HeadingLarge>
-    <HeadingMedium dir="rtl">שלום</HeadingMedium>
-    <HeadingSmall dir="rtl">שלום</HeadingSmall>
-    <HeadingXSmall dir="rtl">שלום</HeadingXSmall>
-    <BodySerifBlock dir="rtl">שלום</BodySerifBlock>
-    <BodySerif dir="rtl">שלום</BodySerif>
-    <BodyMonospace dir="rtl">שלום</BodyMonospace>
-    <Body dir="rtl">שלום</Body>
-    <LabelLarge dir="rtl">שלום</LabelLarge>
-    <LabelMedium dir="rtl">שלום</LabelMedium>
-    <LabelSmall dir="rtl">שלום</LabelSmall>
-    <LabelXSmall dir="rtl">שלום</LabelXSmall>
-    <Tagline dir="rtl">שלום</Tagline>
-    <Caption dir="rtl">שלום</Caption>
-    <Footnote dir="rtl">שלום</Footnote>
-
-    <Title>Привет</Title>
-    <HeadingLarge>Привет</HeadingLarge>
-    <HeadingMedium>Привет</HeadingMedium>
-    <HeadingSmall>Привет</HeadingSmall>
-    <HeadingXSmall>Привет</HeadingXSmall>
-    <BodySerifBlock>Привет</BodySerifBlock>
-    <BodySerif>Привет</BodySerif>
-    <BodyMonospace>Привет</BodyMonospace>
-    <Body>Привет</Body>
-    <LabelLarge>Привет</LabelLarge>
-    <LabelMedium>Привет</LabelMedium>
-    <LabelSmall>Привет</LabelSmall>
-    <LabelXSmall>Привет</LabelXSmall>
-    <Tagline>Привет</Tagline>
-    <Caption>Привет</Caption>
-    <Footnote>Привет</Footnote>
-</View>
-```
+## Lato for Latin languages
 
 ```js
 const {View} = require("@khanacademy/wonder-blocks-core");
@@ -120,4 +48,80 @@ components
 
 ```js
 <Title aria-label="Accessible Title">Title</Title>
+```
+
+## Noto for non-Latin languages
+
+```js
+const {View} = require("@khanacademy/wonder-blocks-core");
+
+<View>
+    <Title dir="rtl">مرحبا</Title>
+    <HeadingLarge dir="rtl">مرحبا</HeadingLarge>
+    <HeadingMedium dir="rtl">مرحبا</HeadingMedium>
+    <HeadingSmall dir="rtl">مرحبا</HeadingSmall>
+    <HeadingXSmall dir="rtl">مرحبا</HeadingXSmall>
+    <BodySerifBlock dir="rtl">مرحبا</BodySerifBlock>
+    <BodySerif dir="rtl">مرحبا</BodySerif>
+    <BodyMonospace dir="rtl">مرحبا</BodyMonospace>
+    <Body dir="rtl">مرحبا</Body>
+    <LabelLarge dir="rtl">مرحبا</LabelLarge>
+    <LabelMedium dir="rtl">مرحبا</LabelMedium>
+    <LabelSmall dir="rtl">مرحبا</LabelSmall>
+    <LabelXSmall dir="rtl">مرحبا</LabelXSmall>
+    <Tagline dir="rtl">مرحبا</Tagline>
+    <Caption dir="rtl">مرحبا</Caption>
+    <Footnote dir="rtl">مرحبا</Footnote>
+
+    <Title>γεια σας</Title>
+    <HeadingLarge>γεια σας</HeadingLarge>
+    <HeadingMedium>γεια σας</HeadingMedium>
+    <HeadingSmall>γεια σας</HeadingSmall>
+    <HeadingXSmall>γεια σας</HeadingXSmall>
+    <BodySerifBlock>γεια σας</BodySerifBlock>
+    <BodySerif>γεια σας</BodySerif>
+    <BodyMonospace>γεια σας</BodyMonospace>
+    <Body>γεια σας</Body>
+    <LabelLarge>γεια σας</LabelLarge>
+    <LabelMedium>γεια σας</LabelMedium>
+    <LabelSmall>γεια σας</LabelSmall>
+    <LabelXSmall>γεια σας</LabelXSmall>
+    <Tagline>γεια σας</Tagline>
+    <Caption>γεια σας</Caption>
+    <Footnote>γεια σας</Footnote>
+
+    <Title dir="rtl">שלום</Title>
+    <HeadingLarge dir="rtl">שלום</HeadingLarge>
+    <HeadingMedium dir="rtl">שלום</HeadingMedium>
+    <HeadingSmall dir="rtl">שלום</HeadingSmall>
+    <HeadingXSmall dir="rtl">שלום</HeadingXSmall>
+    <BodySerifBlock dir="rtl">שלום</BodySerifBlock>
+    <BodySerif dir="rtl">שלום</BodySerif>
+    <BodyMonospace dir="rtl">שלום</BodyMonospace>
+    <Body dir="rtl">שלום</Body>
+    <LabelLarge dir="rtl">שלום</LabelLarge>
+    <LabelMedium dir="rtl">שלום</LabelMedium>
+    <LabelSmall dir="rtl">שלום</LabelSmall>
+    <LabelXSmall dir="rtl">שלום</LabelXSmall>
+    <Tagline dir="rtl">שלום</Tagline>
+    <Caption dir="rtl">שלום</Caption>
+    <Footnote dir="rtl">שלום</Footnote>
+
+    <Title>Привет</Title>
+    <HeadingLarge>Привет</HeadingLarge>
+    <HeadingMedium>Привет</HeadingMedium>
+    <HeadingSmall>Привет</HeadingSmall>
+    <HeadingXSmall>Привет</HeadingXSmall>
+    <BodySerifBlock>Привет</BodySerifBlock>
+    <BodySerif>Привет</BodySerif>
+    <BodyMonospace>Привет</BodyMonospace>
+    <Body>Привет</Body>
+    <LabelLarge>Привет</LabelLarge>
+    <LabelMedium>Привет</LabelMedium>
+    <LabelSmall>Привет</LabelSmall>
+    <LabelXSmall>Привет</LabelXSmall>
+    <Tagline>Привет</Tagline>
+    <Caption>Привет</Caption>
+    <Footnote>Привет</Footnote>
+</View>
 ```
--- packages/wonder-blocks-typography/generated-snapshot.test.js
+++ packages/wonder-blocks-typography/generated-snapshot.test.js
@@ -29,83 +29,6 @@ describe("wonder-blocks-typography", () => {
     it("example 1", () => {
         const {View} = require("@khanacademy/wonder-blocks-core");
 
-        const example = (
-            <View>
-                <Title dir="rtl">مرحبا</Title>
-                <HeadingLarge dir="rtl">مرحبا</HeadingLarge>
-                <HeadingMedium dir="rtl">مرحبا</HeadingMedium>
-                <HeadingSmall dir="rtl">مرحبا</HeadingSmall>
-                <HeadingXSmall dir="rtl">مرحبا</HeadingXSmall>
-                <BodySerifBlock dir="rtl">مرحبا</BodySerifBlock>
-                <BodySerif dir="rtl">مرحبا</BodySerif>
-                <BodyMonospace dir="rtl">مرحبا</BodyMonospace>
-                <Body dir="rtl">مرحبا</Body>
-                <LabelLarge dir="rtl">مرحبا</LabelLarge>
-                <LabelMedium dir="rtl">مرحبا</LabelMedium>
-                <LabelSmall dir="rtl">مرحبا</LabelSmall>
-                <LabelXSmall dir="rtl">مرحبا</LabelXSmall>
-                <Tagline dir="rtl">مرحبا</Tagline>
-                <Caption dir="rtl">مرحبا</Caption>
-                <Footnote dir="rtl">مرحبا</Footnote>
-
-                <Title>ᑕᓂᓯ</Title>
-                <HeadingLarge>ᑕᓂᓯ</HeadingLarge>
-                <HeadingMedium>ᑕᓂᓯ</HeadingMedium>
-                <HeadingSmall>ᑕᓂᓯ</HeadingSmall>
-                <HeadingXSmall>ᑕᓂᓯ</HeadingXSmall>
-                <BodySerifBlock>ᑕᓂᓯ</BodySerifBlock>
-                <BodySerif>ᑕᓂᓯ</BodySerif>
-                <BodyMonospace>ᑕᓂᓯ</BodyMonospace>
-                <Body>ᑕᓂᓯ</Body>
-                <LabelLarge>ᑕᓂᓯ</LabelLarge>
-                <LabelMedium>ᑕᓂᓯ</LabelMedium>
-                <LabelSmall>ᑕᓂᓯ</LabelSmall>
-                <LabelXSmall>ᑕᓂᓯ</LabelXSmall>
-                <Tagline>ᑕᓂᓯ</Tagline>
-                <Caption>ᑕᓂᓯ</Caption>
-                <Footnote>ᑕᓂᓯ</Footnote>
-
-                <Title dir="rtl">שלום</Title>
-                <HeadingLarge dir="rtl">שלום</HeadingLarge>
-                <HeadingMedium dir="rtl">שלום</HeadingMedium>
-                <HeadingSmall dir="rtl">שלום</HeadingSmall>
-                <HeadingXSmall dir="rtl">שלום</HeadingXSmall>
-                <BodySerifBlock dir="rtl">שלום</BodySerifBlock>
-                <BodySerif dir="rtl">שלום</BodySerif>
-                <BodyMonospace dir="rtl">שלום</BodyMonospace>
-                <Body dir="rtl">שלום</Body>
-                <LabelLarge dir="rtl">שלום</LabelLarge>
-                <LabelMedium dir="rtl">שלום</LabelMedium>
-                <LabelSmall dir="rtl">שלום</LabelSmall>
-                <LabelXSmall dir="rtl">שלום</LabelXSmall>
-                <Tagline dir="rtl">שלום</Tagline>
-                <Caption dir="rtl">שלום</Caption>
-                <Footnote dir="rtl">שלום</Footnote>
-
-                <Title>Привет</Title>
-                <HeadingLarge>Привет</HeadingLarge>
-                <HeadingMedium>Привет</HeadingMedium>
-                <HeadingSmall>Привет</HeadingSmall>
-                <HeadingXSmall>Привет</HeadingXSmall>
-                <BodySerifBlock>Привет</BodySerifBlock>
-                <BodySerif>Привет</BodySerif>
-                <BodyMonospace>Привет</BodyMonospace>
-                <Body>Привет</Body>
-                <LabelLarge>Привет</LabelLarge>
-                <LabelMedium>Привет</LabelMedium>
-                <LabelSmall>Привет</LabelSmall>
-                <LabelXSmall>Привет</LabelXSmall>
-                <Tagline>Привет</Tagline>
-                <Caption>Привет</Caption>
-                <Footnote>Привет</Footnote>
-            </View>
-        );
-        const tree = renderer.create(example).toJSON();
-        expect(tree).toMatchSnapshot();
-    });
-    it("example 2", () => {
-        const {View} = require("@khanacademy/wonder-blocks-core");
-
         // NOTE(mdr): I added an `id` attribute to each of these tags, to ensure that
         //     they all pass the `id` attribute correctly. This fact will be saved in
         //     snapshot tests.
@@ -144,7 +67,7 @@ describe("wonder-blocks-typography", () => {
         const tree = renderer.create(example).toJSON();
         expect(tree).toMatchSnapshot();
     });
-    it("example 3", () => {
+    it("example 2", () => {
         const Color = require("@khanacademy/wonder-blocks-color").default;
         const {StyleSheet} = require("aphrodite");
 
@@ -158,8 +81,85 @@ describe("wonder-blocks-typography", () => {
         const tree = renderer.create(example).toJSON();
         expect(tree).toMatchSnapshot();
     });
-    it("example 4", () => {
+    it("example 3", () => {
         const example = <Title aria-label="Accessible Title">Title</Title>;
+        const tree = renderer.create(example).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+    it("example 4", () => {
+        const {View} = require("@khanacademy/wonder-blocks-core");
+
+        const example = (
+            <View>
+                <Title dir="rtl">مرحبا</Title>
+                <HeadingLarge dir="rtl">مرحبا</HeadingLarge>
+                <HeadingMedium dir="rtl">مرحبا</HeadingMedium>
+                <HeadingSmall dir="rtl">مرحبا</HeadingSmall>
+                <HeadingXSmall dir="rtl">مرحبا</HeadingXSmall>
+                <BodySerifBlock dir="rtl">مرحبا</BodySerifBlock>
+                <BodySerif dir="rtl">مرحبا</BodySerif>
+                <BodyMonospace dir="rtl">مرحبا</BodyMonospace>
+                <Body dir="rtl">مرحبا</Body>
+                <LabelLarge dir="rtl">مرحبا</LabelLarge>
+                <LabelMedium dir="rtl">مرحبا</LabelMedium>
+                <LabelSmall dir="rtl">مرحبا</LabelSmall>
+                <LabelXSmall dir="rtl">مرحبا</LabelXSmall>
+                <Tagline dir="rtl">مرحبا</Tagline>
+                <Caption dir="rtl">مرحبا</Caption>
+                <Footnote dir="rtl">مرحبا</Footnote>
+
+                <Title>γεια σας</Title>
+                <HeadingLarge>γεια σας</HeadingLarge>
+                <HeadingMedium>γεια σας</HeadingMedium>
+                <HeadingSmall>γεια σας</HeadingSmall>
+                <HeadingXSmall>γεια σας</HeadingXSmall>
+                <BodySerifBlock>γεια σας</BodySerifBlock>
+                <BodySerif>γεια σας</BodySerif>
+                <BodyMonospace>γεια σας</BodyMonospace>
+                <Body>γεια σας</Body>
+                <LabelLarge>γεια σας</LabelLarge>
+                <LabelMedium>γεια σας</LabelMedium>
+                <LabelSmall>γεια σας</LabelSmall>
+                <LabelXSmall>γεια σας</LabelXSmall>
+                <Tagline>γεια σας</Tagline>
+                <Caption>γεια σας</Caption>
+                <Footnote>γεια σας</Footnote>
+
+                <Title dir="rtl">שלום</Title>
+                <HeadingLarge dir="rtl">שלום</HeadingLarge>
+                <HeadingMedium dir="rtl">שלום</HeadingMedium>
+                <HeadingSmall dir="rtl">שלום</HeadingSmall>
+                <HeadingXSmall dir="rtl">שלום</HeadingXSmall>
+                <BodySerifBlock dir="rtl">שלום</BodySerifBlock>
+                <BodySerif dir="rtl">שלום</BodySerif>
+                <BodyMonospace dir="rtl">שלום</BodyMonospace>
+                <Body dir="rtl">שלום</Body>
+                <LabelLarge dir="rtl">שלום</LabelLarge>
+                <LabelMedium dir="rtl">שלום</LabelMedium>
+                <LabelSmall dir="rtl">שלום</LabelSmall>
+                <LabelXSmall dir="rtl">שלום</LabelXSmall>
+                <Tagline dir="rtl">שלום</Tagline>
+                <Caption dir="rtl">שלום</Caption>
+                <Footnote dir="rtl">שלום</Footnote>
+
+                <Title>Привет</Title>
+                <HeadingLarge>Привет</HeadingLarge>
+                <HeadingMedium>Привет</HeadingMedium>
+                <HeadingSmall>Привет</HeadingSmall>
+                <HeadingXSmall>Привет</HeadingXSmall>
+                <BodySerifBlock>Привет</BodySerifBlock>
+                <BodySerif>Привет</BodySerif>
+                <BodyMonospace>Привет</BodyMonospace>
+                <Body>Привет</Body>
+                <LabelLarge>Привет</LabelLarge>
+                <LabelMedium>Привет</LabelMedium>
+                <LabelSmall>Привет</LabelSmall>
+                <LabelXSmall>Привет</LabelXSmall>
+                <Tagline>Привет</Tagline>
+                <Caption>Привет</Caption>
+                <Footnote>Привет</Footnote>
+            </View>
+        );
         const tree = renderer.create(example).toJSON();
         expect(tree).toMatchSnapshot();
     });
