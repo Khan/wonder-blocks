4a38510c4e14b55b1c32f9ce3253d3dce089085c
Juan David Andrade <juan@khanacademy.org>
2019-05-09 10:26:14 -0400

    WB-553: Implement Breadcrumb v1 (#391)
    * WB-553 Implement Breadcrumb v1
    
    * WB-553 Implement PR feedback
    
    * WB-553 Revert aphrodite.flow changes
    
    * WB-553 Implement new PR feedback

--- styleguide.config.js
+++ styleguide.config.js
@@ -14,6 +14,14 @@ module.exports = {
                 "This section only appears to developers. If you want to include content that only appears in the development server, add `private: true` to the section configuration in `styleguide.config.js`.",
         },
         {
+            name: "Breadcrumbs",
+            content: "packages/wonder-blocks-breadcrumbs/docs.md",
+            components: [
+                "packages/wonder-blocks-breadcrumbs/components/breadcrumbs.js",
+                "packages/wonder-blocks-breadcrumbs/components/breadcrumbs-item.js",
+            ],
+        },
+        {
             name: "Button",
             content: "packages/wonder-blocks-button/docs.md",
             components: "packages/wonder-blocks-button/components/button.js",
