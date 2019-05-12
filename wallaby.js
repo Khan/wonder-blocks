a2f3e2a501bb063602729d75ea4d892f71d4fb72
Kevin Barabash <kevinbarabash@khanacademy.org>
2019-05-11 20:47:03 -0400

    clean up wallaby.js and add comments

--- wallaby.js
+++ wallaby.js
@@ -1,64 +1,88 @@
 const babelConfig = require("./build-settings/babel.config.js");
 
-module.exports = function (wallaby) {
+module.exports = function(wallaby) {
+    const tests = [
+        "packages/wonder-blocks-*/**/*.test.js",
+        "shared-unpackaged/**/*.test.js",
+    ];
+
     return {
+        tests,
+
+        // Wallaby needs to know about all files that may be loaded because
+        // of running a test.
         files: [
-            'packages/wonder-blocks-*/**/*.js',
-            'package.json', // <--
-            { pattern: 'config/jest/*.js', instrument: false },
-            'shared-unpackaged/**/*.js',
-            'utils/**/*.js',
-            'packages/wonder-blocks-*/package.json',
-            { pattern: 'packages/wonder-blocks-*/dist/**/*.js', instrument: false },
-            '!packages/wonder-blocks-*/**/*.test.js',
-            '!packages/wonder-blocks-*/node_modules/**',
-            '!shared-unpackaged/**/*.test.js',
+            "packages/wonder-blocks-*/**/*.js",
+            "shared-unpackaged/**/*.js",
+            "utils/**/*.js",
+            {pattern: "config/jest/*.js", instrument: false},
+
+            // These paths are excluded.
+            "!shared-unpackaged/**/*.test.js",
+            ...tests.map((test) => `!${test}`),
         ],
 
         filesWithNoCoverageCalculated: [
-            'config/jest/*.js',
-            'packages/wonder-blocks-*/dist/**/*.js',
-            'utils/**/*.js',
+            "packages/wonder-blocks-*/dist/**/*.js",
+            "config/jest/*.js",
+            "utils/**/*.js", // we ignore these files in .codecov.yml as well
         ],
 
-        tests: [
-            'packages/wonder-blocks-*/**/*.test.js',
-            'shared-unpackaged/**/*.test.js',
-        ],
-
+        // Wallaby does its own compilation of .js files to support its
+        // advanced logging features.  Wallaby can't parse the flow and
+        // JSX in our source files so need to provide a babel configuration.
         compilers: {
-            "packages/wonder-blocks-*/**/*.js": wallaby.compilers.babel(babelConfig),
+            "packages/wonder-blocks-*/**/*.js": wallaby.compilers.babel(
+                babelConfig,
+            ),
             "shared-unpackaged/**/*.js": wallaby.compilers.babel(babelConfig),
             "utils/**/*.js": wallaby.compilers.babel(babelConfig),
         },
 
         env: {
-            type: 'node',
-            runner: 'node',
-            params: {
-                runner: "--max_old_space_size=4096"
-            },
+            type: "node",
+            runner: "node",
         },
 
-        testFramework: 'jest',
+        testFramework: "jest",
 
-        setup: function (wallaby) {
+        setup: function(wallaby) {
+            // We require "path" here because this setup function is run
+            // in a different context and does not have access to variables
+            // from its closure.
             const path = require("path");
-            const jestConfigPath = path.join(
+
+            const jestConfig = require(path.join(
                 wallaby.localProjectDir,
                 "./config/jest/test.config.js",
-            );
-            const jestConfig = require(jestConfigPath);
+            ));
+
+            // Prevent our jest configuration from re-compiling the files
+            // since wallaby has already done that.
             delete jestConfig.transform;
+
+            // Map @khanacademy/wonder-blocks-foo to /packages/wonder-blocks-foo
+            // so that we load the source code for dependencies when running tests.
             jestConfig.moduleNameMapper = {
                 "^@khanacademy/(.*)$":
-                    wallaby.projectCacheDir + "/packages/$1",
+                    wallaby.projectCacheDir + "/packages/$1/index.js",
             };
-            // for example:
-            // jestConfig.globals = { "__DEV__": true };
+
             wallaby.testFramework.configure(jestConfig);
         },
 
+        // Uncomment to only run tests for files that have been changed.
+        // Coverage reports on http://wallabyjs.com/app will be incomplete
+        // but initial start will be faster.
+        // automaticTestFileSelection: false,
+
+        // Uncomment to only run tests/suites using .only().
+        // Coverage reports on http://wallabyjs.com/app will be incomplete
+        // but initial start will be faster.
+        // runSelectedTestsOnly: true,
+
+        // Uncomment to get additional debug information in the wallaby
+        // console when running testts.
         // debug: true,
     };
 };
