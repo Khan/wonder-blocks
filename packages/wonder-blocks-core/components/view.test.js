09249e672a84f14dea328a82d758177d98fe87d4
Kumail Naqvi <kumail@khanacademy.org>
2019-06-06 15:49:35 -0700

    WB-529: Update view.test.js to test all possible tags

--- packages/wonder-blocks-core/components/view.test.js
+++ packages/wonder-blocks-core/components/view.test.js
@@ -12,4 +12,49 @@ describe("View", () => {
         // Assert
         expect(tree).toMatchSnapshot();
     });
+
+    it("Should set the tag to be section", () => {
+        // Arrage, Act
+        const view = <View tag="section" />;
+        const tree = renderer.create(view).toJSON();
+
+        // Assert
+        expect(tree).toMatchSnapshot();
+    });
+
+    it("Should set the tag to be article", () => {
+        // Arrage, Act
+        const view = <View tag="article" />;
+        const tree = renderer.create(view).toJSON();
+
+        // Assert
+        expect(tree).toMatchSnapshot();
+    });
+
+    it("Should set the tag to be aside", () => {
+        // Arrage, Act
+        const view = <View tag="aside" />;
+        const tree = renderer.create(view).toJSON();
+
+        // Assert
+        expect(tree).toMatchSnapshot();
+    });
+
+    it("Should set the tag to be nav", () => {
+        // Arrage, Act
+        const view = <View tag="nav" />;
+        const tree = renderer.create(view).toJSON();
+
+        // Assert
+        expect(tree).toMatchSnapshot();
+    });
+
+    it("Should set the tag to be div", () => {
+        // Arrage, Act
+        const view = <View />;
+        const tree = renderer.create(view).toJSON();
+
+        // Assert
+        expect(tree).toMatchSnapshot();
+    });
 });
