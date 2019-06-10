0a33be2857978edd43bf2ffe24e1ceb1d21aecf0
Jeff Yates <jeff@somewhatabstract.com>
2019-06-10 18:08:36 -0500

    Added test for withActionScheduler

--- packages/wonder-blocks-timing/components/with-action-scheduler.test.js
+++ packages/wonder-blocks-timing/components/with-action-scheduler.test.js
@@ -1,5 +1,25 @@
 // @flow
+import * as React from "react";
+
+import {mount, unmountAll} from "../../../utils/testing/mount.js";
+
+import withActionScheduler from "./with-action-scheduler.js";
 
 describe("withActionScheduler", () => {
-    it.skip("test this thing", () => {});
+    afterEach(() => {
+        unmountAll();
+    });
+
+    it("should provide wrapped component with IScheduleActions instance", () => {
+        // Arrange
+        const Component = (props) =>
+            props.schedule != null ? "true" : "false";
+
+        // Act
+        const WithScheduler = withActionScheduler(Component);
+        const wrapper = mount(<WithScheduler />);
+
+        // Assert
+        expect(wrapper.text()).toBe("true");
+    });
 });
