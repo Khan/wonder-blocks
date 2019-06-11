fd20787fd82ace24148fd75aae7d56dbf04d6ee4
Kumail Naqvi <kumail@khanacademy.org>
2019-06-11 15:53:52 -0700

    add coverage tests

--- packages/wonder-blocks-tooltip/components/tooltip.integration.test.js
+++ packages/wonder-blocks-tooltip/components/tooltip.integration.test.js
@@ -23,12 +23,10 @@ describe("tooltip integration tests", () => {
         const bubbleWrapper = wrapper.find(TooltipBubble);
         expect(bubbleWrapper.length).toEqual(1);
 
-        bubbleWrapper.simulate("mouseenter");
-
         expect(wrapper).toHaveState("timeoutID", null);
     });
 
-    it("should set a timeout on mouseleave", () => {
+    it("should set a timeout on mouseleave on TooltipAnchor", () => {
         const wrapper = mount(
             <Tooltip content="hello, world">an anchor</Tooltip>,
         );
@@ -41,17 +39,15 @@ describe("tooltip integration tests", () => {
         const anchorWrapper = wrapper.find(TooltipAnchor);
         expect(anchorWrapper.length).toEqual(1);
 
-        const anchor = anchorWrapper.getDOMNode();
-        // start hovering
-        anchor && anchor.dispatchEvent(new FocusEvent("mouseenter"));
-        anchor && anchor.dispatchEvent(new FocusEvent("mouseleave"));
+        const anchorInstance = anchorWrapper.instance();
+        anchorInstance._setActiveState(true, false);
 
-        jest.advanceTimersByTime(TooltipDisappearanceDelay / 2);
+        anchorWrapper.simulate("mouseleave");
         expect(wrapper.state("timeoutID")).toEqual(expect.any(Number));
         expect(wrapper.state("active")).toEqual(true);
     });
 
-    it("should eventually disable the TooltipBubble after mouseleave", () => {
+    it("should disable the timeout if the mouse hovers over TooltipAnchor within the TooltipDisappearanceDelay", () => {
         const wrapper = mount(
             <Tooltip content="hello, world">an anchor</Tooltip>,
         );
@@ -64,40 +60,39 @@ describe("tooltip integration tests", () => {
         const anchorWrapper = wrapper.find(TooltipAnchor);
         expect(anchorWrapper.length).toEqual(1);
 
-        const anchor = anchorWrapper.getDOMNode();
-        // start hovering
-        anchor && anchor.dispatchEvent(new FocusEvent("mouseenter"));
-        anchor && anchor.dispatchEvent(new FocusEvent("mouseleave"));
-        expect(wrapper.state("timeoutID")).toEqual(expect.any(Number));
+        const anchorInstance = anchorWrapper.instance();
+        anchorInstance._setActiveState(true, false);
 
-        jest.advanceTimersByTime(TooltipDisappearanceDelay + 10);
+        anchorWrapper.simulate("mouseleave");
+
+        jest.advanceTimersByTime(TooltipDisappearanceDelay / 2);
+
+        bubbleWrapper.simulate("mouseenter");
         expect(wrapper.state("timeoutID")).toEqual(null);
+        expect(wrapper.state("active")).toEqual(true);
+    });
+
+    it("should close TooltipBubble on mouseleave on TooltipBubble", () => {
+        const wrapper = mount(
+            <Tooltip content="hello, world">an anchor</Tooltip>,
+        );
+
+        wrapper.setState({active: true});
+
+        const bubbleWrapper = wrapper.find(TooltipBubble);
+        expect(bubbleWrapper.length).toEqual(1);
+
+        const anchorWrapper = wrapper.find(TooltipAnchor);
+        expect(anchorWrapper.length).toEqual(1);
+
+        const anchorInstance = anchorWrapper.instance();
+        anchorInstance._setActiveState(true, false);
+
+        anchorWrapper.simulate("mouseleave");
+        bubbleWrapper.simulate("mouseenter");
+        expect(wrapper.state("timeoutID")).toEqual(null);
+
+        bubbleWrapper.simulate("mouseleave");
         expect(wrapper.state("active")).toEqual(false);
-    });
-
-    it("should disable the timeout if the mouse hovers over anchor within the TooltipDisappearanceDelay", () => {
-        const wrapper = mount(
-            <Tooltip content="hello, world">an anchor</Tooltip>,
-        );
-
-        wrapper.setState({active: true});
-
-        const bubbleWrapper = wrapper.find(TooltipBubble);
-        expect(bubbleWrapper.length).toEqual(1);
-
-        const anchorWrapper = wrapper.find(TooltipAnchor);
-        expect(anchorWrapper.length).toEqual(1);
-
-        const anchor = anchorWrapper.getDOMNode();
-        // start hovering
-        anchor && anchor.dispatchEvent(new FocusEvent("mouseenter"));
-        anchor && anchor.dispatchEvent(new FocusEvent("mouseleave"));
-        expect(wrapper.state("timeoutID")).toEqual(expect.any(Number));
-
-        jest.advanceTimersByTime(TooltipDisappearanceDelay / 2);
-        anchor && anchor.dispatchEvent(new FocusEvent("mouseenter"));
-
-        expect(wrapper.state("timeoutID")).toEqual(null);
-        expect(wrapper.state("active")).toEqual(true);
     });
 });
