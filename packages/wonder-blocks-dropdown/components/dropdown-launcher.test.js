82b30c5d35d0243c77657956e8a27de78cba4a6a
Kumail Naqvi <kumail@khanacademy.org>
2019-06-14 13:21:31 -0700

    Add keyboard navigation tests for DropdownLauncher

--- packages/wonder-blocks-dropdown/components/dropdown-launcher.test.js
+++ packages/wonder-blocks-dropdown/components/dropdown-launcher.test.js
@@ -1,13 +1,10 @@
-/* eslint-disable no-console */
 //@flow
 import * as React from "react";
-// import * as ReactDOM from "react-dom";
 import IconButton from "@khanacademy/wonder-blocks-icon-button";
 import {icons} from "@khanacademy/wonder-blocks-icon";
 import {mount, unmountAll} from "../../../utils/testing/mount.js";
 import ActionItem from "./action-item.js";
 import SeparatorItem from "./separator-item.js";
-// import OptionItem from "./option-item.js";
 import Dropdown from "./dropdown.js";
 import DropdownLauncher from "./dropdown-launcher.js";
 import {keyCodes} from "../util/constants.js";
@@ -37,13 +34,13 @@ describe("Dropdown Launcher", () => {
                     />,
                     <ActionItem
                         label="Settings (onClick)"
-                        onClick={() => console.log("user clicked on settings")}
+                        onClick={() => {}}
                         testId="settings"
                     />,
                     <ActionItem
                         label="Help"
                         disabled={true}
-                        onClick={() => console.log("this item is disabled...")}
+                        onClick={() => {}}
                         testId="help"
                     />,
                     <ActionItem
@@ -60,10 +57,10 @@ describe("Dropdown Launcher", () => {
                     />,
                 ]}
             >
-                {(openDropdown) => (
+                {(handleDropdown) => (
                     <IconButton
                         // eslint-disable-next-line react/jsx-handler-names
-                        onClick={openDropdown}
+                        onClick={handleDropdown}
                         icon={icons.search}
                         aria-label="search"
                     />
@@ -93,10 +90,11 @@ describe("Dropdown Launcher", () => {
         anchor.simulate("click");
         const dropdown = dropdownLauncher.find(Dropdown);
 
-        // Act
+        // Act & Assert
+        dropdown.simulate("keydown", {keyCode: keyCodes.down});
+        dropdown.simulate("keyup", {keyCode: keyCodes.down});
         expect(dropdown.instance().focusedIndex).toBe(0);
 
-        // navigate down three times
         dropdown.simulate("keydown", {keyCode: keyCodes.down});
         dropdown.simulate("keyup", {keyCode: keyCodes.down});
         expect(dropdown.instance().focusedIndex).toBe(1);
@@ -104,5 +102,58 @@ describe("Dropdown Launcher", () => {
         dropdown.simulate("keydown", {keyCode: keyCodes.down});
         dropdown.simulate("keyup", {keyCode: keyCodes.down});
         expect(dropdown.instance().focusedIndex).toBe(2);
+    });
+
+    it("closes itself on escape", () => {
+        // Arrange
+        const anchor = dropdownLauncher.find(IconButton);
+        anchor.simulate("click");
+        const dropdown = dropdownLauncher.find(Dropdown);
+
+        // Act
+        dropdown.simulate("keydown", {keyCode: keyCodes.escape});
+        dropdown.simulate("keyup", {keyCode: keyCodes.escape});
+
+        // Assert
+        expect(dropdownLauncher.state("open")).toBe(false);
+    });
+
+    it("closes itself on tab", () => {
+        // Arrange
+        const anchor = dropdownLauncher.find(IconButton);
+        anchor.simulate("click");
+        const dropdown = dropdownLauncher.find(Dropdown);
+
+        // Act
+        dropdown.simulate("keydown", {keyCode: keyCodes.tab});
+        dropdown.simulate("keyup", {keyCode: keyCodes.tab});
+
+        // Assert
+        expect(dropdownLauncher.state("open")).toBe(false);
+    });
+
+    it("closes itself on an external mouse click", () => {
+        // Arrange
+        const anchor = dropdownLauncher.find(IconButton);
+        anchor.simulate("click");
+
+        // Act
+        document.dispatchEvent(new MouseEvent("mouseup"));
+
+        // Assert
+        expect(dropdownLauncher.state("open")).toBe(false);
+    });
+
+    it("open on down key when focused", () => {
+        // Arrange
+        const anchor = dropdownLauncher.find(IconButton);
+        anchor.simulate("click");
+
+        // Act
+        anchor.simulate("keydown", {keyCode: keyCodes.down});
+        anchor.simulate("keyup", {keyCode: keyCodes.down});
+
+        // Assert
+        expect(dropdownLauncher.state("open")).toBe(true);
     });
 });
