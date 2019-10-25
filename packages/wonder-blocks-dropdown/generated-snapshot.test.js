a55ee576d6e5dd93e4c7630393c7bf10b377951e
Juan Andrade <juan@khanacademy.org>
2019-10-25 15:44:01 -0400

    WB-615 feat(dropdown): Add example to MultiSelect

--- packages/wonder-blocks-dropdown/generated-snapshot.test.js
+++ packages/wonder-blocks-dropdown/generated-snapshot.test.js
@@ -1356,8 +1356,18 @@ describe("wonder-blocks-dropdown", () => {
                     aria-labelledby="label-for-multi-select"
                     id="unique-multi-select"
                     placeholder="Accessible MultiSelect"
+                    selectedValues={["one"]}
                 >
-                    <OptionItem label="some value" value="" />
+                    <OptionItem
+                        label="First element"
+                        aria-label="First element, selected"
+                        value="one"
+                    />
+                    <OptionItem
+                        label="Second element"
+                        aria-label="Second element, unselelected"
+                        value="two"
+                    />
                 </MultiSelect>
             </View>
         );
