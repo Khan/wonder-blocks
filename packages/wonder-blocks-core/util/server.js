0fa0bf66ba6af7a60d2f2c4c95c93a04a3d09ce9
Jeff Yates <jeff@somewhatabstract.com>
2019-12-01 14:42:03 -0600

    Implement Server API

---
+++ packages/wonder-blocks-core/util/server.js
@@ -0,0 +1,19 @@
+// @flow
+let serverSide = false;
+
+export default Object.freeze({
+    /**
+     * Check if we are running in server-side mode.
+     *
+     * @returns {boolean} `true` if we are in server-side mode; otherwise,
+     * `false`
+     */
+    isServerSide: () => serverSide,
+
+    /**
+     * Set server-side mode to true.
+     */
+    setServerSide: () => {
+        serverSide = true;
+    },
+});
