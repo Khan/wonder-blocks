34bfc4d340ee6674a43c2269a25664e0f83637a7
Jeff Yates <jeff@khanacademy.org>
2019-12-06 18:02:44 -0600

    WEB-1629.5 Add response cache (#621)
    * Add response cache
    
    * Add TODO for docs
    
    * Fix typo
    
    * Add docs for initializeCache
    
    * Fulfil -> Fulfill (UK -> US)
    
    * Fulfil -> Fulfill (UK -> US)
    
    * Support errors in the response cache
    
    * Refactor for simpler testing and reasoning
    
    * Refactor for easier testing and legibility
    
    * Implement cloning, improve how default cache is setup
    
    * Fulfil -> fulfill (US->UK) and implementing basic support for tracking in-flight requests
    
    * Implemented tests
    
    * Remove cache behavior in favour of cache invalidation
    
    * Remove some redundant code
    
    * Drop the array for options since we're not doing refreshes
    
    * Add cache invalidation support to getEntry
    
    * Remove the freeze; it complicates testing for dependents
    
    * Add test for cache invalidation
    
    * Verify the delete actually deletes

--- packages/wonder-blocks-data/docs.md
+++ packages/wonder-blocks-data/docs.md
@@ -1,1 +1,60 @@
-In progress!
+## initializeCache
+
+Wonder Blocks Data caches data in its response cache. This cache can be
+initialized with data using the `initializeCache` method. The `initializeData`
+method can only be called once to populate the cache. Usually, the data to be
+passed to `initializeCache` will be obtained by calling `fulfillAllDataRequests`
+after tracking data requests (see [TrackData](#trackdata)) during server-side
+rendering.
+
+This method can also be used to initialize the response cache for tests.
+
+### Usage
+
+```js static
+initializeCache(sourceCache: ResponseCache): void;
+```
+
+#### Function arguments
+
+| Argument | Flow&nbsp;Type | Default | Description |
+| --- | --- | --- | --- |
+| `sourceData` | `ResponseCache` | _Required_ | The source cache that will be used to initialize the response cache. |
+
+### Types
+
+#### ResponseCache
+
+```js static
+type HandlerSubcache = {
+    [key: string]: any,
+    ...,
+};
+
+type ResponseCache = {
+    [handlerType: string]: HandlerSubcache,
+    ...,
+};
+```
+
+A response cache is divided into subcaches by handler type. An example is
+shown below.
+
+```js static
+const responseCache = {
+    HANDLER_TYPE_A: {
+        DATA_ID_1: "DATA STRING",
+        DATA_ID_2: ["array", "of", "data"],
+    },
+    HANDLER_TYPE_B: {
+        DATA_ID_3: {
+            some: "data",
+        },
+    },
+};
+```
+
+In this example, the cache contains data retrieved by two handlers; one with
+type `HANDLER_TYPE_A` and one with type `HANDLER_TYPE_B`. Within each subcache,
+the data responses are keyed by the returned value of the respective [request
+handler](#requesthandler)'s `getKey` method for the relevant request.
---
+++ packages/wonder-blocks-data/util/response-cache.js
@@ -0,0 +1,149 @@
+// @flow
+import type {
+    CacheEntry,
+    ResponseCache as Cache,
+    IRequestHandler,
+} from "./types.js";
+
+function deepClone<T: {...}>(source: T | $ReadOnly<T>): $ReadOnly<T> {
+    /**
+     * We want to deep clone the source cache to dodge mutations by external
+     * references. So we serialize the source cache to JSON and parse it
+     * back into a new object.
+     */
+    const serializedInitCache = JSON.stringify(source);
+    const cloneInitCache = JSON.parse(serializedInitCache);
+    return Object.freeze(cloneInitCache);
+}
+
+/**
+ * The default instance is stored here.
+ * It's created below in the Default() static property.
+ */
+let _default: ResponseCache;
+
+/**
+ * Implements the response cache.
+ *
+ * INTERNAL USE ONLY
+ */
+export class ResponseCache {
+    static get Default() {
+        if (!_default) {
+            _default = new ResponseCache();
+        }
+        return _default;
+    }
+
+    _cache: Cache = {};
+
+    constructor(source?: ?Cache = undefined) {
+        if (source != null) {
+            this.initialize(source);
+        }
+    }
+
+    _setCacheEntry<TOptions, TData>(
+        handler: IRequestHandler<TOptions, TData>,
+        options: TOptions,
+        entry: CacheEntry<TData>,
+    ): CacheEntry<TData> {
+        const requestType = handler.type;
+
+        // Ensure we have a cache location for this handler type.
+        this._cache[requestType] = this._cache[requestType] || {};
+
+        // Cache the data.
+        const key = handler.getKey(options);
+        this._cache[requestType][key] = Object.freeze(entry);
+        return this._cache[requestType][key];
+    }
+
+    /**
+     * Initialize the cache from a given cache state.
+     *
+     * This can only be called if the cache is not already in use.
+     */
+    initialize = (source: Cache): void => {
+        if (Object.keys(this._cache).length !== 0) {
+            throw new Error(
+                "Cannot initialize data response cache more than once",
+            );
+        }
+
+        try {
+            /**
+             * Object.assign only performs a shallow clone.
+             * So we deep clone it and then assign the clone values to our
+             * internal cache.
+             */
+            const cloneInitCache = deepClone(source);
+            Object.assign(this._cache, cloneInitCache);
+        } catch (e) {
+            throw new Error(
+                `An error occurred trying to initialize the data response cache: ${e}`,
+            );
+        }
+    };
+
+    /**
+     * Cache data for a specific response.
+     */
+    cacheData = <TOptions, TData>(
+        handler: IRequestHandler<TOptions, TData>,
+        options: TOptions,
+        data: TData,
+    ): CacheEntry<TData> => {
+        return this._setCacheEntry(handler, options, {data});
+    };
+
+    /**
+     * Cache an error for a specific response.
+     */
+    cacheError = <TOptions, TData>(
+        handler: IRequestHandler<TOptions, TData>,
+        options: TOptions,
+        error: Error | string,
+    ): CacheEntry<TData> => {
+        const errorMessage = typeof error === "string" ? error : error.message;
+        return this._setCacheEntry(handler, options, {error: errorMessage});
+    };
+
+    /**
+     * Retrieve data from our cache.
+     */
+    getEntry = <TOptions, TData>(
+        handler: IRequestHandler<TOptions, TData>,
+        options: TOptions,
+    ): ?$ReadOnly<CacheEntry<TData>> => {
+        const requestType = handler.type;
+
+        // Get the subcache for the handler.
+        const handlerCache = this._cache[requestType];
+        if (!handlerCache) {
+            return null;
+        }
+
+        const key = handler.getKey(options);
+        if (handler.invalidateCache(options)) {
+            delete handlerCache[key];
+        }
+
+        // Get the response.
+        const entry = handlerCache[key];
+        return entry == null ? null : entry;
+    };
+
+    /**
+     * Deep clone the cache.
+     */
+    clone = (): $ReadOnly<Cache> => {
+        try {
+            return deepClone(this._cache);
+        } catch (e) {
+            throw new Error(
+                `An error occurred while trying to clone the cache: ${e}`,
+            );
+        }
+    };
+}
