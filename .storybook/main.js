9e9f1c01d7662c1a139b13809e931dc08640436e
Kevin Barabash <kevinbarabash@khanacademy.org>
2020-01-21 18:47:41 -0500

    Add actions, knobs, and jsx storybook addons (#719)
    * Add actions, knobs, and jsx storybook addons
    
    * exclude the 'With Knobs' story from chromatic screenshots
    
    * Fix issue with buttonsWithKnobs vs withKnobs
    
    * move common addon config to global ./storybook/preview.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,10 @@
 module.exports = {
     stories: ["../packages/**/*.stories.js"],
     addons: [
+        "@storybook/addon-actions/register",
         "@storybook/addon-backgrounds/register",
+        "@storybook/addon-knobs/register",
         "@storybook/addon-viewport/register",
+        "storybook-addon-jsx/register",
     ],
 };
