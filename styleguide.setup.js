5a61cb76acbaba90015095fdc1329a68724d1736
Kevin Barabash <kevinb@khanacademy.org>
2020-10-09 16:38:06 -0400

    WEB-2990.?: Use promises instead of async/await (#1055)
    ## Summary:
    Even though we're using async/await in some places in webapp, the code we generator
    here doesn't see webapp's version of regeneratorRuntime.  This PR changes the use
    of async/await in clickable-behavior.js to Promises instead.
    
    This PR also removes the changes I made to the styleguidist config to drop the import
    of @babel/polyfill which was providing regeneratorRuntime locally.
    
    Issue: WEB-2990
    
    ## Test plan:
    - yarn start
    - load localhost:6060 and see that it loads fine without regeneratorRuntime errors
    - yarn test
    
    Reviewers: #fe-infra
    
    Author: kevinbarabash
    
    Reviewers: jeresig
    
    Required Reviewers: 
    
    Approved by: jeresig
    
    Checks: ✅ Mixed content, ❓ Redirect rules, ❓ Header rules, ⏭  Auto-approve dependabot PRs, ❓ Pages changed, ✅ chromatic
    
    Pull request URL: https://github.com/Khan/wonder-blocks/pull/1055

--- styleguide.setup.js
+++
@@ -1,7 +0,0 @@
-/**
- * Styleguidist doesn't include regeneratorRuntime by default so in order for
- * code using async functions to work we need to tell styleguidist to import
- * the babel polyfill before any other code.
- */
-// eslint-disable-next-line import/no-unassigned-import
-import "@babel/polyfill";
