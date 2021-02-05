bbfe6da321c16b4d60b3ebce2938c6692a886f0e
Kevin Barabash <kevinbarabash@khanacademy.org>
2021-02-05 16:49:43 -0500

    fix another typo

--- packages/wonder-blocks-clickable/components/clickable-behavior.js
+++ packages/wonder-blocks-clickable/components/clickable-behavior.js
@@ -170,7 +170,7 @@ type Props =
            * first and safeWithNav will only be run if beforeNav does not reject.
            *
            * WARNING: Using this with `target="_blank"` will trigger built-in popup
-           * blockers in Firefox and Safari.  This is becuase we do navigation
+           * blockers in Firefox and Safari.  This is because we do navigation
            * programmatically and `beforeNav` causes a delay which means that the
            * browser can't make a directly link between a user action and the
            * navigation.
