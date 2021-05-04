3af4560f479ccc4f89006e1fb6e68db8301ff904
Kevin Barabash <kevinbarabash@khanacademy.org>
2021-05-04 12:11:47 -0400

    Handle mailto:, tel:, etc. hrefs in isClientSideUrl

--- packages/wonder-blocks-clickable/src/util/__tests__/is-client-side-url.js.test.js
+++ packages/wonder-blocks-clickable/src/util/__tests__/is-client-side-url.js.test.js
@@ -3,7 +3,7 @@ import {isClientSideUrl} from "../is-client-side-url.js";
 
 describe("isExternalUrl", () => {
     test("returns boolean based on the url", () => {
-        // external urls
+        // external URLs
         expect(
             isClientSideUrl(
                 "//khanacademy.zendesk.com/hc/en-us/articles/236355907",
@@ -18,12 +18,16 @@ describe("isExternalUrl", () => {
         expect(isClientSideUrl("//www.google.com")).toEqual(false);
         expect(isClientSideUrl("//")).toEqual(false);
 
-        // fake hrefs
-        expect(isClientSideUrl("#")).toEqual(false);
+        // non-http(s) URLs
         expect(isClientSideUrl("javascript:void(0);")).toEqual(false);
         expect(isClientSideUrl("javascript:void(0)")).toEqual(false);
+        expect(isClientSideUrl("mailto:foo@example.com")).toEqual(false);
+        expect(isClientSideUrl("tel:+1234567890")).toEqual(false);
 
-        // internal urls
+        // fake HREFs
+        expect(isClientSideUrl("#")).toEqual(false);
+
+        // internal URLs
         expect(isClientSideUrl("/foo//bar")).toEqual(true);
         expect(isClientSideUrl("/coach/dashboard")).toEqual(true);
         expect(isClientSideUrl("/math/early-math/modal/e/addition_1")).toEqual(
