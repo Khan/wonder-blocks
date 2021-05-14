d32f0e5e0cba221cb7ea2453e674b24a34f594c3
Carlos Rivas <carlosrivasvalencia@CarlosVnciasMBP.attlocal.net>
2021-05-14 12:16:11 -0500

    Add example in Storybook

--- packages/wonder-blocks-clickable/src/components/clickable.stories.js
+++ packages/wonder-blocks-clickable/src/components/clickable.stories.js
@@ -5,6 +5,8 @@ import {StyleSheet} from "aphrodite";
 import Clickable from "@khanacademy/wonder-blocks-clickable";
 import {View} from "@khanacademy/wonder-blocks-core";
 import Color from "@khanacademy/wonder-blocks-color";
+import {Strut} from "@khanacademy/wonder-blocks-layout";
+import Spacing from "@khanacademy/wonder-blocks-spacing";
 import {Body} from "@khanacademy/wonder-blocks-typography";
 
 import type {StoryComponentType} from "@storybook/react";
@@ -12,6 +14,47 @@ import type {StoryComponentType} from "@storybook/react";
 export default {
     title: "Clickable",
 };
+
+export const basic: StoryComponentType = () => (
+    <View>
+        <View style={styles.centerText}>
+            <Clickable
+                href="https://www.khanacademy.org/about/tos"
+                skipClientNav={true}
+            >
+                {({hovered, focused, pressed}) => (
+                    <View
+                        style={[
+                            hovered && styles.hovered,
+                            pressed && styles.pressed,
+                        ]}
+                    >
+                        <Body>This text is clickable!</Body>
+                    </View>
+                )}
+            </Clickable>
+        </View>
+        <Strut size={Spacing.xLarge_32} />
+        <View style={[styles.centerText, styles.dark]}>
+            <Clickable
+                href="https://www.khanacademy.org/about/tos"
+                skipClientNav={true}
+                light={true}
+            >
+                {({hovered, focused, pressed}) => (
+                    <View
+                        style={[
+                            hovered && styles.hovered,
+                            pressed && styles.pressed,
+                        ]}
+                    >
+                        <Body>This text is clickable!</Body>
+                    </View>
+                )}
+            </Clickable>
+        </View>
+    </View>
+);
 
 export const keyboardNavigation: StoryComponentType = () => (
     <View>
@@ -74,5 +117,13 @@ const styles = StyleSheet.create({
     },
     focused: {
         outline: `solid 4px ${Color.lightBlue}`,
+    },
+    centerText: {
+        textAlign: "center",
+    },
+    dark: {
+        backgroundColor: Color.darkBlue,
+        color: Color.white,
+        padding: Spacing.xSmall_8,
     },
 });
