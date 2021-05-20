ed78b8604893151ac0f3e821c92d5d83709ee22b
Reid Mitchell <reid@khanacademy.org>
2021-05-20 15:11:24 -0500

    Use string | void instead of ?string for description ID
    This was giving a Flow error, since ?string means string | null | void.
    I only meant it to be string | void, and Flow accepts this.

--- packages/wonder-blocks-form/src/components/choice-internal.js
+++ packages/wonder-blocks-form/src/components/choice-internal.js
@@ -118,7 +118,7 @@ type DefaultProps = {|
             </LabelMedium>
         );
     }
-    getDescriptionId(): ?string {
+    getDescriptionId(): string | void {
         const {description, id} = this.props;
         return description && id && `${id}-description`;
     }
