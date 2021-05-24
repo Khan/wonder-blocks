6388c466aa151d4c31ad43428d7c45501ee9024c
Carlos Rivas <carlosrivasvalencia@CarlosVnciasMBP.attlocal.net>
2021-05-24 12:27:11 -0500

    WB-1051 - Add States to TextField
    Integrated the States that TextField will use. This includes `enabled`,
    `disabled`, `error`, and `focused`. Included the `focused` style for
    when the user is focused on (editing) the TextField. I attached images
    below of this new style.
    
    Issue: https://khanacademy.atlassian.net/browse/WB-1051
    
    Test plan:
    1. View the generated Storybook link. Check the "Form / TextField" section.
    2. You can also view Styleguidist under the "Form" section.
    NOTE: No props have been added yet so the TextField is stuck in the
    `enabled` state, but it will also show the `focused` state when you click
    on the TextField.

--- packages/wonder-blocks-form/src/components/text-field.js
+++ packages/wonder-blocks-form/src/components/text-field.js
@@ -7,16 +7,48 @@ import Color from "@khanacademy/wonder-blocks-color";
 import Spacing from "@khanacademy/wonder-blocks-spacing";
 import {styles as typographyStyles} from "@khanacademy/wonder-blocks-typography";
 
-export default class TextField extends React.Component<{||}> {
+type State = {|
+    enabled: boolean,
+    disabled: boolean,
+    error: ?string,
+    focused: boolean,
+|};
+
+export default class TextField extends React.Component<{||}, State> {
+    state: State = {
+        enabled: true,
+        disabled: false,
+        error: null,
+        focused: false,
+    };
+
+    handleOnFocus: () => void = () => {
+        this.setState({
+            focused: true,
+        });
+    };
+
+    handleOnBlur: () => void = () => {
+        this.setState({
+            focused: false,
+        });
+    };
+
     render(): React.Node {
         return (
             <input
                 className={css([
                     styles.input,
                     typographyStyles.LabelMedium,
-                    styles.default,
+                    this.state.enabled && styles.default,
+                    this.state.disabled && styles.disabled,
+                    this.state.error && styles.error,
+                    this.state.focused && styles.focused,
                 ])}
                 placeholder="Placeholder"
+                disabled={this.state.disabled}
+                onFocus={this.handleOnFocus}
+                onBlur={this.handleOnBlur}
             />
         );
     }
@@ -32,23 +64,37 @@ const styles = StyleSheet.create({
         margin: 0,
         outline: "none",
         boxShadow: "none",
-        "::placeholder": {
-            color: Color.offBlack64,
-        },
     },
     default: {
         background: Color.white,
         border: `1px solid ${Color.offBlack16}`,
         color: Color.offBlack,
+        "::placeholder": {
+            color: Color.offBlack64,
+        },
     },
     error: {
         background: "rgba(217, 41, 22, 0.06)",
         border: `1px solid ${Color.red}`,
         color: Color.offBlack,
+        "::placeholder": {
+            color: Color.offBlack64,
+        },
     },
     disabled: {
         background: Color.offWhite,
         border: `1px solid ${Color.offBlack16}`,
         color: Color.offBlack64,
+        "::placeholder": {
+            color: Color.offBlack32,
+        },
+    },
+    focused: {
+        background: Color.white,
+        border: `1px solid ${Color.blue}`,
+        color: Color.offBlack,
+        "::placeholder": {
+            color: Color.offBlack64,
+        },
     },
 });
