59052ff0e99d03d8a683efacb67ec0087e93baa7
Kevin Barabash <kevinb@khanacademy.org>
2021-08-26 10:26:14 -0400

    Add comments to testing helpers

--- utils/testing/user-event.js
+++ utils/testing/user-event.js
@@ -2,6 +2,14 @@
 /**
  * Wraps @testing-library/user-event so that they automatically advance timers so
  * that the timeout in mockRequestAnimationFrame will fire.
+ *
+ * The reason we can't use @testing-library's async methods to wait for things
+ * is that we're using using fake timers for tests so we instead need to advance
+ * the jest's fake timer in order for the callback passed to requestAnimationFrame
+ * to fire.
+ *
+ * requestAnimationFrame by components using schedule.animationFrame(), one of
+ * which is dropdown-core.js.
  */
 import userEvent from "@testing-library/user-event";
 
