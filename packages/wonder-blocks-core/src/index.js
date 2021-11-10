79cd5a301532dacf2be447ae659dd68e8a2ecc49
Kevin Barabash <kevinb@khanacademy.org>
2021-11-10 17:42:22 -0500

    export the hooks from index.js

--- packages/wonder-blocks-core/src/index.js
+++ packages/wonder-blocks-core/src/index.js
@@ -10,5 +10,10 @@ export {default as UniqueIDProvider} from "./components/unique-id-provider.js";
 export {default as addStyle} from "./util/add-style.js";
 export {default as getElementIntersection} from "./util/get-element-intersection.js";
 export {default as Server} from "./util/server.js";
+export {useRenderState} from "./hooks/use-render-state.js";
+export {
+    useUniqueIdWithMock,
+    useUniqueIdWithoutMock,
+} from "./hooks/use-unique-id.js";
 
 export type {AriaProps, Intersection, IIdentifierFactory, StyleType};
