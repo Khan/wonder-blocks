e70b35d8f005b7a75b45670d731afb85f156668f
Kevin Barabash <kevinb@khanacademy.org>
2021-12-07 13:34:08 -0500

    add missing test file

---
+++ packages/wonder-blocks-core/src/components/__tests__/render-state-root.test.js
@@ -0,0 +1,58 @@
+// @flow
+import * as React from "react";
+import {render} from "@testing-library/react";
+
+import {RenderStateRoot} from "../render-state-root.js";
+// TODO(somewhatabstract, FEI-4174): Update eslint-plugin-import when they
+// have fixed:
+// https://github.com/import-js/eslint-plugin-import/issues/2073
+// eslint-disable-next-line import/named
+import {RenderState, RenderStateContext} from "../render-state-context.js";
+
+const {useContext} = React;
+
+describe("RenderStateRoot", () => {
+    test("the first render should set context's value to RenderState.Initial", () => {
+        // Arrange
+        const values = [];
+        const TestComponent = () => {
+            const value = useContext(RenderStateContext);
+
+            values.push(value);
+
+            return null;
+        };
+
+        // Act
+        render(
+            <RenderStateRoot>
+                <TestComponent />
+            </RenderStateRoot>,
+        );
+
+        // Assert
+        expect(values[0]).toEqual(RenderState.Initial);
+    });
+
+    test("the second render should set context's value to RenderState.Standard", () => {
+        // Arrange
+        const values = [];
+        const TestComponent = () => {
+            const value = useContext(RenderStateContext);
+
+            values.push(value);
+
+            return null;
+        };
+
+        // Act
+        render(
+            <RenderStateRoot>
+                <TestComponent />
+            </RenderStateRoot>,
+        );
+
+        // Assert
+        expect(values[1]).toEqual(RenderState.Standard);
+    });
+});
