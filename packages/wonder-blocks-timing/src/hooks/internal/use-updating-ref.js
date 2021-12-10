baf4c3c838bff00d780a10985b0f72d9bd202103
Kevin Barabash <kevinb@khanacademy.org>
2021-12-10 10:39:43 -0500

    one last docs addition

--- packages/wonder-blocks-timing/src/hooks/internal/use-updating-ref.js
+++ packages/wonder-blocks-timing/src/hooks/internal/use-updating-ref.js
@@ -4,6 +4,11 @@ import {useEffect, useRef} from "react";
 /**
  * Returns a ref whose .current value is updated whenever
  * the `value` passed to this hook changes.
+ *
+ * this is great for values that you want to reference from
+ * within a useCallback or useEffect event listener, without
+ * re-triggering the effect when the value changes
+ *
  * @returns {{current: T}}
  */
 export const useUpdatingRef = <T>(value: T): {|current: T|} => {
