94e7c7712b1fcf2ae8808db4bd1630eade96f8b5
Juan Andrade <juan@khanacademy.org>
2021-12-21 18:52:55 -0500

    Add comments for CellCore, include changeset

--- packages/wonder-blocks-cell/src/components/internal/cell-core.js
+++ packages/wonder-blocks-cell/src/components/internal/cell-core.js
@@ -22,6 +22,14 @@ type CellCoreProps = {|
     children: React.Node,
 |};
 
+/**
+ * CellCore is the base cell wrapper. It's used as the skeleton/layout that is
+ * used by BasicCell and DetailCell (and any other variants).
+ *
+ * Both variants share how they render their accessories, and the main
+ * responsibility of this component is to render the contents that are passed in
+ * (using the `children` prop).
+ */
 const CellCore = (props: CellCoreProps): React.Node => {
     const {
         children,
