3acd496753fe9415af1e9068196141be8adc4a65
Juan Andrade <juan@khanacademy.org>
2022-02-01 17:13:11 -0500

    WB-1227: Storybook - Fix WB aliases on the webpack config file (#1329)
    ## Summary:
    
    Overriding the Storybook's webpack config so we can work with custom WB aliases
    to be able to run Storybook inside our monorepo.
    
    Issue: https://khanacademy.atlassian.net/browse/WB-1227
    
    ## Test plan:
    
    ```
    yarn start:storybook
    ```
    
    1. Verify that storybook starts locally.
    2. Verify that the Storybook chromatic build works fine.
    
    Author: jandrade
    
    Reviewers: jeresig
    
    Required Reviewers:
    
    Approved By: jeresig
    
    Checks: ✅ codecov/project, ✅ Mixed content, ⚪️ Redirect rules, ⚪️ Header rules, ⚪️ Pages changed, ⏭  dependabot, ✅ gerald, ✅ lint_and_test (ubuntu-latest, 12), ✅ chromatic (ubuntu-latest, 12)
    
    Pull Request URL: https://github.com/Khan/wonder-blocks/pull/1329

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,6 @@
+const path = require("path");
+const webpack = require("webpack");
+
 module.exports = {
     stories: ["../packages/**/*.stories.@(js|mdx)"],
     addons: [
@@ -10,5 +13,28 @@ module.exports = {
     ],
     reactOptions: {
         fastRefresh: true,
+    },
+    // Overriding the default webpack config to allow using storybook with our
+    // monorepo.
+    // See https://storybook.js.org/docs/react/configure/webpack
+    webpackFinal: async (config, {configType}) => {
+        const wbRegex = /^@khanacademy\/wonder-blocks(-.*)$/;
+        // Aliases for internal wonder-blocks packages
+        config.plugins.push(
+            new webpack.NormalModuleReplacementPlugin(wbRegex, function (
+                resource,
+            ) {
+                // Replace the module with a direct reference to the
+                // internal folder.
+                // $1 is the unique package name, e.g. cell, birthday-picker
+                resource.request = resource.request.replace(
+                    wbRegex,
+                    path.resolve(__dirname, "../packages/wonder-blocks$1/src"),
+                );
+            }),
+        );
+
+        // Return the altered config
+        return config;
     },
 };
