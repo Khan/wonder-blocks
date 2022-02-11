c9922b342f896e0ae032e4745338f2d117243397
Jeff Yates <jeff@khanacademy.org>
2022-02-11 16:46:18 -0600

    [🔥AUDIT🔥] [fei4261.resultsnotcachedresponses] Export resultFromCachedResponse (#1348)
    🖍 _This is an audit!_ 🖍
    
    ## Summary:
    This just adds an export for code that already exists.
    
    Issue: FEI-4261
    
    ## Test plan:
    `yarn test`
    `yarn flow`
    
    Author: somewhatabstract
    
    Auditors: somewhatabstract, #frontend-infra
    
    Required Reviewers:
    
    Approved By:
    
    Checks: ✅ codecov/project, ✅ Mixed content, ⚪️ Redirect rules, ⚪️ Header rules, ⚪️ Pages changed, ✅ lint_and_test (ubuntu-latest, 12), ⏭  dependabot, ✅ gerald, ✅ chromatic (ubuntu-latest, 12)
    
    Pull Request URL: https://github.com/Khan/wonder-blocks/pull/1348

--- packages/wonder-blocks-data/src/index.js
+++ packages/wonder-blocks-data/src/index.js
@@ -52,6 +52,7 @@ export {useServerEffect} from "./hooks/use-server-effect.js";
 export {useRequestInterception} from "./hooks/use-request-interception.js";
 export {useSharedCache, clearSharedCache} from "./hooks/use-shared-cache.js";
 export {ScopedInMemoryCache} from "./util/scoped-in-memory-cache.js";
+export {resultFromCachedResponse} from "./util/result-from-cache-response.js";
 
 // GraphQL
 export {GqlRouter} from "./components/gql-router.js";
