368e1d98a0bcb25b6c98182a166c8a037cd3ba9b
Kevin Barabash <kevinb@khanacademy.org>
2022-03-29 10:32:18 -0400

    Export ngetpos so that it can be use by handlebars i18n functions in webapp

---
+++ .changeset/seven-pots-think.md
@@ -0,0 +1,5 @@
+---
+"@khanacademy/wonder-blocks-i18n": minor
+---
+
+Export ngetpos so that it can be use by handlebars i18n functions in webapp
--- packages/wonder-blocks-i18n/src/index.js
+++ packages/wonder-blocks-i18n/src/index.js
@@ -5,6 +5,7 @@ export {
     ngettext,
     doNotTranslate,
     doNotTranslateYet,
+    ngetpos, // used by handlebars translation functions in webapp
 } from "./functions/i18n.js";
 
 export {localeToFixed, getDecimalSeparator} from "./functions/l10n.js";
