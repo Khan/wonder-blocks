f2f061b49488b63dd78cbfd8a4f3709597768a3b
Jeff Yates <jeff@khanacademy.org>
2022-05-01 11:16:24 -0400

    [fei4370.4.documentation] Adding docs for mocking framework

--- packages/wonder-blocks-testing/src/index.js
+++ packages/wonder-blocks-testing/src/index.js
@@ -18,16 +18,12 @@ export type {
 } from "./fixtures/types.js";
 
 // Fetch mocking framework
-export type {MockResponse} from "./make-mock-response.js";
-export {RespondWith} from "./make-mock-response.js";
 export {mockFetch} from "./fetch/mock-fetch.js";
-export type {
-    FetchMockFn,
-    FetchMock,
-    FetchMockOperation,
-} from "./fetch/types.js";
 export {mockGqlFetch} from "./gql/mock-gql-fetch.js";
-export type {GqlFetchMockFn, GqlMock, GqlMockOperation} from "./gql/types.js";
+export {RespondWith} from "./make-mock-response.js";
+export type {MockResponse} from "./make-mock-response.js";
+export type {FetchMockFn, FetchMockOperation} from "./fetch/types.js";
+export type {GqlFetchMockFn, GqlMockOperation} from "./gql/types.js";
 
 // Test harness framework
 export * from "./harness/types.js";
