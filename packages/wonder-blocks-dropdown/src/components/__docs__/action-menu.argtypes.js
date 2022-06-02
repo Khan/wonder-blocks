976ce81f9c1459f8ff83890deae3e09fff8e9403
Juan Andrade <juan@khanacademy.org>
2022-06-02 10:24:40 -0400

    WB-1134.1: Add stories for ActionMenu (#1511)
    ## Summary:
    
    First part of the wb-dropdown migration to storybook:
    
    - Adding stories for ActionMenu.
    - Adding props table for both `ActionMenu` and `ActionItem`.
    
    Issue: WB-1134
    
    ## Test plan:
    
    Open storybook and navigate to `Dropdown > ActionMenu`.
    
    Verify that all the examples are similar from the ones in the StyleGuidist site:
    
    https://wonder-blocks.netlify.app/#actionmenu
    
    <img width="1050" alt="Screen Shot 2022-06-01 at 3 35 22 PM" src="https://user-images.githubusercontent.com/843075/171487523-b81cdcaa-8a31-4d17-ace6-c38465cd4115.png">
    
    <img width="997" alt="Screen Shot 2022-06-01 at 3 35 36 PM" src="https://user-images.githubusercontent.com/843075/171487477-8b84afda-f6f2-4b03-ad6b-98b9e0f8274e.png">
    
    Author: jandrade
    
    Reviewers: jeresig
    
    Required Reviewers:
    
    Approved By: jeresig
    
    Checks: ✅ Test (ubuntu-latest, 16.x, 2/2), ✅ Test (ubuntu-latest, 16.x, 1/2), ✅ Lint (ubuntu-latest, 16.x), ✅ Check build sizes (ubuntu-latest, 16.x), ✅ Mixed content, ⚪️ Redirect rules, ⚪️ Header rules, ⚪️ Pages changed, ✅ gerald, ✅ Prime node_modules cache for primary configuration (ubuntu-latest, 16.x), ⏭  dependabot, ✅ Chromatic - Build on review PR (push) / chromatic (ubuntu-latest, 16.x), ⏭  Chromatic - Skip on dependabot PRs (push)
    
    Pull Request URL: https://github.com/Khan/wonder-blocks/pull/1511

---
+++ packages/wonder-blocks-dropdown/src/components/__docs__/action-menu.argtypes.js
@@ -0,0 +1,44 @@
+// @flow
+export default {
+    alignment: {
+        table: {
+            category: "Layout",
+        },
+    },
+    disabled: {
+        table: {
+            category: "States",
+        },
+    },
+    opened: {
+        control: "boolean",
+        table: {
+            category: "States",
+        },
+    },
+    onToggle: {
+        table: {
+            category: "Events",
+        },
+    },
+    onChange: {
+        table: {
+            category: "Events",
+        },
+    },
+    dropdownStyle: {
+        table: {
+            category: "Styling",
+        },
+    },
+    style: {
+        table: {
+            category: "Styling",
+        },
+    },
+    className: {
+        table: {
+            category: "Styling",
+        },
+    },
+};
