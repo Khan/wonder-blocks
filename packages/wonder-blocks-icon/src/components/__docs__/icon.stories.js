8785a9fe1abf9b98d981309c942d9dc395d39286
Nisha Yerunkar <nisha@khanacademy.org>
2022-06-28 11:43:17 -0700

    Add Icons to Storybook (#1537)
    ## Summary:
    Add the documentation for Icons to Storybook. I used the documentation in
    styleguidist as a reference.
    
    Issue: https://khanacademy.atlassian.net/browse/WB-1137
    
    <img width="1322" alt="Screen Shot 2022-06-28 at 7 24 51 AM" src="https://user-images.githubusercontent.com/13231763/176204979-5dcdfca4-b846-429e-8698-e3219af65464.png">
    <img width="1301" alt="Screen Shot 2022-06-28 at 7 24 59 AM" src="https://user-images.githubusercontent.com/13231763/176204992-7fda42e1-7d92-4ba9-9ada-0cf9ab462c84.png">
    <img width="1299" alt="Screen Shot 2022-06-28 at 7 25 14 AM" src="https://user-images.githubusercontent.com/13231763/176205005-6832bc8a-6cc0-4f23-887d-64998ee85fb8.png">
    <img width="1321" alt="Screen Shot 2022-06-28 at 7 25 26 AM" src="https://user-images.githubusercontent.com/13231763/176205017-d888822c-21a7-42fb-b98a-20c591bd9911.png">
    <img width="1317" alt="Screen Shot 2022-06-28 at 7 25 33 AM" src="https://user-images.githubusercontent.com/13231763/176205025-456ea716-5243-4eeb-8568-daf9598c7186.png">
    <img width="1354" alt="Screen Shot 2022-06-28 at 7 25 40 AM" src="https://user-images.githubusercontent.com/13231763/176205041-7cbb6874-203a-4934-a51a-a6d10d868d00.png">
    
    Canvas dropdowns:
    
    <img width="1519" alt="Screen Shot 2022-06-28 at 7 26 06 AM" src="https://user-images.githubusercontent.com/13231763/176205072-56a143dd-0818-491a-9e88-e94af4041e07.png">
    <img width="1513" alt="Screen Shot 2022-06-28 at 7 26 19 AM" src="https://user-images.githubusercontent.com/13231763/176205091-34a1a30c-e26d-448c-bf01-2d89c44851d9.png">
    
    
    ## Test plan:
    - Read through the descriptions and confirm that the information is correct
      and there are no typos.
    - Interact with the controls in the Default canvas and check there is a
      dropdown for the `icon` and for `size`.
    
    Author: nishasy
    
    Reviewers: jeresig
    
    Required Reviewers:
    
    Approved By: jeresig
    
    Checks: ✅ codecov/project, ✅ Lint (ubuntu-latest, 16.x), ✅ Test (ubuntu-latest, 16.x, 2/2), ✅ Test (ubuntu-latest, 16.x, 1/2), ✅ Check build sizes (ubuntu-latest, 16.x), ✅ Mixed content, ⚪️ Redirect rules, ⚪️ Header rules, ⚪️ Pages changed, ✅ Prime node_modules cache for primary configuration (ubuntu-latest, 16.x), ✅ gerald, ⏭  dependabot, ✅ Chromatic - Build on review PR (push) / chromatic (ubuntu-latest, 16.x), ⏭  Chromatic - Skip on dependabot PRs (push)
    
    Pull Request URL: https://github.com/Khan/wonder-blocks/pull/1537

---
+++ packages/wonder-blocks-icon/src/components/__docs__/icon.stories.js
@@ -0,0 +1,148 @@
+// @flow
+import * as React from "react";
+import type {StoryComponentType} from "@storybook/react";
+
+import Color from "@khanacademy/wonder-blocks-color";
+import {View} from "@khanacademy/wonder-blocks-core";
+import Icon, {icons} from "@khanacademy/wonder-blocks-icon";
+import {LabelMedium} from "@khanacademy/wonder-blocks-typography";
+
+import ComponentInfo from "../../../../../.storybook/components/component-info.js";
+import {name, version} from "../../../package.json";
+
+export default {
+    title: "Icon / Icon",
+    component: Icon,
+    parameters: {
+        componentSubtitle: ((
+            <ComponentInfo name={name} version={version} />
+        ): any),
+    },
+    argTypes: {
+        icon: {
+            options: icons,
+        },
+        size: {
+            options: ["small", "medium", "large", "xlarge"],
+        },
+    },
+};
+
+export const Default: StoryComponentType = (args) => {
+    return <Icon {...args} />;
+};
+
+Default.args = {icon: icons.search, size: "small"};
+
+export const Basic: StoryComponentType = () => {
+    return <Icon icon={icons.search} />;
+};
+
+Basic.parameters = {
+    docs: {
+        storyDescription: `Minimal icon usage. This is a search icon.
+            Icons are size \`"small"\` by default.`,
+    },
+};
+
+export const Sizes: StoryComponentType = () => {
+    return (
+        <table>
+            <tr>
+                <td>
+                    <LabelMedium>{"small"}</LabelMedium>
+                </td>
+                <td>
+                    <Icon icon={icons.search} size="small" />
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <LabelMedium>{"medium"}</LabelMedium>
+                </td>
+                <td>
+                    <Icon icon={icons.search} size="medium" />
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <LabelMedium>{"large"}</LabelMedium>
+                </td>
+                <td>
+                    <Icon icon={icons.search} size="large" />
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <LabelMedium>{"xlarge"}</LabelMedium>
+                </td>
+                <td>
+                    <Icon icon={icons.search} size="xlarge" />
+                </td>
+            </tr>
+        </table>
+    );
+};
+
+Sizes.parameters = {
+    docs: {
+        storyDescription: `The size of an icon is determined by the \`Icon\`'s
+            \`size\` prop. While we don't currently have assets for sizes
+            larger than medium, we can still render any icon at any size
+            in a pinch. The available sizes are \`"small"\`, \`"medium"\`,
+            \`"large"\`, and \`"xlarge"\`. `,
+    },
+};
+
+export const Variants: StoryComponentType = () => {
+    const iconsWithLabels = Object.entries(icons).map(([iconName, _]) => {
+        return (
+            <tr>
+                <td>
+                    <Icon icon={icons[iconName]} />
+                </td>
+                <td>
+                    <LabelMedium>{iconName}</LabelMedium>
+                </td>
+            </tr>
+        );
+    });
+
+    return <table>{iconsWithLabels}</table>;
+};
+
+Variants.parameters = {
+    docs: {
+        storyDescription: `The actual icon is determined by the \`Icon\`'s
+            \`icon\` prop. Here are the icons that are already defined in
+            Wonder Blocks and ready to use. Just import \`{icons}\` from
+            \`"@khanacademy/wonder-blocks-icon"\`.`,
+    },
+};
+
+export const WithColor: StoryComponentType = () => {
+    return <Icon size="small" icon={icons.info} color={Color.red} />;
+};
+
+WithColor.parameters = {
+    docs: {
+        storyDescription:
+            "The color of an icon can be specified through its `color` prop.",
+    },
+};
+
+export const Inline: StoryComponentType = () => {
+    return (
+        <View>
+            Here is an icon
+            <Icon size="small" icon={icons.info} style={{margin: 2}} />
+            when it is inline.
+        </View>
+    );
+};
+
+Inline.parameters = {
+    docs: {
+        storyDescription: "Icons have `display: inline-block` by default.",
+    },
+};
