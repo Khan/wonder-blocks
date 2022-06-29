c7a620812f52f7421ea317a1619fc704ef480680
Jeff Yates <jeff@khanacademy.org>
2022-06-29 16:34:55 -0500

    [fei4635.1] Make fixtures framework CSF/storybook only and simplify API

--- packages/wonder-blocks-testing/src/fixtures/fixtures.js
+++ packages/wonder-blocks-testing/src/fixtures/fixtures.js
@@ -1,55 +1,14 @@
 // @flow
 import * as React from "react";
-import {getConfiguration} from "./setup.js";
-import {combineOptions} from "./combine-options.js";
+import {action} from "@storybook/addon-actions";
 
-import type {FixturesOptions, GetPropsOptions} from "./types.js";
+import type {FixtureProps} from "./types.js";
 
-type FixtureProps<TProps: {...}> =
-    | $ReadOnly<TProps>
-    | ((options: $ReadOnly<GetPropsOptions>) => $ReadOnly<TProps>);
-
-const normalizeOptions = <
-    TComponent: React.ComponentType<any>,
-    TProps: React.ElementConfig<TComponent>,
->(
-    componentOrOptions:
-        | TComponent
-        | $ReadOnly<FixturesOptions<TComponent, TProps>>,
-): $ReadOnly<FixturesOptions<TComponent, TProps>> => {
-    // To differentiate between a React component and a FixturesOptions object,
-    // we have to do some type checking.
-    //
-    // Alternatives I considered were:
-    // - Use an additional parameter for the options and then do an arg number
-    //   check, but that always makes typing a function harder and often breaks
-    //   types. I didn't want that battle today.
-    // - Use a tuple when providing component and options with the first element
-    //   being the component and the second being the options. However that
-    //   feels like an obscure API even though it's really easy to do the
-    //   typing.
-    if (
-        // Most React components, whether functional or class-based, are
-        // inherently functions in JavaScript, so a check for functions is
-        // usually sufficient.
-        typeof componentOrOptions === "function" ||
-        // However, the return of React.forwardRef is not a function,
-        // so we also have to cope with that.
-        // A forwardRef has $$typeof = Symbol(react.forward_ref) and a
-        // render function.
-        // $FlowIgnore[prop-missing]
-        typeof componentOrOptions.render === "function"
-    ) {
-        return {
-            // $FlowIgnore[incompatible-return]
-            component: componentOrOptions,
-        };
-    }
-    // We can't test for React.ComponentType at runtime.
-    // Let's assume our simple heuristic above is sufficient.
-    // $FlowIgnore[incompatible-return]
-    return componentOrOptions;
-};
+type FixtureFn<TProps: {...}> = (
+    description: string,
+    props: FixtureProps<TProps>,
+    wrapper?: React.ComponentType<TProps>,
+) => mixed;
 
 /**
  * Describe a group of fixtures for a given component.
@@ -57,71 +16,63 @@ const normalizeOptions = <
  * Only one `fixtures` call should be used per fixture file as it returns
  * the exports for that file.
  *
- * @param {FixtureOptions<TProps>} options Options describing the
+ * @param {component: React.ComponentType<any>} options Options describing the
  * fixture group.
  * @param {FixtureFn<TProps> => void} fn A function that provides a `fixture`
  * function for defining fixtures.
- * @returns {Exports} The object to be exported as `module.exports`.
- *
- * TODO(somewhatabstract): Determine a way around this requirement so we
- * can support named exports and default exports via the adapters in a
- * deterministic way. Currently this is imposed on us because of how
- * storybook, the popular framework, uses both default and named exports for
- * its interface.
+ * @returns {(
+ *    description: string,
+ *    props: FixtureProps<TProps>,
+ *    wrapper?: React.ComponentType<TProps>,
+ * ) => mixed} A function to create a CSF compatible story.
  */
 export const fixtures = <
     TComponent: React.ComponentType<any>,
     TProps: React.ElementConfig<TComponent>,
 >(
-    componentOrOptions:
-        | TComponent
-        | $ReadOnly<FixturesOptions<TComponent, TProps>>,
-    fn: (
-        fixture: (
-            description: string,
-            props: FixtureProps<TProps>,
-            wrapper?: React.ComponentType<TProps>,
-        ) => void,
-    ) => void,
-): ?$ReadOnly<mixed> => {
-    const {adapter, defaultAdapterOptions} = getConfiguration();
+    Component: TComponent,
+): FixtureFn<TProps> => {
+    const templateMap = new WeakMap();
+    // We use this to make sure each story gets a unique name.
+    let storyNumber = 1;
 
-    const {
-        title,
-        component,
-        description: groupDescription,
-        defaultWrapper,
-        additionalAdapterOptions,
-    } = normalizeOptions<TComponent, TProps>(componentOrOptions);
-
-    // 1. Create a new adapter group.
-    const group = adapter.declareGroup<TProps>({
-        title,
-        description: groupDescription,
-        getDefaultTitle: () =>
-            component.displayName || component.name || "Component",
-    });
-
-    // 2. Invoke fn with a function that can add a new fixture.
-    const addFixture = (description, props, wrapper = null): void => {
-        group.declareFixture({
-            description,
-            getProps: (options) =>
-                typeof props === "function" ? props(options) : props,
-            component: wrapper ?? defaultWrapper ?? component,
-        });
+    const getPropsOptions = {
+        log: (message, ...args) => action(message)(...args),
+        logHandler: action,
     };
-    fn(addFixture);
 
-    // 3. Combine the adapter options from the fixture group with the
-    //    defaults from our setup.
-    const groupAdapterOverrides =
-        additionalAdapterOptions?.[adapter.name] ?? {};
-    const combinedAdapterOptions = combineOptions(
-        defaultAdapterOptions,
-        groupAdapterOverrides,
-    );
+    const makeStory = (
+        description: string,
+        props: FixtureProps<TProps>,
+        wrapper: ?React.ComponentType<TProps> = null,
+    ): mixed => {
+        const storyName = `${storyNumber++} ${description}`;
+        const getProps = (options) =>
+            typeof props === "function" ? props(options) : props;
 
-    // 4. Call close on the group and return the result.
-    return group.closeGroup(combinedAdapterOptions);
+        // We create a “template” of how args map to rendering
+        // for each type of component as the component here could
+        // be the component under test, or wrapped in a wrapper
+        // component. We don't use decorators for the wrapper
+        // because we may not be in a storybook context and it
+        // keeps the framework API simpler this way.
+        let Template = templateMap.get((Component: any));
+        if (Template == null) {
+            Template = (args) => <Component {...args} />;
+            templateMap.set((Component: any), Template);
+        }
+
+        // Each story that shares that component then reuses that
+        // template.
+        const story = Template.bind({});
+        story.args = getProps(getPropsOptions);
+        // Adding a story name here means that we don't have to
+        // care about naming the exports correctly, if we don't
+        // want (useful if we need to autogenerate or manually
+        // expose ESM exports).
+        story.storyName = storyName;
+
+        return story;
+    };
+    return makeStory;
 };
