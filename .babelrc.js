df17c556438902eb960bc01e161f8fc3b79321e7
Jeff Yates <jeff@khanacademy.org>
2022-09-01 17:33:29 -0500

    [fei4139.wb.1] Update to v29 of jest

--- .babelrc.js
+++ .babelrc.js
@@ -1,12 +1,6 @@
 /**
- * HACK(somewhatabstract): Due to https://github.com/facebook/jest/issues/11741,
- * we need to have this file, or updating inline snapshots can fail rather
- * cryptically.
- *
- * We should remove this when jest is fixed.
- * See FEI-4139 in Jira.
- *
- * NOTE: This has to be at the same level as every `package.json` that
- * has child jest tests that may update inline snapshots, it seems.
+ * HACK(somewhatabstract):
+ * Jest looks for babel config in the cwd during the transform step so we need
+ * to direct it to the real one.
  */
 module.exports = require("./build-settings/babel.config.js");
