98829fdc23589a8c745495d9331a6c7105232d53
Kevin Barabash <kevinb@khanacademy.org>
2022-11-01 14:40:05 -0400

    Land full Banner component implementation (#1637)
    ## Summary:
    
    All the parts of banner have been landed onto `feature/banner`.
    Base implementation: https://khanacademy.atlassian.net/browse/WB-1384
    Other additions: https://khanacademy.atlassian.net/browse/WB-1385
    
    Now we can land it into the wonder blocks repo!
    
    ## Test Plan:
    
    Storybook:
    https://5e1bf4b385e3fb0020b7073c-hetukdqgof.chromatic.com/?path=/docs/banner--default
    
    `yarn jest packages/wonder-blocks-banner/src/components/__tests__/banner.test.js`
    
    Author: kevinbarabash
    
    Reviewers: jandrade
    
    Required Reviewers:
    
    Approved By: jandrade
    
    Checks: ✅ codecov/project, ✅ Lint (ubuntu-latest, 16.x), ✅ Test (ubuntu-latest, 16.x, 2/2), ✅ Test (ubuntu-latest, 16.x, 1/2), ✅ Check build sizes (ubuntu-latest, 16.x), ✅ Update Chromatic (ubuntu-latest, 16.x), ✅ Gather coverage (ubuntu-latest, 16.x), ✅ Mixed content, ⚪️ Redirect rules, ⚪️ Header rules, ⚪️ Pages changed, ✅ gerald, ✅ Prime node_modules cache for primary configuration (ubuntu-latest, 16.x), ⏭  dependabot, ✅ Chromatic - Build on review PR (push) / chromatic (ubuntu-latest, 16.x), ✅ Prime node_modules cache for primary configuration (ubuntu-latest, 16.x), ⏭  Chromatic - Skip on dependabot PRs (push)
    
    Pull Request URL: https://github.com/Khan/wonder-blocks/pull/1637

---
+++ packages/wonder-blocks-banner/src/components/__docs__/banner.argtypes.js
@@ -0,0 +1,89 @@
+// @flow
+const actionsMappings = {
+    none: null,
+    buttons: [
+        {title: "Button 1", onClick: () => {}},
+        {title: "Button 2", onClick: () => {}},
+    ],
+    links: [
+        {title: "Link 1", href: "/"},
+        {title: "Link 2", href: "/"},
+    ],
+    buttonAndLink: [
+        {title: "Button", onClick: () => {}},
+        {title: "Link", href: "/"},
+    ],
+    linkAndButton: [
+        {title: "Link", href: "/"},
+        {title: "Button", onClick: () => {}},
+    ],
+};
+
+const dismissMappings = {
+    none: null,
+    "log on click": () => {
+        // eslint-disable-next-line no-console
+        console.log("Dimiss button clicked!");
+    },
+};
+
+export default {
+    kind: {
+        control: {type: "select"},
+        defaultValue: "info",
+        description: "Determines the color and icon of the banner.",
+        options: ["info", "success", "warning", "critical"],
+        table: {
+            type: {summary: `"info" | "success" | "warning" | "critical"`},
+            defaultValue: {summary: `"info"`},
+        },
+        type: {required: false},
+    },
+    layout: {
+        control: {type: "select"},
+        description: "Determines the edge style of the Banner.",
+        options: ["floating", "full-width"],
+        table: {
+            type: {summary: `"floating" | "full-width"`},
+        },
+        type: {required: true},
+    },
+    text: {
+        control: {type: "text"},
+        description:
+            "Text on the banner (LabelSmall) or a node if you want something different.",
+        table: {type: {summary: "string | React.Node"}},
+        type: {required: true},
+    },
+    actions: {
+        control: {type: "select"},
+        description: `Links or tertiary Buttons that appear to the right of the
+            text.\n\nThe ActionTrigger must have either an onClick or an href
+            field, or both.`,
+        options: actionsMappings,
+        table: {
+            type: {
+                summary: "Array<ActionTrigger>",
+                detail: "type ActionTrigger = {|\n\ttitle: string,\n\tonClick: () => void | href: string,\n\tariaLabel?: string,\n|}",
+            },
+        },
+    },
+    onDismiss: {
+        control: {type: "select"},
+        description:
+            "If present, dismiss button is on right side. If not, no button appears.",
+        options: dismissMappings,
+        table: {required: false},
+    },
+    dismissAriaLabel: {
+        control: {type: "text"},
+        description:
+            "The accessible label for the dismiss button. Please pass in a translated string.",
+        defaultValue: "Dismiss banner.",
+        table: {
+            type: {summary: "string"},
+            defaultValue: {summary: `"Dismiss banner."`},
+        },
+        type: {required: false},
+    },
+};
