79f75cbc0e2e90f19b2b8323b1c79179f2a7bc4e
Juan Andrade <juan@khanacademy.org>
2022-11-21 12:58:46 -0500

    add TooltipContent

---
+++ packages/wonder-blocks-tooltip/src/components/__docs__/tooltip-content.stories.js
@@ -0,0 +1,89 @@
+// @flow
+import * as React from "react";
+import {TooltipContent} from "@khanacademy/wonder-blocks-tooltip";
+import {Body, LabelSmall} from "@khanacademy/wonder-blocks-typography";
+
+import type {StoryComponentType} from "@storybook/react";
+
+import ComponentInfo from "../../../../../.storybook/components/component-info.js";
+import {name, version} from "../../../package.json";
+
+export default {
+    title: "Tooltip / TooltipContent",
+    component: TooltipContent,
+    parameters: {
+        componentSubtitle: ((
+            <ComponentInfo name={name} version={version} />
+        ): any),
+    },
+};
+
+const Template = (args) => <TooltipContent {...args} />;
+
+/**
+ * Default example (interactive).
+ */
+export const Default: StoryComponentType = Template.bind({});
+
+Default.args = {
+    title: "A Tooltip with a title",
+    children: "some text",
+};
+
+/**
+ * Only text content
+ */
+export const OnlyTextContent: StoryComponentType = Template.bind({});
+
+OnlyTextContent.args = {
+    children: "Only the content",
+};
+
+OnlyTextContent.parameters = {
+    docs: {
+        description: {
+            story: "This shows the default which is text rendered using `LabelMedium`.",
+        },
+    },
+};
+
+/**
+ * Titled content
+ */
+export const TitledContent: StoryComponentType = Template.bind({});
+
+TitledContent.args = {
+    title: "This tooltip has a title",
+    children: "Some content in my tooltip",
+};
+
+TitledContent.parameters = {
+    docs: {
+        description: {
+            story: "This shows the default with a title; the title is rendered using `HeadingSmall`.",
+        },
+    },
+};
+
+/**
+ * Custom title and custom content
+ */
+export const CustomContent: StoryComponentType = Template.bind({});
+
+CustomContent.args = {
+    title: <Body>Body text title!</Body>,
+    children: (
+        <>
+            <Body>Body text content!</Body>
+            <LabelSmall>And LabelSmall!</LabelSmall>
+        </>
+    ),
+};
+
+CustomContent.parameters = {
+    docs: {
+        description: {
+            story: "This shows how we can customize both the title and the content.",
+        },
+    },
+};
