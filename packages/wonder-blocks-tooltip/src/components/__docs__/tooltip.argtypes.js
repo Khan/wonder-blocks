6cf700f29afce1fea57097fbacb38a9b88237929
Juan Andrade <juan@khanacademy.org>
2022-11-21 11:59:27 -0500

    WB-1146.1: Port `Tooltip` to Storybook
    - Port the `Tooltip` component from `wonder-blocks-tooltip` to Storybook.
    - Removed some stories that are no longer relevant.
    - Added more documentation to the stories.
    
    Test plan
    
    Verify that the stories look correct in Storybook (Docs).

---
+++ packages/wonder-blocks-tooltip/src/components/__docs__/tooltip.argtypes.js
@@ -0,0 +1,15 @@
+// @flow
+
+export default {
+    placement: {
+        control: {
+            type: "select",
+            options: ["top", "bottom", "right", "left"],
+        },
+    },
+    title: {
+        control: {
+            type: "text",
+        },
+    },
+};
