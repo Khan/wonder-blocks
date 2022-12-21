735451c82a53ad932a72c198b1882c6664c97f4e
Juan Andrade <juan@khanacademy.org>
2022-12-21 09:54:27 -0500

    WB-1440: Migrate `wonder-blocks-spacing` to Storybook
    Summary:
    
    - Migrated `wonder-blocks-spacing` from Styleguidist to Storybook.
    - Added a `SpacingTable` component to display the spacing values in a
      table.
    - Repurposed the spacing docs to be more close to how we display Colors.
    - Removed Styleguidist docs relaed to Spacing.
    
    Test Plan:
    
    Navigate to /?path=/docs/spacing-spacing--page and verify that the docs
    make sense, and that all spacing values are correct.

---
+++ packages/wonder-blocks-spacing/src/__docs__/spacing-table.js
@@ -0,0 +1,86 @@
+// @flow
+import * as React from "react";
+import {StyleSheet} from "aphrodite";
+
+import Color from "@khanacademy/wonder-blocks-color";
+import {addStyle, View} from "@khanacademy/wonder-blocks-core";
+import Spacing from "@khanacademy/wonder-blocks-spacing";
+import {LabelMedium} from "@khanacademy/wonder-blocks-typography";
+
+const StyledTable = addStyle("table");
+const StyledTableRow = addStyle("tr");
+const StyledTableHeader = addStyle("th");
+const StyledTableCell = addStyle("td");
+
+export default function SpacingTable(): React.Node {
+    return (
+        <StyledTable style={styles.table}>
+            <thead>
+                <StyledTableRow style={styles.header}>
+                    <StyledTableHeader style={styles.cell}>
+                        Size
+                    </StyledTableHeader>
+                    <StyledTableHeader style={styles.cell}>
+                        Space
+                    </StyledTableHeader>
+                    <StyledTableHeader style={styles.cell}>
+                        Example vertical
+                    </StyledTableHeader>
+                    <StyledTableHeader style={styles.cell}>
+                        Example horizontal
+                    </StyledTableHeader>
+                </StyledTableRow>
+            </thead>
+            <tbody>
+                {Object.keys(Spacing).map((spaceName, idx) => (
+                    <StyledTableRow key={idx} style={styles.row}>
+                        <StyledTableCell style={styles.cell}>
+                            <LabelMedium>{spaceName}</LabelMedium>
+                        </StyledTableCell>
+                        <StyledTableCell style={styles.cell}>
+                            <LabelMedium>{Spacing[spaceName]}px</LabelMedium>
+                        </StyledTableCell>
+                        <StyledTableCell style={styles.cell}>
+                            <View
+                                style={{
+                                    backgroundColor: Color.purple,
+                                    width: Spacing[spaceName],
+                                    height: Spacing.medium_16,
+                                }}
+                            />
+                        </StyledTableCell>
+                        <StyledTableCell style={styles.cell}>
+                            <View
+                                style={{
+                                    backgroundColor: Color.purple,
+                                    width: Spacing.medium_16,
+                                    height: Spacing[spaceName],
+                                }}
+                            />
+                        </StyledTableCell>
+                    </StyledTableRow>
+                ))}
+            </tbody>
+        </StyledTable>
+    );
+}
+
+const styles = StyleSheet.create({
+    table: {
+        borderCollapse: "collapse",
+        borderSpacing: 0,
+        margin: `${Spacing.xLarge_32}px 0`,
+        textAlign: "left",
+        width: "100%",
+    },
+    header: {
+        backgroundColor: Color.offWhite,
+    },
+    row: {
+        borderTop: `1px solid ${Color.offBlack8}`,
+    },
+    cell: {
+        padding: Spacing.xSmall_8,
+        verticalAlign: "middle",
+    },
+});
