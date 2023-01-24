fb7917c5fedf156e7f4e77ad9aa2defaf32aa100
Juan Andrade <juan@khanacademy.org>
2023-01-24 17:00:58 -0500

    More changes: including Serif Source Pro, new logo

--- .storybook/wonder-blocks-theme.js
+++ .storybook/wonder-blocks-theme.js
@@ -9,8 +9,19 @@ export default create({
     brandTitle: "Wonder Blocks",
     brandUrl: "/",
     brandImage: "./logo.svg",
+    colorPrimary: "#E31937",
+    colorSecondary: "#489FDF",
 
     // Typography
     fontBase: '"Lato", sans-serif',
     fontCode: "Inconsolata",
+
+    // Text colors
+    textColor: "#21242C",
+    textInverseColor: "rgba(255,255,255,0.9)",
+
+    // Toolbar default and active colors
+    barTextColor: "silver",
+    barSelectedColor: "black",
+    barBg: "hotpink",
 });
