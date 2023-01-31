daa815c3e03efb5b087a02ac8a5f467bbd55f13c
Juan Andrade <juan@khanacademy.org>
2023-01-31 16:55:34 -0500

    hack-enzyme.6: Port `wonder-blocks-popover` to RTL
    This is the next stop in a series of changes to port all of our tests to
    RTL.
    
    This change ports the `wonder-blocks-popover` package to RTL. Also
    deletes some unused test utilities.
    
    ```
    yarn test
    ```

--- packages/wonder-blocks-popover/src/components/__tests__/popover-dialog.test.js
+++ packages/wonder-blocks-popover/src/components/__tests__/popover-dialog.test.js
@@ -1,45 +1,27 @@
 // @flow
 import * as React from "react";
-import {mount} from "enzyme";
-import "jest-enzyme";
+import {render} from "@testing-library/react";
 
-import {TooltipTail} from "@khanacademy/wonder-blocks-tooltip";
-
+import type {Placement} from "@khanacademy/wonder-blocks-tooltip";
 import PopoverDialog from "../popover-dialog.js";
 import PopoverContentCore from "../popover-content-core.js";
 
 jest.mock("@khanacademy/wonder-blocks-tooltip");
 
 describe("PopoverDialog", () => {
-    it("should update the tail color to match the content's color", () => {
-        // Arrange
-        const onUpdateMock = jest.fn();
-
-        // Act
-        const wrapper = mount(
-            <PopoverDialog placement="top" onUpdate={onUpdateMock}>
-                <PopoverContentCore color="darkBlue">
-                    popover content
-                </PopoverContentCore>
-            </PopoverDialog>,
-        );
-
-        // Assert
-        expect(wrapper.find(TooltipTail).prop("color")).toBe("darkBlue");
-    });
-
     it("should call onUpdate if placement is changed", () => {
         // Arrange
         const onUpdateMock = jest.fn();
-
-        const wrapper = mount(
-            <PopoverDialog placement="top" onUpdate={onUpdateMock}>
+        const UnderTest = ({placement}: {|placement: Placement|}) => (
+            <PopoverDialog placement={placement} onUpdate={onUpdateMock}>
                 <PopoverContentCore>popover content</PopoverContentCore>
-            </PopoverDialog>,
+            </PopoverDialog>
         );
 
+        const {rerender} = render(<UnderTest placement="top" />);
+
         // Act
-        wrapper.setProps({placement: "bottom"});
+        rerender(<UnderTest placement="bottom" />);
 
         // Assert
         expect(onUpdateMock).toBeCalledWith("bottom");
@@ -49,14 +31,16 @@ describe("PopoverDialog", () => {
         // Arrange
         const onUpdateMock = jest.fn();
 
-        const wrapper = mount(
-            <PopoverDialog placement="top" onUpdate={onUpdateMock}>
+        const UnderTest = ({placement}: {|placement: Placement|}) => (
+            <PopoverDialog placement={placement} onUpdate={onUpdateMock}>
                 <PopoverContentCore>popover content</PopoverContentCore>
-            </PopoverDialog>,
+            </PopoverDialog>
         );
 
+        const {rerender} = render(<UnderTest placement="top" />);
+
         // Act
-        wrapper.setProps({placement: "top"});
+        rerender(<UnderTest placement="top" />);
 
         // Assert
         expect(onUpdateMock).not.toBeCalled();
