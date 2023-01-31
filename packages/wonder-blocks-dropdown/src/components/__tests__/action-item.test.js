423628b61a89125c6095b26efa34aa34f779330f
Juan Andrade <juan@khanacademy.org>
2023-01-31 10:55:51 -0500

    Add back Link test

--- packages/wonder-blocks-dropdown/src/components/__tests__/action-item.test.js
+++ packages/wonder-blocks-dropdown/src/components/__tests__/action-item.test.js
@@ -1,8 +1,15 @@
 // @flow
 import * as React from "react";
 import {render, screen} from "@testing-library/react";
+import * as ReactRouterDOM from "react-router-dom";
 
 import ActionItem from "../action-item.js";
+
+jest.mock("react-router-dom", () => ({
+    __esModule: true,
+    ...jest.requireActual("react-router-dom"),
+    Link: jest.fn().mockReturnValue(<a href="/link">some link</a>),
+}));
 
 describe("ActionItem", () => {
     it("should render with disabled styles", () => {
@@ -24,6 +31,21 @@ describe("ActionItem", () => {
         // Assert
         // eslint-disable-next-line testing-library/no-node-access
         expect(document.querySelectorAll("a")).toHaveLength(1);
+    });
+
+    it("should render a Link if there's a router", () => {
+        // Arrange
+        const linkSpy = jest.spyOn(ReactRouterDOM, "Link");
+
+        // Act
+        render(
+            <ReactRouterDOM.MemoryRouter>
+                <ActionItem href="/foo" label="Example" />
+            </ReactRouterDOM.MemoryRouter>,
+        );
+
+        // Assert
+        expect(linkSpy).toHaveBeenCalled();
     });
 
     it("should set the lang attribute if it's passed down", () => {
