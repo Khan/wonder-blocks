e66911171b7a1065c2a2887fa664e6fe6c997c03
Juan Andrade <juan@khanacademy.org>
2023-02-01 10:40:46 -0500

    Add back color test

--- packages/wonder-blocks-popover/src/components/__tests__/popover-dialog.test.js
+++ packages/wonder-blocks-popover/src/components/__tests__/popover-dialog.test.js
@@ -1,6 +1,7 @@
 // @flow
 import * as React from "react";
 import {render} from "@testing-library/react";
+import * as Tooltip from "@khanacademy/wonder-blocks-tooltip";
 
 import type {Placement} from "@khanacademy/wonder-blocks-tooltip";
 import PopoverDialog from "../popover-dialog.js";
@@ -9,6 +10,26 @@ import PopoverContentCore from "../popover-content-core.js";
 jest.mock("@khanacademy/wonder-blocks-tooltip");
 
 describe("PopoverDialog", () => {
+    it("should update the tail color to match the content's color", () => {
+        // Arrange
+        const tooltipTailSpy = jest.spyOn(Tooltip, "TooltipTail");
+
+        // Act
+        render(
+            <PopoverDialog placement="top" onUpdate={jest.fn()}>
+                <PopoverContentCore color="darkBlue">
+                    popover content
+                </PopoverContentCore>
+            </PopoverDialog>,
+        );
+
+        // Assert
+        expect(tooltipTailSpy).toHaveBeenCalledWith(
+            expect.objectContaining({color: "darkBlue"}),
+            {},
+        );
+    });
+
     it("should call onUpdate if placement is changed", () => {
         // Arrange
         const onUpdateMock = jest.fn();
