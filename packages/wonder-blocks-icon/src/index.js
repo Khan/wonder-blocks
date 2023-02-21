1a5624d424f7efe478b7acdfa7bbb10a13b0d2eb
Kevin Barabash <kevinb@khanacademy.org>
2023-02-21 17:03:20 -0500

    FEI-4957: Update to use migrated wonder-stuff packages (#1741)
    ## Summary:
    This PR also fixes a Flow error with one of the uses of 'entries()' and tweaks out we export icons to use 'export * from'.  We use this pattern in wonder-stuff as well.
    
    Issue: FEI-4957
    
    ## Test plan:
    - yarn flow
    - yarn lint
    - yarn start
    - verify that http://localhost:6061/?path=/story/icon-icon--variants loads without any issues
    - let CI run the remainder of the checks
    
    Author: kevinbarabash
    
    Reviewers: jeresig, #typescript
    
    Required Reviewers:
    
    Approved By: jeresig
    
    Checks: ✅ Test (ubuntu-latest, 16.x, 2/2), ✅ Test (ubuntu-latest, 16.x, 1/2), ✅ Check build sizes (ubuntu-latest, 16.x), ✅ Lint (ubuntu-latest, 16.x), ✅ Prime node_modules cache for primary configuration (ubuntu-latest, 16.x), ✅ gerald, ✅ Check build sizes (ubuntu-latest, 16.x), ✅ Test (ubuntu-latest, 16.x, 2/2), ✅ Test (ubuntu-latest, 16.x, 1/2), ✅ Lint (ubuntu-latest, 16.x), ✅ Prime node_modules cache for primary configuration (ubuntu-latest, 16.x), ✅ gerald, ⏭  dependabot, ✅ Chromatic - Build on review PR (push) / chromatic (ubuntu-latest, 16.x), ⏭  Chromatic - Skip on dependabot PRs (push)
    
    Pull Request URL: https://github.com/Khan/wonder-blocks/pull/1741

--- packages/wonder-blocks-icon/src/index.js
+++ packages/wonder-blocks-icon/src/index.js
@@ -1,11 +1,8 @@
 // @flow
 import Icon from "./components/icon.js";
-import * as iconAssets from "./util/icon-assets.js";
 import type {IconAsset, IconSize} from "./util/icon-assets.js";
 
-// $FlowIgnore[prop-missing]: Flow doesn't know about __esModule
-const {__esModule: _, ...icons} = iconAssets;
+export * as icons from "./util/icon-assets.js";
 
 export type {IconAsset, IconSize};
-export {icons};
 export default Icon;
