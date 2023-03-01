529d268b249d81fc7da329a65728886f520efb91
Kevin Barabash <kevinb@khanacademy.org>
2023-03-01 18:51:04 -0500

    config babel for storybook using the method suggested in the docs

--- .storybook/main.js
+++ .storybook/main.js
@@ -15,6 +15,23 @@ module.exports = {
         // To enable interaction testing
         "@storybook/addon-interactions",
     ],
+    babel: async (options) => {
+        console.log("babel options =", options);
+        return {
+            ...options,
+            plugins: [
+                ...options.plugins,
+                // NOTE(kevinb): `plugin-proposal-class-properties` must come before
+                // `plugin-transform-classes`.
+                ["@babel/plugin-proposal-class-properties", {loose: true}],
+                ["@babel/plugin-transform-classes", {loose: true}],
+                // NOTE(kevinb): these are here just to quiet warnings about these
+                // plugins not using `loose: true`.
+                ["@babel/plugin-proposal-private-property-in-object", {loose: true}],
+                ["@babel/plugin-proposal-private-methods", {loose: true}],
+            ]
+        };
+    },
     features: {
         // Enables playback controls
         interactionsDebugger: true,
