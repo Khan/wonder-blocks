7834c2e9719530a289833aac540f617084377e87
Kevin Barabash <kevinb@khanacademy.org>
2023-03-01 16:52:32 -0500

    Tweak aphrodite lib def a bit, add changeset files

---
+++ .changeset/quiet-pets-yell.md
@@ -0,0 +1,5 @@
+---
+"@khanacademy/wonder-blocks-core": minor
+---
+
+Update aphrodite lib def and StyleType
--- types/aphrodite.d.ts
+++ types/aphrodite.d.ts
@@ -2,27 +2,35 @@ declare module "aphrodite" {
     import * as React from "react";
 
     type _CSSProperties = React.CSSProperties & {
+        /**
+         * Browser Specific
+         */
         MsFlexBasis?: React.CSSProperties["flexBasis"];
         MsFlexPreferredSize?: React.CSSProperties["flexBasis"];
         WebkitFlexBasis?: React.CSSProperties["flexBasis"];
         flexBasis?: React.CSSProperties["flexBasis"];
+        "::-moz-focus-inner"?: React.CSSProperties;
+
+        /**
+         * Media queries
+         */
+        "@media (max-width: 1023px)"?: React.CSSProperties;
+        "@media (min-width: 1024px)"?: React.CSSProperties;
+        "@media (min-width: 1168px)"?: React.CSSProperties;
     };
 
     /**
      * A CSS property definition.
      */
     export type CSSProperties = _CSSProperties & {
+        /**
+         * Pseudo-selectors
+         */
         "::placeholder"?: _CSSProperties;
         ":after"?: _CSSProperties;
         ":focus-visible"?: _CSSProperties;
         ":focus"?: _CSSProperties;
         ":hover"?: _CSSProperties;
-
-        "@media (max-width: 1023px)"?: _CSSProperties;
-        "@media (min-width: 1024px)"?: _CSSProperties;
-        "@media (min-width: 1168px)"?: _CSSProperties;
-
-        "::-moz-focus-inner"?: _CSSProperties;
     };
 
     /**
