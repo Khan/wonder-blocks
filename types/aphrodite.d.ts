0d28bb1c87cfebf9501bc6ee0dcf2fee2483362c
Kevin Barabash <kevinb@khanacademy.org>
2023-03-01 11:21:44 -0500

    FEI-4966.3: Configure typescript and type definitions for dependencies (#1746)
    ## Summary:
    Before we can convert all of the source code from Flow to TypeScript we need to have TypeScript configured properly and lib defs for third party libraries.  The codemod will be run in the next PR.
    
    Issue: FEI-4966
    
    ## Test plan:
    - run 'yarn tsc --noEmit' in the next PR to check that things are configured properly
    
    Author: kevinbarabash
    
    Reviewers: jeresig, kevinbarabash
    
    Required Reviewers:
    
    Approved By: jeresig, jeresig
    
    Checks: ✅ codecov/project, ❌ Lint (ubuntu-latest, 16.x), ✅ Test (ubuntu-latest, 16.x, 2/2), ✅ Test (ubuntu-latest, 16.x, 1/2), ✅ Check build sizes (ubuntu-latest, 16.x), ❌ gerald, ✅ Prime node_modules cache for primary configuration (ubuntu-latest, 16.x), ⏭  dependabot, ✅ Chromatic - Build on review PR (push) / chromatic (ubuntu-latest, 16.x), ⏭  Chromatic - Skip on dependabot PRs (push)
    
    Pull Request URL: https://github.com/Khan/wonder-blocks/pull/1746

---
+++ types/aphrodite.d.ts
@@ -0,0 +1,29 @@
+declare module "aphrodite" {
+    /**
+     * A CSS property definition.
+     */
+    export type CSSProperties = any;
+
+    /**
+     * Aphrodite style declaration
+     */
+    export type StyleDeclaration = Record<string, any>;
+
+    export interface StyleSheetStatic {
+        /**
+         * Create style sheet
+         */
+        create<T extends StyleDeclaration>(styles: T): T;
+        /**
+         * Rehydrate class names from server renderer
+         */
+        rehydrate(renderedClassNames: Array<string>): void;
+    }
+
+    export var StyleSheet: StyleSheetStatic;
+
+    /**
+     * Get class names from passed styles
+     */
+    export function css(...styles: Array<any>): string;
+}
