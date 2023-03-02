3638b2c28848b14020fede20ac17ee2052f384d5
Kevin Barabash <kevinb@khanacademy.org>
2023-03-01 19:28:16 -0500

    re-add .babelrc.js and update the configuration there properly

---
+++ .storybook/.babelrc.js
@@ -0,0 +1,28 @@
+/**
+ * Storybook doesn't seem to like the "exports a function" style of babel
+ * config. And it pics up our root .babelrc.js file that is added to workaround
+ * jest. So, we need to provide its own.
+ *
+ * We should remove this when jest is fixed.
+ * See FEI-4139 in Jira.
+ */
+const babelConfig = require("../build-settings/babel.config");
+
+const config = babelConfig({
+    env: () => false,
+});
+
+module.exports = {
+    ...config,
+    plugins: [
+        ...config.plugins,
+        // NOTE(kevinb): `plugin-proposal-class-properties` must come before
+        // `plugin-transform-classes`.
+        ["@babel/plugin-proposal-class-properties", {loose: true}],
+        ["@babel/plugin-transform-classes", {loose: true}],
+        // NOTE(kevinb): these are here just to quiet warnings about these
+        // plugins not using `loose: true`.
+        ["@babel/plugin-proposal-private-property-in-object", {loose: true}],
+        ["@babel/plugin-proposal-private-methods", {loose: true}],
+    ],
+};
