17e994446e05027c14dbb49dde7c487bb6d4aeab
Kevin Barabash <kevinb@khanacademy.org>
2023-03-03 11:29:14 -0500

    Fix misc TS types issues

--- build-scripts/gen-flow-types.ts
+++ build-scripts/gen-flow-types.ts
@@ -1,4 +1,5 @@
 import {execFileSync} from "child_process";
+import * as fs from "fs";
 import * as path from "path";
 import * as fglob from "fast-glob";
 
@@ -14,6 +15,18 @@ for (const inFile of files) {
     try {
         execFileSync("yarn", args, {cwd: rootDir});
         console.log(`✅ wrote: ${outFile}`);
+
+        // `flowgen` sometimes outputs code that uses `mixins` instead of `extends`
+        // so we do some post-processing on the files to clean that up.
+        const contents = fs.readFileSync(path.join(rootDir, outFile), "utf-8");
+        if (contents.includes(" mixins ")) {
+            console.log("replacing 'mixins' with 'extends'");
+            fs.writeFileSync(
+                path.join(rootDir, outFile),
+                contents.replace(/ mixins /g, " extends "),
+                "utf-8",
+            );
+        }
     } catch (e) {
         console.log(`❌ error processing: ${inFile}: ${e}`);
     }
