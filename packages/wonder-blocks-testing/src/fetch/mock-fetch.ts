Khan/wonder-blocks

commit a41745ec4bdcbaff896f632450e55b6c6fe38b41

FEI-4966.4: Run codemod to convert .js(x) files to .ts(x) (#1749)
## Summary:
This PR was created by running the following commands:
- cd ../flow-to-typescript-codemod
- yarn typescriptify convert -p ../wonder-blocks --write --delete
- yarn typescriptify fix --autoSuppressErrors --jiraSlug FEI-5019 -p ../wonder-blocks --config ../wonder-blocks/tsconfig-check.json
- cd ../wonder-blocks
- yarn prettier --write packages/**/*.ts packages/**/*.tsx

Issue: FEI-4966

## Test plan:
- yarn tsc --package tsconfig-check.json, see that there are a handful of unsuppressed errors, these will be address in a subsequent PR

Author: kevinbarabash

Reviewers: jeresig

Required Reviewers:

Approved By: jeresig

Checks: ❌ Test (ubuntu-latest, 16.x, 2/2), ❌ Test (ubuntu-latest, 16.x, 1/2), ❌ Check build sizes (ubuntu-latest, 16.x), ❌ Lint (ubuntu-latest, 16.x), ✅ gerald, ✅ Prime node_modules cache for primary configuration (ubuntu-latest, 16.x), ⏭  dependabot, ❌ Chromatic - Build on review PR (push) / chromatic (ubuntu-latest, 16.x), ⏭  Chromatic - Skip on dependabot PRs (push)

Pull Request URL: https://github.com/Khan/wonder-blocks/pull/1749

packages/wonder-blocks-testing/src/fetch/mock-fetch.ts
@@ -1,0 +1,19 @@
+import {fetchRequestMatchesMock} from "./fetch-request-matches-mock";
+import {mockRequester} from "../mock-requester";
+import type {FetchMockFn, FetchMockOperation} from "./types";
+
+/**
+ * A mock for the fetch function passed to GqlRouter.
+ */
+// @ts-expect-error [FEI-5019] - TS2709 - Cannot use namespace '_' as a type.
+export const mockFetch = (): FetchMockFn =>
+    mockRequester<FetchMockOperation, _>(
+        fetchRequestMatchesMock,
+        (input, init) =>
+            `Input: ${
+                typeof input === "string"
+                    ? input
+                    : JSON.stringify(input, null, 2)
+            }
+Options: ${init == null ? "None" : JSON.stringify(init, null, 2)}`,
+    );

commit a7fd4c0757501a0afc6b90c4cad2cb495203fe83

[🔥AUDIT🔥] [fixfetchmock] Fix formatting of fetch mock errors (#1823)
🖍 _This is an audit!_ 🖍

## Summary:
This fixes a regression that was introduced via the codemod in https://github.com/Khan/wonder-blocks/pull/1749.

Issue: FEI-5042

## Test plan:
`yarn test`
Also will verify that the existing snapshots in webapp pass as-is with this change.

Author: somewhatabstract

Auditors: #typescript, #wonder-blocks

Required Reviewers:

Approved By:

Checks: ⌛ Test (ubuntu-latest, 16.x, 2/2), ⌛ Test (ubuntu-latest, 16.x, 1/2), ⌛ Check build sizes (ubuntu-latest, 16.x), ⌛ Lint (ubuntu-latest, 16.x), ⏭  dependabot, ✅ gerald, ⏭  Chromatic - Build on Release PR (PR opened), ✅ Prime node_modules cache for primary configuration (ubuntu-latest, 16.x), ⌛ Chromatic - Build on review PR (push) / chromatic (ubuntu-latest, 16.x), ⏭  Chromatic - Skip on dependabot PRs (push)

Pull Request URL: https://github.com/Khan/wonder-blocks/pull/1823

packages/wonder-blocks-testing/src/fetch/mock-fetch.ts
@@ -8,11 +8,13 @@ import type {FetchMockFn, FetchMockOperation} from "./types";
 export const mockFetch = (): FetchMockFn =>
     mockRequester<FetchMockOperation, any>(
         fetchRequestMatchesMock,
+        // NOTE(somewhatabstract): The indentation is expected on the lines
+        // here.
         (input, init) =>
             `Input: ${
                 typeof input === "string"
                     ? input
                     : JSON.stringify(input, null, 2)
             }
-Options: ${init == null ? "None" : JSON.stringify(init, null, 2)}`,
+    Options: ${init == null ? "None" : JSON.stringify(init, null, 2)}`,
     );

packages/wonder-blocks-testing/src/fetch/__tests__/__snapshots__/mock-fetch.test.ts.snap
@@ -3,13 +3,13 @@
 exports[`#mockFetch should reject with a useful error when there are no matching mocks for %s 1`] = `
 "No matching mock response found for request:
     Input: http://example.com/foo
-Options: None"
+    Options: None"
 `;
 
 exports[`#mockFetch should reject with a useful error when there are no matching mocks for %s 2`] = `
 "No matching mock response found for request:
     Input: "http://example.com/foo"
-Options: {
+    Options: {
   "method": "GET"
 }"
 `;
@@ -23,7 +23,7 @@ exports[`#mockFetch should reject with a useful error when there are no matching
   "compress": true,
   "counter": 0
 }
-Options: {
+    Options: {
   "method": "POST"
 }"
 `;