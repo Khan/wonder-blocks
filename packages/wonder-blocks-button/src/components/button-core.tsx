3446b69a11fe4d76dd61b766e18499ace3457194
Marian Wang <77523470+marianmwang@users.noreply.github.com>
2023-06-28 14:30:17 -0700

    Modified tertiary disabled and focused button styling to fix underline length (#1925)
    ## Summary:
    When a tertiary button is disabled and focused, the underline is too long if the user supplies their own width. This fixes the underline length to match the text.
    
    Issue: WB-1557
    
    ## Test plan:
    - Verify in storybook
    - Run and update unit tests
    
    | Before | After |
    |--------|--------|
    | <img width="199" alt="Screen Shot 2023-06-28 at 10 50 11 AM" src="https://github.com/Khan/wonder-blocks/assets/77523470/aa401be5-e996-4a05-b7fb-643993503400"> | <img width="190" alt="Screen Shot 2023-06-28 at 10 48 13 AM" src="https://github.com/Khan/wonder-blocks/assets/77523470/0879f32a-2531-4e02-bf83-49f27e1b9e18"> |
    
    Author: marianmwang
    
    Reviewers: jandrade
    
    Required Reviewers:
    
    Approved By: jandrade
    
    Checks: ✅ codecov/project, ✅ gerald, ✅ Build Storybook (ubuntu-latest, 16.x), ✅ Lint (ubuntu-latest, 16.x), ✅ Test (ubuntu-latest, 16.x, 2/2), ✅ Test (ubuntu-latest, 16.x, 1/2), ✅ Check build sizes (ubuntu-latest, 16.x), ✅ Prime node_modules cache for primary configuration (ubuntu-latest, 16.x), ✅ gerald, ⏭  dependabot, ✅ Chromatic - Build on review PR (push) / chromatic (ubuntu-latest, 16.x), ⏭  Chromatic - Skip on dependabot PRs (push)
    
    Pull Request URL: https://github.com/Khan/wonder-blocks/pull/1925

--- packages/wonder-blocks-button/src/components/button-core.tsx
+++ packages/wonder-blocks-button/src/components/button-core.tsx
@@ -111,6 +111,12 @@ export default class ButtonCore extends React.Component<Props> {
                         (pressed
                             ? buttonStyles.active
                             : (hovered || focused) && buttonStyles.focus),
+                    kind === "tertiary" &&
+                        disabled &&
+                        focused && [
+                            buttonStyles.focus,
+                            buttonStyles.disabledFocus,
+                        ],
                 ]}
             >
                 {icon && (
@@ -387,23 +393,10 @@ const _generateStyles = (
             disabled: {
                 color: light ? fadedColor : offBlack32,
                 cursor: "default",
-                ":focus": {
-                    ":after": {
-                        content: "''",
-                        position: "absolute",
-                        height: 2,
-                        width: `calc(100% - ${iconWidth}px)`,
-                        right: 0,
-                        /**
-                         * For tertiary buttons, underline is applied to the
-                         * internal `span` element, So we need to calculate the
-                         * offset to center the line in the button (right after
-                         * the span).
-                         */
-                        bottom: `calc(50% - 11px)`,
-                        background: light ? white : offBlack32,
-                        borderRadius: 2,
-                    },
+            },
+            disabledFocus: {
+                ":after": {
+                    background: light ? white : offBlack32,
                 },
             },
         };
