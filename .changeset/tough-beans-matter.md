64a188e35fb4ae6417cf37c69139598aa3908b62
Jeff Yates <jeff@khanacademy.org>
2023-07-10 14:53:12 -0500

    [fei5213.1.useforceupdatefix] Fix useForceUpdate (#1931)
    ## Summary:
    This update makes sure that no matter how many times `forceUpdate` is called, it will trigger a re-render. Before this change, the new test case would fail because the boolean value, after multiple calls, would not have actually changed, so React never forced a new render.
    
    With this update, things work as intended.
    
    Issue: FEI-5213
    
    ## Test plan:
    `yarn test`
    
    Author: somewhatabstract
    
    Reviewers: kevinbarabash
    
    Required Reviewers:
    
    Approved By: kevinbarabash
    
    Checks: ✅ codecov/project, ✅ Build Storybook (ubuntu-latest, 16.x), ✅ Lint (ubuntu-latest, 16.x), ✅ Test (ubuntu-latest, 16.x, 2/2), ✅ Test (ubuntu-latest, 16.x, 1/2), ✅ Check build sizes (ubuntu-latest, 16.x), ✅ Prime node_modules cache for primary configuration (ubuntu-latest, 16.x), ⏭  dependabot, ✅ gerald, ✅ Lint (ubuntu-latest, 16.x), ✅ Test (ubuntu-latest, 16.x, 2/2), ✅ Test (ubuntu-latest, 16.x, 1/2), ✅ Check build sizes (ubuntu-latest, 16.x), ✅ Build Storybook (ubuntu-latest, 16.x), ✅ Prime node_modules cache for primary configuration (ubuntu-latest, 16.x), ⏭  Chromatic - Build on Release PR (PR opened), ✅ gerald, ⏭  dependabot, ✅ Chromatic - Build on review PR (push) / chromatic (ubuntu-latest, 16.x), ⏭  Chromatic - Skip on dependabot PRs (push)
    
    Pull Request URL: https://github.com/Khan/wonder-blocks/pull/1931

---
+++ .changeset/tough-beans-matter.md
@@ -0,0 +1,5 @@
+---
+"@khanacademy/wonder-blocks-core": patch
+---
+
+Fix useForceUpdate so that regardless of how many times it is called before a new render, it will always cause a new render
--- packages/wonder-blocks-core/src/hooks/use-force-update.ts
+++ packages/wonder-blocks-core/src/hooks/use-force-update.ts
@@ -13,10 +13,25 @@ import * as React from "react";
  * @returns {() => void} A function that forces the component to update.
  */
 export const useForceUpdate = (): (() => void) => {
-    const [, setState] = React.useState(false);
-    const forceUpdate = React.useCallback(
-        () => setState((state) => !state),
-        [],
-    );
+    const updatePendingRef = React.useRef(false);
+    const [, setUpdateToggle] = React.useState(false);
+
+    const forceUpdate = React.useCallback(() => {
+        if (updatePendingRef.current) {
+            // If an update is already pending, then we do nothing.
+            return;
+        }
+
+        // Otherwise, if we haven't been asked to force an update since our
+        // last render then we toggle the state to invoke a render.
+        setUpdateToggle((toggle) => !toggle);
+        updatePendingRef.current = true;
+    }, []);
+
+    // Reset to false when we've rendered.
+    // This ensures that we reset our counter the next time we're asked to
+    // force an update.
+    updatePendingRef.current = false;
+
     return forceUpdate;
 };
