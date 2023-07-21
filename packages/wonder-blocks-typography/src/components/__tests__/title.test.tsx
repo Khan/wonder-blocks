f32c55cc38fdf04aeb33a867e997a52ca0d0483a
Nisha Yerunkar <nisha@khanacademy.org>
2023-07-21 14:06:25 -0700

    Remove waitFor from tests

--- packages/wonder-blocks-typography/src/components/__tests__/title.test.tsx
+++ packages/wonder-blocks-typography/src/components/__tests__/title.test.tsx
@@ -1,10 +1,10 @@
 import * as React from "react";
-import {render, waitFor} from "@testing-library/react";
+import {render} from "@testing-library/react";
 
 import Title from "../title";
 
 describe("Title", () => {
-    test("forwards the ref to the heading element", async () => {
+    test("forwards the ref to the heading element", () => {
         // Arrange
         const ref: React.RefObject<HTMLHeadingElement> = React.createRef();
 
@@ -12,8 +12,6 @@ describe("Title", () => {
         render(<Title ref={ref}>This is a title</Title>);
 
         // Assert
-        await waitFor(() => {
-            expect(ref.current).toBeInstanceOf(HTMLHeadingElement);
-        });
+        expect(ref.current).toBeInstanceOf(HTMLHeadingElement);
     });
 });
