b27c25d8371cecdcd5cf4b5f4b3f8756de2d14ec
Juan Andrade <juan@khanacademy.org>
2023-08-10 12:31:51 -0400

    WB-1571: Implement withScopedTheme HOC
    Adds a new HOC that allows you to scope a theme to a specific component.
    This uses internally the `useScopedTheme` hook, but with the difference
    that it can be used by class components.

--- packages/wonder-blocks-theming/src/index.ts
+++ packages/wonder-blocks-theming/src/index.ts
@@ -2,3 +2,9 @@ export {default as tokens} from "./tokens";
 export {mergeTheme} from "./utils/merge-theme";
 export {default as createThemeContext} from "./utils/create-theme-context";
 export {default as useScopedTheme} from "./hooks/use-scoped-theme";
+// HOC
+export {
+    default as withScopedTheme,
+    type WithThemeProps,
+} from "./components/with-scoped-theme";
+export {type ThemedStylesFn} from "./types";
