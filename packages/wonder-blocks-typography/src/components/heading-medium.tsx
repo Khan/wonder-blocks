f95c5ff85ab4446dedc16eea6670ee0f036940ff
Jeff Yates <jeff@khanacademy.org>
2023-08-10 12:35:22 -0500

    [propsfor] Move code to using PropsFor

--- packages/wonder-blocks-typography/src/components/heading-medium.tsx
+++ packages/wonder-blocks-typography/src/components/heading-medium.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
-import {Text} from "@khanacademy/wonder-blocks-core";
+import {PropsFor, Text} from "@khanacademy/wonder-blocks-core";
 
 import styles from "../util/styles";
 
-import type {Props} from "../util/types";
+type Props = PropsFor<typeof Text>;
 
 const HeadingMedium = React.forwardRef(function HeadingMedium(
     {style, children, tag = "h3", ...otherProps}: Props,
--- packages/wonder-blocks-typography/src/components/tagline.tsx
+++ packages/wonder-blocks-typography/src/components/tagline.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
-import {Text} from "@khanacademy/wonder-blocks-core";
+import {PropsFor, Text} from "@khanacademy/wonder-blocks-core";
 
 import styles from "../util/styles";
 
-import type {Props} from "../util/types";
+type Props = PropsFor<typeof Text>;
 
 const Tagline = React.forwardRef(function Tagline(
     {style, children, tag = "span", ...otherProps}: Props,
