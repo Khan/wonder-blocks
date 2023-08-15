014cd622be2feb4da803bb73ee24c12f07f726d5
Juan Andrade <juan@khanacademy.org>
2023-08-15 10:45:39 -0400

    WB-1568: Add theme utilities (#1977)
    ## Summary:
    
    - Adds `mergeTheme`: This utility is useful for merging themes together. It's a
    bit like Object.assign, but it does a deep merge so that nested objects are
    merged together and returned as a new object.
    
    1. #1975
    2. **WB-1568: Add theme utilities [CURRENT]**
    3. #1978
    4. #1979 
    5. #1981
    6. #1982
    7. Create Storybook example for adding theming support to a custom component (WIP)
    
    
    Issue: WB-1568
    
    ## Test plan:
    
    Verify that the docs are correct and that the tests pass.
    
    https://5e1bf4b385e3fb0020b7073c-tigkwckdoj.chromatic.com/?path=/docs/theming-mergetheme--docs
    <img width="851" alt="Screenshot 2023-08-10 at 6 02 37 PM" src="https://github.com/Khan/wonder-blocks/assets/843075/13edade1-5ec5-4576-abc4-57c5a7379392">
    
    Author: jandrade
    
    Reviewers: nishasy, jandrade
    
    Required Reviewers:
    
    Approved By: nishasy
    
    Checks: ✅ gerald, ⌛ Prime node_modules cache for primary configuration (ubuntu-latest, 16.x), ⏭  dependabot, ⌛ Chromatic - Build on review PR (push) / chromatic (ubuntu-latest, 16.x), ⏭  Chromatic - Skip on dependabot PRs (push)
    
    Pull Request URL: https://github.com/Khan/wonder-blocks/pull/1977

---
+++ __docs__/wonder-blocks-theming/theme-examples.tsx
@@ -0,0 +1,75 @@
+import * as React from "react";
+
+import {
+    createThemeContext,
+    mergeTheme,
+    tokens,
+} from "@khanacademy/wonder-blocks-theming";
+
+const buttonDefaultTheme = {
+    color: {
+        bg: {
+            primary: tokens.color.blue,
+        },
+        text: {
+            light: tokens.color.white,
+        },
+    },
+    border: {
+        radius: tokens.border.radius.medium_4,
+    },
+};
+
+const buttonCustomTheme = mergeTheme(buttonDefaultTheme, {
+    color: {
+        bg: {
+            primary: tokens.color.pink,
+        },
+    },
+    border: {
+        radius: tokens.border.radius.large_6,
+    },
+});
+
+const ButtonThemeContext = createThemeContext(buttonDefaultTheme);
+
+// TODO(WB-1577): Replace this with the actual WB Button component.
+const ThemedButton = () => {
+    const theme = React.useContext(ButtonThemeContext);
+
+    return (
+        <button
+            style={{
+                background: theme.color.bg.primary,
+                color: theme.color.text.light,
+                borderRadius: theme.border.radius,
+                padding: tokens.spacing.medium_16,
+            }}
+        >
+            This is a themed button!
+        </button>
+    );
+};
+
+/**
+ * A wrapper component that provides a theme to the button.
+ */
+const ThemeWrapper = ({theme}: {theme: any}) => {
+    return (
+        <ButtonThemeContext.Provider value={theme}>
+            <ThemedButton />
+        </ButtonThemeContext.Provider>
+    );
+};
+
+/**
+ * NOTE: The following components are meant to be used in the Storybook docs
+ * only.
+ */
+export const ButtonWithTheme = () => (
+    <ThemeWrapper theme={buttonDefaultTheme} />
+);
+
+export const ButtonWithCustomTheme = () => (
+    <ThemeWrapper theme={buttonCustomTheme} />
+);
--- packages/wonder-blocks-theming/src/index.ts
+++ packages/wonder-blocks-theming/src/index.ts
@@ -1,1 +1,3 @@
 export {default as tokens} from "./tokens";
+export {mergeTheme} from "./utils/merge-theme";
+export {default as createThemeContext} from "./utils/create-theme-context";
