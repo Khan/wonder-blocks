fb7040438955ff1792634836095bf2dc69835f1b
Marian Wang <77523470+marianmwang@users.noreply.github.com>
2023-09-07 12:53:11 -0700

    Add new xsmall, small, and medium size prop to IconButton (#2040)
    ## Summary:
    - Added new size prop with 3 options based on designs found in Figma
      - Created new icon button util file with 2 simple functions
    - Refactored icon button stories file by using comments as descriptions
    - Factored out icon button stories argtypes
    <img width="136" alt="Screen Shot 2023-09-05 at 2 22 37 PM" src="https://github.com/Khan/wonder-blocks/assets/77523470/880fb63d-c8d9-471d-bd4c-d00484981207">
    
    Issue: WB-1528
    
    ## Test plan:
    - New test file was created for icon util functions
    - New snapshot generated for sizes
    
    Author: marianmwang
    
    Reviewers: jandrade
    
    Required Reviewers:
    
    Approved By: jandrade
    
    Checks: ✅ codecov/project, ✅ Lint (ubuntu-latest, 16.x), ✅ Check build sizes (ubuntu-latest, 16.x), ✅ Test (ubuntu-latest, 16.x, 2/2), ✅ Test (ubuntu-latest, 16.x, 1/2), ✅ gerald, ✅ Prime node_modules cache for primary configuration (ubuntu-latest, 16.x), ⏭  dependabot, ✅ Chromatic - Build on review PR (push) / chromatic (ubuntu-latest, 16.x), ⏭  Chromatic - Skip on dependabot PRs (push)
    
    Pull Request URL: https://github.com/Khan/wonder-blocks/pull/2040

---
+++ packages/wonder-blocks-icon-button/src/util/icon-button-util.ts
@@ -0,0 +1,22 @@
+import {IconSize} from "@khanacademy/wonder-blocks-icon";
+import {IconButtonSize} from "../components/icon-button";
+
+/**
+ * A function that returns the icon size for a given icon button size.
+ */
+export const iconSizeForButtonSize = (size: IconButtonSize): IconSize => {
+    switch (size) {
+        case "xsmall":
+            return "small";
+        case "small":
+            return "medium";
+        case "medium":
+            return "medium";
+    }
+};
+
+/**
+ * A function that returns the size of the touch target in pixels for a given icon button size.
+ */
+export const targetPixelsForSize = (size: IconButtonSize): number =>
+    ({xsmall: 24, small: 32, medium: 40}[size]);
