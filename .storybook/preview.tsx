ced86b06a83ac6b091418deb386720bb0572c4f3
Juan Andrade <juan@khanacademy.org>
2023-09-18 17:33:25 -0400

    WB-1573: Add storybook example for adding theming support to a component (#1574) (#1986)
    ## Summary:
    
    - Adds an overview section to the theming docs.
    - Adds guidelines for adding theming support to a component.
    - Modifies the sorting order of the storybook stories to put the overview
    sections at the top.
    
    Issue: WB-1573
    
    ## Test plan:
    
    Verify that the new page is clear and documents how to add themes to a given component:
    
    https://5e1bf4b385e3fb0020b7073c-jsshumrrxg.chromatic.com/?path=/docs/theming-guides-adding-a-theme--docs
    
    Author: jandrade
    
    Reviewers: #wonder-blocks, nishasy
    
    Required Reviewers:
    
    Approved By:
    
    Checks: ✅ codecov/project, ✅ Test (ubuntu-latest, 16.x, 2/2), ✅ Check build sizes (ubuntu-latest, 16.x), ✅ Lint (ubuntu-latest, 16.x), ✅ Test (ubuntu-latest, 16.x, 1/2), ✅ gerald, ⏭  Publish npm snapshot, ✅ gerald, ✅ Prime node_modules cache for primary configuration (ubuntu-latest, 16.x), ✅ Test (ubuntu-latest, 16.x, 2/2), ✅ Test (ubuntu-latest, 16.x, 1/2), ✅ Lint (ubuntu-latest, 16.x), ✅ Check build sizes (ubuntu-latest, 16.x), ⏭  Publish npm snapshot, ✅ Prime node_modules cache for primary configuration (ubuntu-latest, 16.x), ✅ gerald, ⏭  dependabot, ✅ Chromatic - Build on review PR (push) / chromatic (ubuntu-latest, 16.x), ⏭  Chromatic - Skip on dependabot PRs (push)
    
    Pull Request URL: https://github.com/Khan/wonder-blocks/pull/1986

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -72,7 +72,14 @@ export const parameters = {
         // display the stories (or examples first), then we will display all the
         // mdx pages under __docs__.
         storySort: {
-            order: ["Components", "**/__docs__/**", "Overview"],
+            includeNames: true,
+            order: [
+                "Overview",
+                "*",
+                ["Overview", "*"],
+                "**/__docs__/**/*.mdx",
+                "**/*.stories.@(js|jsx|ts|tsx)",
+            ],
         },
     },
     docs: {
