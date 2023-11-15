2634dca4027343171417fb1ff269cd022b951891
Juan Andrade <juan@khanacademy.org>
2023-11-15 17:40:20 -0500

    Fix type test

--- packages/wonder-blocks-icon/src/components/__tests__/phosphor-icon.typestest.tsx
+++ packages/wonder-blocks-icon/src/components/__tests__/phosphor-icon.typestest.tsx
@@ -3,10 +3,6 @@ import PlusCircleRegular from "@phosphor-icons/core/regular/plus-circle.svg";
 import PlusCircleBold from "@phosphor-icons/core/bold/plus-circle-bold.svg";
 import PlusCircleFill from "@phosphor-icons/core/fill/plus-circle-fill.svg";
 
-// @ts-expect-error - invalid icon weight (duotone)
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import VideoDuoTone from "@phosphor-icons/core/duotone/video-duotone.svg";
-
 import {PhosphorIcon} from "../phosphor-icon";
 
 // Valid: small + bold
