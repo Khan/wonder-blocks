54c17ed1d78878aeb1222942cc23f98179dcbfc7
Juan Andrade <juan@khanacademy.org>
2023-11-15 19:17:08 -0500

    Simplify SVG types

--- types/assets.d.ts
+++ types/assets.d.ts
@@ -6,22 +6,3 @@
 declare module "*.jpg";
 declare module "*.png";
 declare module "*.svg";
-
-// Support specific SVG paths from @phosphor-icons/core.
-declare type PhosphorRegular = string & {weight: "PhosphorRegular"};
-declare module "@phosphor-icons/core/regular/*.svg" {
-    const icon: PhosphorRegular;
-    export default icon;
-}
-
-declare type PhosphorBold = string & {weight: "PhosphorBold"};
-declare module "@phosphor-icons/core/bold/*-bold.svg" {
-    const icon: PhosphorBold;
-    export default icon;
-}
-
-declare type PhosphorFill = string & {weight: "PhosphorFill"};
-declare module "@phosphor-icons/core/fill/*-fill.svg" {
-    const icon: PhosphorFill;
-    export default icon;
-}
