07a06be6a9072382bad1a3547de52c47a8fa1631
Juan Andrade <juan@khanacademy.org>
2023-11-27 14:19:06 -0500

    Remove Icon component

--- packages/wonder-blocks-icon/src/index.ts
+++ packages/wonder-blocks-icon/src/index.ts
@@ -1,8 +1,2 @@
-import Icon from "./components/icon";
-import type {IconAsset, IconSize} from "./util/icon-assets";
-
-export * as icons from "./util/icon-assets";
 export {PhosphorIcon} from "./components/phosphor-icon";
-export type {PhosphorIconAsset} from "./types";
-export type {IconAsset, IconSize};
-export default Icon;
+export type {IconSize, PhosphorIconAsset} from "./types";
--- packages/wonder-blocks-icon/src/types.ts
+++ packages/wonder-blocks-icon/src/types.ts
@@ -2,3 +2,8 @@
  * All the possible icon weights.
  */
 export type PhosphorIconAsset = PhosphorRegular | PhosphorBold | PhosphorFill;
+
+/**
+ * All the possible icon sizes.
+ */
+export type IconSize = "small" | "medium" | "large" | "xlarge";
