d0d567a5b71f75457a7eaf6dfc54062ee853e380
Nisha Yerunkar <nisha@khanacademy.org>
2023-12-19 17:49:48 -0800

    Accordion header overflow fix (#2152)
    ## Summary:
    
    Currently, there is an issue with a particular accordion section in webapp where long titles in the
    custom header cause the accordion contents to overflow. This is fixed by adding `minWidth: 0`
    to the styles in Wonder Blocks Accordion Header.
    
    I also included a story in this PR that replicates the issue, which is fixed with the new style.
    
    | Before | After |
    | --- | --- |
    | <img width="651" alt="Screenshot 2023-12-19 at 3 41 54 PM" src="https://github.com/Khan/wonder-blocks/assets/13231763/a4106bef-9cef-4479-953e-6c5ed307d699"> | <img width="654" alt="Screenshot 2023-12-19 at 3 41 37 PM" src="https://github.com/Khan/wonder-blocks/assets/13231763/5e9a55e4-b886-4a21-aba3-ad886d820bee"> |
    
    
    Issue: none
    
    ## Test plan:
    - Go to the following story: /?path=/story/accordion-accordionsection--react-element-in-header
    - Confirm that the second accordion header has an ellipsis when the window is small
    
    Author: nishasy
    
    Reviewers: kevinbarabash, jandrade, somewhatabstract
    
    Required Reviewers:
    
    Approved By: kevinbarabash
    
    Checks: ✅ Chromatic - Get results on regular PRs (ubuntu-latest, 20.x), ❌ codecov/project, ✅ Lint (ubuntu-latest, 20.x), ✅ Test (ubuntu-latest, 20.x, 2/2), ✅ Check build sizes (ubuntu-latest, 20.x), ✅ Test (ubuntu-latest, 20.x, 1/2), ✅ Chromatic - Build on regular PRs / chromatic (ubuntu-latest, 20.x), ✅ Publish npm snapshot (ubuntu-latest, 20.x), ⏭️  Chromatic - Skip on Release PR (changesets), ✅ Prime node_modules cache for primary configuration (ubuntu-latest, 20.x), ✅ Check for .changeset entries for all changed files (ubuntu-latest, 20.x), ✅ gerald, ⏭️  dependabot
    
    Pull Request URL: https://github.com/Khan/wonder-blocks/pull/2152

---
+++ .changeset/wild-kangaroos-impress.md
@@ -0,0 +1,5 @@
+---
+"@khanacademy/wonder-blocks-accordion": patch
+---
+
+Use minWidth: 0 in the header to prevent overflow issues
--- packages/wonder-blocks-accordion/src/components/accordion-section-header.tsx
+++ packages/wonder-blocks-accordion/src/components/accordion-section-header.tsx
@@ -159,6 +159,12 @@ const ANIMATION_LENGTH = "300ms";
 
 const styles = StyleSheet.create({
     heading: {
+        // As this is a grid item, it has a default minWidth of auto,
+        // which means it would grow to fit its content. minWidth 0 is
+        // necessary here to stop a custom header from overflowing out of
+        // it container when its content is too long (See AccordionSection's
+        // "React Element in Header" story).
+        minWidth: 0,
         marginTop: 0,
     },
     headerWrapper: {
