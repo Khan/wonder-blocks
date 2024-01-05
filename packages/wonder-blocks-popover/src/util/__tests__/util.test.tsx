77b8fc245409deb895189a2feaec588f973e9cfa
Juan Andrade <juan@khanacademy.org>
2024-01-05 17:32:17 -0500

    Don't return focus to trigger element in certain cases

---
+++ packages/wonder-blocks-popover/src/util/__tests__/util.test.tsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+import {render, screen} from "@testing-library/react";
+import {isFocusable} from "../util";
+
+describe("isFocusable", () => {
+    it("should mark a button as focusable", () => {
+        // Arrange
+        render(<button>Open popover</button>);
+
+        // Act
+        const result = isFocusable(screen.getByRole("button"));
+
+        // Assert
+        expect(result).toBe(true);
+    });
+
+    it("should mark a div as non-focusable", () => {
+        // Arrange
+        render(<div>placeholder</div>);
+
+        // Act
+        const result = isFocusable(screen.getByText("placeholder"));
+
+        // Assert
+        expect(result).toBe(false);
+    });
+
+    it("should mark a div with tabIndex greater than -1 as focusable", () => {
+        // Arrange
+        render(<div tabIndex={0}>placeholder</div>);
+
+        // Act
+        const result = isFocusable(screen.getByText("placeholder"));
+
+        // Assert
+        expect(result).toBe(true);
+    });
+});
