317c5a71f5e406eb6afc847a4627273965d2a65f
Juan Andrade <juan@khanacademy.org>
2024-02-01 11:56:50 -0500

    Add transform to migrate spacing to tokens

---
+++ wb-codemod/transforms/__tests__/migrate-spacing-to-tokens.test.ts
@@ -0,0 +1,31 @@
+import transform from "../migrate-spacing-to-tokens";
+
+// eslint-disable-next-line import/no-commonjs, @typescript-eslint/no-var-requires
+const defineInlineTest = require("jscodeshift/dist/testUtils").defineInlineTest;
+
+const transformOptions = {
+    printOptions: {
+        objectCurlySpacing: false,
+    },
+};
+
+describe("migrate-spacing-to-tokens", () => {
+    defineInlineTest(
+        transform,
+        transformOptions,
+        `import Spacing from "@khanacademy/wonder-blocks-spacing";`,
+        `import {spacing} from "@khanacademy/wonder-blocks-tokens";`,
+        "should replace default import with named import",
+    );
+
+    defineInlineTest(
+        transform,
+        transformOptions,
+        `
+import Spacing from "@khanacademy/wonder-blocks-spacing";
+import {color} from "@khanacademy/wonder-blocks-tokens";
+`,
+        `import {color, spacing} from "@khanacademy/wonder-blocks-tokens";`,
+        "should add a named import to the existing import declaration",
+    );
+});
