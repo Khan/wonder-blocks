be8cfa470efc7e712596ec6b34b0571fd5f3eb02
Juan Andrade <juan@khanacademy.org>
2024-02-05 10:19:50 -0500

    WB-1642.1: Add transform to migrate color to tokens (#2178)
    ## Summary:
    
    Creates a codemod to migrate the wb-color imports to wb-tokens. This will allow
    us to have a consistent spacing across the repo(s) and make it easier to change
    in the future.
    
    Issue: WB-1642
    
    ## Test plan:
    
    - Run the codemod on the repo (next PR).
    - Run unit tests for the codemod.
    
    Author: jandrade
    
    Reviewers: kevinbarabash, jandrade
    
    Required Reviewers:
    
    Approved By: kevinbarabash
    
    Checks: ✅ Chromatic - Get results on regular PRs (ubuntu-latest, 20.x), ✅ Test (ubuntu-latest, 20.x, 2/2), ✅ Check build sizes (ubuntu-latest, 20.x), ✅ Lint (ubuntu-latest, 20.x), ✅ Test (ubuntu-latest, 20.x, 1/2), ✅ Chromatic - Build on regular PRs / chromatic (ubuntu-latest, 20.x), ⏭️  Chromatic - Skip on Release PR (changesets), ✅ Publish npm snapshot (ubuntu-latest, 20.x), ✅ Prime node_modules cache for primary configuration (ubuntu-latest, 20.x), ✅ Check for .changeset entries for all changed files (ubuntu-latest, 20.x), ✅ gerald, ⏭️  dependabot
    
    Pull Request URL: https://github.com/Khan/wonder-blocks/pull/2178

---
+++ wb-codemod/transforms/migrate-color-to-tokens.ts
@@ -0,0 +1,141 @@
+import {
+    API,
+    FileInfo,
+    ImportSpecifier,
+    ModuleSpecifier,
+    Options,
+} from "jscodeshift";
+
+// From
+const SOURCE_IMPORT_DECLARATION = "@khanacademy/wonder-blocks-color";
+const SOURCE_SPECIFIER = "Color";
+// To
+const TARGET_IMPORT_DECLARATION = "@khanacademy/wonder-blocks-tokens";
+const TARGET_SPECIFIER = "color";
+
+/**
+ * Transforms: import Color from "@khanacademy/wonder-blocks-color" const
+ * bgColor = Color.blue;
+ *
+ * to: import {color} from "@khanacademy/wonder-blocks-tokens" const bgColor =
+ * color.blue;
+ *
+ * NOTE: This transform is similar to migrate-spacing-to-tokens.ts, but it
+ * differs in the way it replaces the usage of the default specifier. Spacing
+ * only exports a default specifier, while Color exports both a default and
+ * named specifier(s) like `mix` and `fade`.
+ */
+export default function transform(file: FileInfo, api: API, options: Options) {
+    const j = api.jscodeshift;
+    const root = j(file.source);
+
+    // Use the default specifier as the source specifier.
+    let sourceSpecifier = SOURCE_SPECIFIER;
+
+    // Step 1: Verify if the import exists
+    const sourceImport = root.find(j.ImportDeclaration, {
+        source: {value: SOURCE_IMPORT_DECLARATION},
+    });
+
+    // If the import doesn't exist, we don't need to do anything.
+    if (sourceImport.size() === 0) {
+        return;
+    }
+
+    // Step 2: Replace the import
+    const targetImport = root.find(j.ImportDeclaration, {
+        source: {value: TARGET_IMPORT_DECLARATION},
+    });
+
+    sourceImport.forEach((path) => {
+        // Find the default specifier
+        const defaultSpecifier = path.value.specifiers?.find(
+            (specifier) => specifier.type === "ImportDefaultSpecifier",
+        );
+
+        // If there's no default specifier, we don't need to do anything.
+        if (!defaultSpecifier) {
+            return;
+        }
+
+        // There might be cases where the default specifier uses a different
+        // name from the one defined in SOURCE_SPECIFIER, so we need to make
+        // sure we use the correct name to complete step 3.
+        if (defaultSpecifier?.local) {
+            sourceSpecifier = defaultSpecifier.local?.name;
+        }
+
+        // Remove the source specifier from the import declaration
+        const hasRemainingSpecifiers = path.value.specifiers?.some(
+            (specifier) => specifier.local?.name !== sourceSpecifier,
+        );
+
+        // Replace default import with named import
+        const targetSpecifier = j.importSpecifier(
+            j.identifier(TARGET_SPECIFIER),
+        );
+
+        // If the target import is already used, we add a named import to the
+        // existing import declaration.
+        if (targetImport.size() > 0) {
+            // NOTE: We only add the specifier if it doesn't exist already.
+            const specifierExistsInTarget = targetImport
+                .get()
+                .node.specifiers.some(
+                    (specifier: ModuleSpecifier) =>
+                        specifier.local?.name === TARGET_SPECIFIER,
+                );
+
+            if (!specifierExistsInTarget) {
+                targetImport.get().node.specifiers.push(targetSpecifier);
+                // sort the specifiers
+                targetImport
+                    .get()
+                    .node.specifiers.sort(
+                        (a: ImportSpecifier, b: ImportSpecifier) =>
+                            a.imported.name.localeCompare(b.imported.name),
+                    );
+            }
+
+            // Remove the "wonder-blocks-color" import declaration
+            if (!hasRemainingSpecifiers) {
+                j(path).remove();
+            } else {
+                // Remove the default specifier from the import declaration
+                path.value.specifiers = path.value.specifiers?.filter(
+                    (specifier) => specifier.local?.name !== sourceSpecifier,
+                );
+            }
+            return;
+        }
+
+        // The target import doesn't exist, so we create a new one.
+        const newTargetImport = j.importDeclaration(
+            [targetSpecifier],
+            j.literal(TARGET_IMPORT_DECLARATION),
+        );
+
+        // Color was the only specifier in the import declaration.
+        if (!hasRemainingSpecifiers) {
+            // Replace the existing import declaration with the new one.
+            j(path).replaceWith(newTargetImport);
+        } else {
+            // There are other specifiers in the import declaration (e.g. mix,
+            // fade).
+            // We keep the other specifiers and remove the default specifier.
+            path.value.specifiers = path.value.specifiers?.filter(
+                (specifier) => specifier.local?.name !== sourceSpecifier,
+            );
+            // Move the default specifier to a named import in the target import
+            // declaration
+            j(path).insertAfter(newTargetImport);
+        }
+    });
+
+    // Step 3: Replace the call sites that use the default specifier.
+    root.find(j.Identifier, {name: sourceSpecifier}).forEach((path) => {
+        path.node.name = TARGET_SPECIFIER;
+    });
+
+    return root.toSource(options.printOptions);
+}
