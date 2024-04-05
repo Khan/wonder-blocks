60374e6ee6491cf7772d420a89d6a9b54e676417
Juan Andrade <juan@khanacademy.org>
2024-04-05 18:53:42 -0400

    Remove wonder-blocks-spacing

--- packages/wonder-blocks-tokens/src/index.ts
+++ packages/wonder-blocks-tokens/src/index.ts
@@ -1,9 +1,7 @@
-// TODO(WB-1643): Move wonder-blocks-spacing to tokens
-import spacing from "@khanacademy/wonder-blocks-spacing";
-
 import {border} from "./tokens/border";
 import {color} from "./tokens/color";
 import {font} from "./tokens/font";
+import {spacing} from "./tokens/spacing";
 
 import {mix, fade} from "./util/utils";
 
