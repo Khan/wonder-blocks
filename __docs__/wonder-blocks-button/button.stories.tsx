44e311adea87689444363cf5f6e7227bf3fd4a00
Juan Andrade <juan@khanacademy.org>
2024-05-17 16:31:15 -0400

    Migrate Button to css-modules (hacky version)

--- __docs__/wonder-blocks-button/button.stories.tsx
+++ __docs__/wonder-blocks-button/button.stories.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import {StyleSheet} from "aphrodite";
+// import {StyleSheet} from "aphrodite";
 import type {Meta, StoryObj} from "@storybook/react";
 import {expect, fireEvent, userEvent, within} from "@storybook/test";
 
 import {MemoryRouter, Route, Switch} from "react-router-dom";
 
-import type {StyleDeclaration} from "aphrodite";
+// import type {StyleDeclaration} from "aphrodite";
 
 import pencilSimple from "@phosphor-icons/core/regular/pencil-simple.svg";
 import pencilSimpleBold from "@phosphor-icons/core/bold/pencil-simple-bold.svg";
@@ -26,6 +26,8 @@ import ComponentInfo from "../../.storybook/components/component-info";
 
 import ButtonArgTypes from "./button.argtypes";
 import {ThemeSwitcherContext} from "@khanacademy/wonder-blocks-theming";
+
+import styles from "./button.stories.module.css";
 
 /**
  * Reusable button component.
@@ -132,31 +134,31 @@ export const Tertiary: StoryComponentType = {
     },
 };
 
-export const styles: StyleDeclaration = StyleSheet.create({
-    row: {
-        flexDirection: "row",
-        alignItems: "center",
-        marginBottom: spacing.xSmall_8,
-    },
-    button: {
-        marginRight: spacing.xSmall_8,
-    },
-    truncatedButton: {
-        maxWidth: 200,
-        marginBottom: spacing.medium_16,
-    },
-    fillSpace: {
-        minWidth: 140,
-    },
-    example: {
-        background: color.offWhite,
-        padding: spacing.medium_16,
-    },
-    label: {
-        marginTop: spacing.large_24,
-        marginBottom: spacing.xSmall_8,
-    },
-});
+// export const styles: StyleDeclaration = StyleSheet.create({
+//     row: {
+//         flexDirection: "row",
+//         alignItems: "center",
+//         marginBottom: spacing.xSmall_8,
+//     },
+//     button: {
+//         marginRight: spacing.xSmall_8,
+//     },
+//     truncatedButton: {
+//         maxWidth: 200,
+//         marginBottom: spacing.medium_16,
+//     },
+//     fillSpace: {
+//         minWidth: 140,
+//     },
+//     example: {
+//         background: color.offWhite,
+//         padding: spacing.medium_16,
+//     },
+//     label: {
+//         marginTop: spacing.large_24,
+//         marginBottom: spacing.xSmall_8,
+//     },
+// });
 
 export const Variants: StoryComponentType = () => (
     <View style={{padding: spacing.medium_16, gap: spacing.medium_16}}>
