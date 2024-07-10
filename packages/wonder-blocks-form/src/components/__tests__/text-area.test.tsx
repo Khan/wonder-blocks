dc62a7a5cea6fd6f88a6bfb3bf10b0afdc3daa4b
Bea Esguerra <beaesguerra@khanacademy.org>
2024-07-10 11:15:24 -0600

    Add more base tests

--- packages/wonder-blocks-form/src/components/__tests__/text-area.test.tsx
+++ packages/wonder-blocks-form/src/components/__tests__/text-area.test.tsx
@@ -2,54 +2,125 @@ import * as React from "react";
 import {render, screen} from "@testing-library/react";
 
 import {RenderStateRoot} from "@khanacademy/wonder-blocks-core";
+import {userEvent} from "@testing-library/user-event";
 import TextArea from "../text-area";
 
 const defaultOptions = {
     wrapper: RenderStateRoot,
 };
 describe("TextArea", () => {
-    it("id prop is passed to textarea", async () => {
+    describe("Attributes", () => {
+        it("should use the id prop for the textarea", async () => {
+            // Arrange
+            const testId = "test-id";
+            // Act
+            render(
+                <TextArea id={testId} value="" onChange={() => {}} />,
+                defaultOptions,
+            );
+
+            // Assert
+            const textArea = await screen.findByRole("textbox");
+            expect(textArea).toHaveAttribute("id", testId);
+        });
+
+        it("should use an auto-generated id for the textarea when id prop is not set", async () => {
+            // Arrange
+
+            // Act
+            render(<TextArea value="" onChange={() => {}} />, defaultOptions);
+
+            // Assert
+            // Since the generated id is unique, we cannot know what it will be. We
+            // only test if the id attribute starts with "uid-", then followed by
+            // "text-field-" as the scope assigned to IDProvider.
+            const textArea = await screen.findByRole("textbox");
+            expect(textArea.getAttribute("id")).toMatch(/uid-text-area-.*$/);
+        });
+
+        it("should use the testId prop for the textarea element", async () => {
+            // Arrange
+            const testId = "test-id";
+            render(
+                <TextArea value="Text" onChange={() => {}} testId={testId} />,
+                defaultOptions,
+            );
+
+            // Act
+
+            // Assert
+            const textArea = await screen.findByRole("textbox");
+            expect(textArea).toHaveAttribute("data-testid", testId);
+        });
+
+        it("should set the placeholder when the prop when provided", async () => {
+            // Arrange
+            const placeholder = "Test placeholder";
+            render(
+                <TextArea
+                    placeholder={placeholder}
+                    value="Text"
+                    onChange={() => {}}
+                />,
+                defaultOptions,
+            );
+
+            // Act
+
+            // Assert
+            const textArea = await screen.findByRole("textbox");
+            expect(textArea).toHaveAttribute("placeholder", placeholder);
+        });
+
+        it("should set the disabled attribute when the disabled prop is true", async () => {
+            // Arrange
+            render(
+                <TextArea disabled={true} value="Text" onChange={() => {}} />,
+                defaultOptions,
+            );
+
+            // Act
+
+            // Assert
+            const textArea = await screen.findByRole("textbox");
+            expect(textArea).toBeDisabled();
+        });
+    });
+
+    it("should use the value prop", async () => {
         // Arrange
-        const testId = "test-id";
-        // Act
+        const testValue = "test value";
         render(
-            <TextArea id={testId} value="" onChange={() => {}} />,
+            <TextArea value={testValue} onChange={() => {}} />,
             defaultOptions,
         );
 
-        // Assert
-        expect(await screen.findByRole("textbox")).toHaveAttribute(
-            "id",
-            testId,
-        );
-    });
-
-    it("auto-generated id is passed to textarea when id prop is not set", async () => {
-        // Arrange
-
         // Act
-        render(<TextArea value="" onChange={() => {}} />, defaultOptions);
 
         // Assert
-        // Since the generated id is unique, we cannot know what it will be. We
-        // only test if the id attribute starts with "uid-", then followed by
-        // "text-field-" as the scope assigned to IDProvider.
         const textArea = await screen.findByRole("textbox");
-        expect(textArea.getAttribute("id")).toMatch(/uid-text-area-.*$/);
+        expect(textArea).toHaveValue(testValue);
     });
 
-    it("testId prop is passed to the input element", async () => {
-        // Arrange
-        const testId = "test-id";
-        render(
-            <TextArea value="Text" onChange={() => {}} testId={testId} />,
-            defaultOptions,
-        );
+    describe("Event Handlers", () => {
+        it("should call the onChange prop when the textArea value changes", async () => {
+            // Arrange
+            const onChangeMock = jest.fn();
+            render(
+                <TextArea value="" onChange={onChangeMock} />,
+                defaultOptions,
+            );
 
-        // Act
+            // Act
+            // Type one letter
+            const letterToType = "X";
+            await userEvent.type(
+                await screen.findByRole("textbox"),
+                letterToType,
+            );
 
-        // Assert
-        const input = await screen.findByRole("textbox");
-        expect(input).toHaveAttribute("data-testid", testId);
+            // Assert
+            expect(onChangeMock).toHaveBeenCalledOnceWith(letterToType);
+        });
     });
 });
