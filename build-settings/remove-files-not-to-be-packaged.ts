eefa362f1c4afafc48707d9e4113874064816842
Jeff Yates <jeff@khanacademy.org>
2024-08-29 17:04:03 -0500

    [fei5815.1.removetsinfo] add some commentary

--- build-settings/remove-files-not-to-be-packaged.ts
+++ build-settings/remove-files-not-to-be-packaged.ts
@@ -9,6 +9,12 @@ const gitIgnorePath = path.join(rootDir, ".gitignore");
 const npmIgnorePath = path.join(rootDir, ".npmignore");
 
 // Now we loop each line of npmIgnore and delete the files that match.
+// NOTE: We really want to exclude everything listed in the .npmignore file
+// but that might include source-controlled files. So, we also check
+// the .gitignore file to see if it's matched there as well before we delete
+// anything. Ideally, changeset publish will honour the .npmignore file
+// and not include those files in the package - if that starts happening,
+// we can probably just remove this script.
 const npmIgnoreContent = fs.readFileSync(npmIgnorePath, "utf-8");
 const npmIgnorer = ignore().add(npmIgnoreContent);
 const gitIgnoreContent = fs.readFileSync(gitIgnorePath, "utf-8");
