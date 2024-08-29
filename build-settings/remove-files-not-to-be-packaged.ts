940273af42be51ea2904a3a1169bc932a1d09162
Jeff Yates <jeff@khanacademy.org>
2024-08-29 16:56:32 -0500

    [fei5815.1.removetsinfo] Small tweaks

--- build-settings/remove-files-not-to-be-packaged.ts
+++ build-settings/remove-files-not-to-be-packaged.ts
@@ -14,18 +14,19 @@ const npmIgnorer = ignore().add(npmIgnoreContent);
 const gitIgnoreContent = fs.readFileSync(gitIgnorePath, "utf-8");
 const gitIgnorer = ignore().add(gitIgnoreContent);
 
-const packagePaths = fglob.sync("packages/wonder-blocks-*", {
+const packagePaths = fglob.sync("packages/wonder-blocks-*/**/*", {
     cwd: rootDir,
     onlyFiles: false,
 });
 
 for (const fsPath of packagePaths) {
     const absPath = path.join(rootDir, fsPath);
-    if (npmIgnorer.ignores(fsPath) && gitIgnorer.ignores(fsPath)) {
-        if (fs.lstatSync(absPath).isDirectory()) {
-            fs.rmdirSync(absPath, {recursive: true});
-        } else {
-            fs.unlinkSync(absPath);
-        }
+    if (
+        fs.existsSync(fsPath) &&
+        npmIgnorer.ignores(fsPath) &&
+        gitIgnorer.ignores(fsPath)
+    ) {
+        console.log(`Removing ${fsPath}`);
+        fs.rmSync(absPath, {recursive: true});
     }
 }
