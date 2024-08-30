02a1b298e64228da324a0dca3ab2645dd942ef09
Jeff Yates <jeff@khanacademy.org>
2024-08-30 14:55:28 -0500

    [fei5815.1.removetsinfo] Add .npmignore so we stop packaging ts config and build info (#2312)
    ## Summary:
    Our packages are shipping the tsconfig and tsbuildinfo files ([example][1]) - this is not necessary. This change adds an `.npmignore` file to prevent these files from being included in the packages.
    
    However, our current publishing approach doesn't seem to honour that ignore file even though `npm pack --dry-run` does. So, this also updates the tooling to remove files according to whether they are ignored by both the `.npmignore` file and the `.gitignore` file (to avoid deleting source-controlled files). In addition, each package.json now explicitly lists what files it should be including.
    
    This combination means we can easily exclude files and folders in the `dist` location, without having really explicit `files` definitions that might be hard to maintain. 
    
    Issue: FEI-5815
    
    ## Test plan:
    Check the pre-release packages to see that they no longer include these files.
    
    [1]:https://www.npmjs.com/package/@khanacademy/wonder-blocks-core/v/7.0.0?activeTab=code
    
    Author: somewhatabstract
    
    Reviewers: jeresig
    
    Required Reviewers:
    
    Approved By: jeresig
    
    Checks: ✅ Chromatic - Get results on regular PRs (ubuntu-latest, 20.x), ✅ Test (ubuntu-latest, 20.x, 2/2), ✅ Check build sizes (ubuntu-latest, 20.x), ✅ Test (ubuntu-latest, 20.x, 1/2), ✅ Lint (ubuntu-latest, 20.x), ✅ Chromatic - Build on regular PRs / chromatic (ubuntu-latest, 20.x), ✅ Publish npm snapshot (ubuntu-latest, 20.x), ⏭️  Chromatic - Skip on Release PR (changesets), ✅ Prime node_modules cache for primary configuration (ubuntu-latest, 20.x), ✅ Check for .changeset entries for all changed files (ubuntu-latest, 20.x), ✅ gerald, ⏭️  dependabot
    
    Pull Request URL: https://github.com/Khan/wonder-blocks/pull/2312

--- packages/wonder-blocks-cell/src/index.ts
+++ packages/wonder-blocks-cell/src/index.ts
@@ -1,4 +1,6 @@
 import CompactCell from "./components/compact-cell";
 import DetailCell from "./components/detail-cell";
 
+export type {HorizontalRuleVariant, TypographyText} from "./util/types";
+
 export {CompactCell, DetailCell};
