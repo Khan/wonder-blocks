61144867d0d42a6fc3babcb51b4e9632ee6126ef
Nisha Yerunkar <nisha@khanacademy.org>
2024-10-23 19:21:56 -0700

    add combobox tile

--- __docs__/components/gallery/component-gallery.tsx
+++ __docs__/components/gallery/component-gallery.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import {RenderStateRoot, View} from "@khanacademy/wonder-blocks-core";
+import {View} from "@khanacademy/wonder-blocks-core";
 
 import AccordionTile from "./tiles/accordion-tile";
 import AccordionSectionTile from "./tiles/accordion-section-tile";
@@ -9,26 +9,27 @@ import BannerTile from "./tiles/banner-tile";
 import BirthdayPickerTile from "./tiles/birthday-picker-tile";
 import BreadcrumbsTile from "./tiles/breadcrumbs-tile";
 import ButtonTile from "./tiles/button-tile";
+import CheckboxTile from "./tiles/checkbox-tile";
+import CheckboxGroupTile from "./tiles/checkbox-group-tile";
+import ComboboxTile from "./tiles/combobox-tile";
 import CompactCellTile from "./tiles/compact-cell-tile";
 import DetailCellTile from "./tiles/detail-cell-tile";
 import IconButtonTile from "./tiles/icon-button-tile";
 import IconTile from "./tiles/icon-tile";
+import LabeledTextFieldTile from "./tiles/labeled-text-field-tile";
 import LinkTile from "./tiles/link-tile";
 import ModalTile from "./tiles/modal-tile";
 import MultiSelectTile from "./tiles/multi-select-tile";
 import PillTile from "./tiles/pill-tile";
 import PopoverTile from "./tiles/popover-tile";
 import ProgressSpinnerTile from "./tiles/progress-spinner-tile";
+import RadioGroupTile from "./tiles/radio-group-tile";
 import SearchFieldTile from "./tiles/search-field-tile";
 import SingleSelectTile from "./tiles/single-select-tile";
 import SwitchTile from "./tiles/switch-tile";
+import TextFieldTile from "./tiles/text-field-tile";
 import ToolbarTile from "./tiles/toolbar-tile";
 import TooltipTile from "./tiles/tooltip-tile";
-import CheckboxTile from "./tiles/checkbox-tile";
-import CheckboxGroupTile from "./tiles/checkbox-group-tile";
-import RadioGroupTile from "./tiles/radio-group-tile";
-import TextFieldTile from "./tiles/text-field-tile";
-import LabeledTextFieldTile from "./tiles/labeled-text-field-tile";
 
 export default function ComponentGallery() {
     return (
@@ -44,6 +45,7 @@ export default function ComponentGallery() {
             <ActionMenuTile />
             <MultiSelectTile />
             <SingleSelectTile />
+            <ComboboxTile />
             <CheckboxTile />
             <CheckboxGroupTile />
             <RadioGroupTile />
---
+++ __docs__/components/gallery/tiles/combobox-tile.tsx
@@ -0,0 +1,64 @@
+import * as React from "react";
+import {RenderStateRoot, View} from "@khanacademy/wonder-blocks-core";
+
+import ComponentTile from "../component-tile";
+import {Combobox, OptionItem} from "@khanacademy/wonder-blocks-dropdown";
+
+type MaybeString = string | null | undefined;
+type MaybeValueOrValues = MaybeString | Array<MaybeString>;
+
+export default function ComboboxTile() {
+    const [currentValue, setCurrentValue] =
+        React.useState<MaybeValueOrValues>("");
+
+    return (
+        <RenderStateRoot>
+            <ComponentTile
+                name="Combobox"
+                href="/?path=/docs/packages-dropdown-combobox--docs"
+                description={`A Combobox is an input widget that has an
+                    associated listbox. This listbox enables users to
+                    choose one or more values for the input from a
+                    collection of option items.`}
+            >
+                <View>
+                    <Combobox
+                        value={currentValue}
+                        placeholder="Select an item"
+                        selectionType="single"
+                        onChange={(newValue: MaybeValueOrValues) => {
+                            setCurrentValue(newValue);
+                        }}
+                    >
+                        <OptionItem label="Banana" value="banana" key={0} />
+                        <OptionItem
+                            label="Strawberry"
+                            value="strawberry"
+                            disabled
+                            key={1}
+                        />
+
+                        <OptionItem label="Pear" value="pear" key={2} />
+                        <OptionItem
+                            label="Pineapple"
+                            value="pineapple"
+                            key={3}
+                        />
+
+                        <OptionItem label="Orange" value="orange" key={4} />
+                        <OptionItem
+                            label="Watermelon"
+                            value="watermelon"
+                            key={5}
+                        />
+
+                        <OptionItem label="Apple" value="apple" key={6} />
+                        <OptionItem label="Grape" value="grape" key={7} />
+                        <OptionItem label="Lemon" value="lemon" key={8} />
+                        <OptionItem label="Mango" value="mango" key={9} />
+                    </Combobox>
+                </View>
+            </ComponentTile>
+        </RenderStateRoot>
+    );
+}
