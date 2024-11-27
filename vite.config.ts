6d829151183e92384012cb030beca0df962c09f1
Juan Andrade <juan@khanacademy.org>
2024-11-27 10:16:54 -0500

    More lint fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import react from "@vitejs/plugin-react";
 import {resolve} from "path";
+import react from "@vitejs/plugin-react";
 import {defineConfig} from "vite";
 
 export default defineConfig({
