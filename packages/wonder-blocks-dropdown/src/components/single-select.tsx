54c0dfcebd9a6bb9b60f45d13e16527aa05d1cbc
Bea Esguerra <beaesguerra@khanacademy.org>
2024-11-26 17:29:41 -0700

    update story docs for props and be more explicit about when validation is triggered

--- packages/wonder-blocks-dropdown/src/components/single-select.tsx
+++ packages/wonder-blocks-dropdown/src/components/single-select.tsx
@@ -321,8 +321,8 @@ const SingleSelect = (props: Props) => {
         if (onToggle) {
             onToggle(opened);
         }
-        if (!opened && required) {
-            // If closed and field is required, validate the selected value
+        if (!opened && required && !selectedValue) {
+            // If closed, field is required, and no value is selected, validate
             handleValidation(selectedValue);
         }
     };
@@ -376,6 +376,7 @@ const SingleSelect = (props: Props) => {
             onToggle(false);
         }
 
+        // Validate when a value is selected
         handleValidation(newSelectedValue);
     };
 
@@ -449,9 +450,10 @@ const SingleSelect = (props: Props) => {
     };
 
     const handleOpenerBlur = () => {
-        if (!open && required) {
-            // Only validate on opener blur if the dropdown is closed. This
-            // prevents an error when the dropdown is opened without a value yet
+        if (!open && required && !selectedValue) {
+            // Only validate on opener blur if the dropdown is closed, the field
+            // is required, and no value is selected. This prevents an error when
+            // the dropdown is opened without a value yet.
             handleValidation(selectedValue);
         }
     };
