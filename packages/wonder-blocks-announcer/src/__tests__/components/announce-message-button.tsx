c0e3d06306c64af8e9b379d59291788c990f7653
Marcy Sutton Todd <marcytodd@khanacademy.org>
2024-12-10 14:35:12 -0800

    Remove commented-out test code

--- packages/wonder-blocks-announcer/src/__tests__/components/announce-message-button.tsx
+++ packages/wonder-blocks-announcer/src/__tests__/components/announce-message-button.tsx
@@ -1,6 +1,4 @@
 import * as React from "react";
-// TODO: fix dupMap.get is not a function issue
-// import Button from "@khanacademy/wonder-blocks-button";
 import {announceMessage} from "../../announce-message";
 import {type AnnounceMessageProps} from "../../announce-message";
 
