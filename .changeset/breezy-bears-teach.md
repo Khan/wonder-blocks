9a7f220ab43e2fc1062559b7a5cce7b748bf665c
Bea Esguerra <beaesguerra@users.noreply.github.com>
2024-12-10 13:29:29 -0700

    SearchField: Add validation props and refine states (#2363)
    ## Summary:
    To make the SearchField component more consistent with other form field components like TextField and TextArea, we add validation related props to the component. See [LabeledField Implementation Spec](https://khanacademy.atlassian.net/wiki/spaces/WB/pages/2261352551/LabeledField+Implementation+Spec) for more details!
    
    The changes include:
    - Add error, instantValidation, validate, onValidate props to SearchField
    - Minor tweaks
      - Hide clear icon button if the field is disabled
      - Refine magnifying glass icon styling to make it [match Figma component](https://www.figma.com/design/VbVu3h2BpBhH80niq101MHHE/%F0%9F%92%A0-Main-Components?node-id=13840-8605&t=83MBW2IFJ6nx42vp-4) more (smaller, bold icon, spacing, update disabled state)
    - Storybook:
      - Add error and validation stories for SearchField
      - Add variant stories for SearchField states
    
    Issue: WB-1761
    
    ## Test plan:
    - Review Storybook docs for Error and Validation stories
      - `?path=/docs/packages-searchfield--docs#error`
      - `?path=/docs/packages-searchfield--docs#validation`
    - Review prop documentation for SearchField (`?path=/docs/packages-searchfield--docs`)
    - Review new Search Field All Variants stories (`?path=/docs/packages-searchfield-all-variants--docs`)
    - Review Chromatic diff for styling updates (left some comments in the UI Tests!)
    
    Author: beaesguerra
    
    Reviewers: beaesguerra, jandrade, marcysutton
    
    Required Reviewers:
    
    Approved By: jandrade, marcysutton
    
    Checks: ✅ Chromatic - Get results on regular PRs (ubuntu-latest, 20.x), ✅ Test / Test (ubuntu-latest, 20.x, 2/2), ✅ Test / Test (ubuntu-latest, 20.x, 1/2), ✅ Lint / Lint (ubuntu-latest, 20.x), ✅ Check build sizes (ubuntu-latest, 20.x), ✅ Publish npm snapshot (ubuntu-latest, 20.x), ✅ Chromatic - Build on regular PRs / chromatic (ubuntu-latest, 20.x), ✅ Prime node_modules cache for primary configuration (ubuntu-latest, 20.x), ⏭️  Chromatic - Skip on Release PR (changesets), ✅ Check for .changeset entries for all changed files (ubuntu-latest, 20.x), ✅ gerald, ⏭️  dependabot
    
    Pull Request URL: https://github.com/Khan/wonder-blocks/pull/2363

---
+++ .changeset/breezy-bears-teach.md
@@ -0,0 +1,9 @@
+---
+"@khanacademy/wonder-blocks-search-field": minor
+---
+
+# SearchField
+
+- Adds `error`, `instantValidation`, `validate`, and `onValidate` props to be consistent with form components.
+- Refine magnifying glass icon styling to make it match Figma more (smaller, bold icon, spacing, update disabled state)
+- Hide the clear button if the SearchField is disabled
---
+++ __docs__/wonder-blocks-search-field/search-field-variants.stories.tsx
@@ -0,0 +1,166 @@
+import * as React from "react";
+import {StyleSheet} from "aphrodite";
+import type {Meta, StoryObj} from "@storybook/react";
+
+import {View} from "@khanacademy/wonder-blocks-core";
+import {color, spacing} from "@khanacademy/wonder-blocks-tokens";
+import {LabelLarge, LabelMedium} from "@khanacademy/wonder-blocks-typography";
+import SearchField from "@khanacademy/wonder-blocks-search-field";
+
+/**
+ * The following stories are used to generate the pseudo states for the
+ * SearchField component. This is only used for visual testing in Chromatic.
+ */
+export default {
+    title: "Packages / SearchField / All Variants",
+    parameters: {
+        docs: {
+            autodocs: false,
+        },
+    },
+} as Meta;
+
+type StoryComponentType = StoryObj<typeof SearchField>;
+
+const longText =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.";
+const longTextWithNoWordBreak =
+    "Loremipsumdolorsitametconsecteturadipiscingelitseddoeiusmodtemporincididuntutlaboreetdoloremagnaaliqua";
+
+const states = [
+    {
+        label: "Default",
+        props: {},
+    },
+    {
+        label: "Disabled",
+        props: {disabled: true},
+    },
+    {
+        label: "Error",
+        props: {error: true},
+    },
+];
+const States = (props: {
+    light: boolean;
+    label: string;
+    value?: string;
+    placeholder?: string;
+}) => {
+    return (
+        <View
+            style={[props.light && styles.darkDefault, styles.statesContainer]}
+        >
+            <LabelLarge style={props.light && {color: color.white}}>
+                {props.label}
+            </LabelLarge>
+            <View style={[styles.scenarios]}>
+                {states.map((scenario) => {
+                    return (
+                        <View style={styles.scenario} key={scenario.label}>
+                            <LabelMedium
+                                style={props.light && {color: color.white}}
+                            >
+                                {scenario.label}
+                            </LabelMedium>
+                            <SearchField
+                                value=""
+                                onChange={() => {}}
+                                {...props}
+                                {...scenario.props}
+                            />
+                        </View>
+                    );
+                })}
+            </View>
+        </View>
+    );
+};
+
+const AllVariants = () => (
+    <View>
+        {[false, true].map((light) => {
+            return (
+                <React.Fragment key={`light-${light}`}>
+                    <States light={light} label="Default" />
+                    <States light={light} label="With Value" value="Text" />
+                    <States
+                        light={light}
+                        label="With Value (long)"
+                        value={longText}
+                    />
+                    <States
+                        light={light}
+                        label="With Value (long, no word breaks)"
+                        value={longTextWithNoWordBreak}
+                    />
+                    <States
+                        light={light}
+                        label="With Placeholder"
+                        placeholder="Placeholder text"
+                    />
+                    <States
+                        light={light}
+                        label="With Placeholder (long)"
+                        placeholder={longText}
+                    />
+                    <States
+                        light={light}
+                        label="With Placeholder (long, no word breaks)"
+                        placeholder={longTextWithNoWordBreak}
+                    />
+                </React.Fragment>
+            );
+        })}
+    </View>
+);
+
+export const Default: StoryComponentType = {
+    render: AllVariants,
+};
+
+/**
+ * There are currently only hover styles on the clear button.
+ */
+export const Hover: StoryComponentType = {
+    render: AllVariants,
+    parameters: {pseudo: {hover: true}},
+};
+
+export const Focus: StoryComponentType = {
+    render: AllVariants,
+    parameters: {pseudo: {focusVisible: true}},
+};
+
+export const HoverFocus: StoryComponentType = {
+    name: "Hover + Focus",
+    render: AllVariants,
+    parameters: {pseudo: {hover: true, focusVisible: true}},
+};
+
+/**
+ * There are currently no active styles.
+ */
+export const Active: StoryComponentType = {
+    render: AllVariants,
+    parameters: {pseudo: {active: true}},
+};
+
+const styles = StyleSheet.create({
+    darkDefault: {
+        backgroundColor: color.darkBlue,
+    },
+    statesContainer: {
+        padding: spacing.medium_16,
+    },
+    scenarios: {
+        flexDirection: "row",
+        alignItems: "center",
+        gap: spacing.xxxLarge_64,
+        flexWrap: "wrap",
+    },
+    scenario: {
+        gap: spacing.small_12,
+        overflow: "hidden",
+    },
+});
