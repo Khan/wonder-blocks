698665eae2b7448388db309dc593489df025a54e
Marcy Sutton Todd <marcytodd@khanacademy.org>
2024-12-10 14:15:16 -0800

    Remove console.log

--- packages/wonder-blocks-announcer/src/announce-message.ts
+++ packages/wonder-blocks-announcer/src/announce-message.ts
@@ -26,7 +26,6 @@ export function announceMessage({
 
     if (initialTimeout > 0) {
         setTimeout(() => {
-            console.log("set timeout", initialTimeout);
             return announcer.announce(message, level, debounceThreshold);
         }, initialTimeout);
     } else {
