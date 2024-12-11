c692276ed529b08d72067a1bfaab5abf44f22f9e
Jeff Yates <jeff@khanacademy.org>
2024-12-11 15:02:19 -0600

    [wb1812.3.migratewb] Migrate uses of IDProvider

--- packages/wonder-blocks-popover/src/components/popover.tsx
+++ packages/wonder-blocks-popover/src/components/popover.tsx
@@ -1,8 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-// eslint-disable-next-line import/no-deprecated
-import {IDProvider} from "@khanacademy/wonder-blocks-core";
+import {Id} from "@khanacademy/wonder-blocks-core";
 import {TooltipPopper} from "@khanacademy/wonder-blocks-tooltip";
 import {maybeGetPortalMountedModalHostElement} from "@khanacademy/wonder-blocks-modal";
 
@@ -390,7 +389,7 @@ export default class Popover extends React.Component<Props, State> {
                     placement: placement,
                 }}
             >
-                <IDProvider id={id} scope="popover">
+                <Id id={id}>
                     {(uniqueId) => (
                         <React.Fragment>
                             <PopoverAnchor
@@ -405,7 +404,7 @@ export default class Popover extends React.Component<Props, State> {
                             {this.renderPortal(uniqueId, opened)}
                         </React.Fragment>
                     )}
-                </IDProvider>
+                </Id>
 
                 {dismissEnabled && opened && (
                     <PopoverEventListener
