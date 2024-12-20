b3af2c9909b5065547ff531989cc40a652336caa
Kevin Barabash <kevinbarabash@khanacademy.org>
2024-12-20 15:02:04 -0500

    Update useOnMountEffect to pass isMountedRef to callback

--- packages/wonder-blocks-core/src/hooks/use-on-mount-effect.ts
+++ packages/wonder-blocks-core/src/hooks/use-on-mount-effect.ts
@@ -5,7 +5,7 @@ import * as React from "react";
  *
  * If the callback returns a cleanup function, it will be called when the component is unmounted.
  *
- * @param {() => (void | (() => void))} callback function that forces the component to update.
+ * @param {(isMountedRef: React.MutableRefObject<boolean>) => void | (() => void)} callback function that forces the component to update.
  *
  * The following code snippets are equivalent
  * ```
@@ -27,8 +27,42 @@ import * as React from "react";
  * }, []);
  *
  * If you only need to do something on mount, don't return a cleanup function from `callback`.
+ *
+ * If your callback is async, use the `isMountedRef` ref that's passed to the callback to ensure
+ * that the component using `useOnMountEffec` hasn't been unmounted, e.g.
+ *
+ * ```
+ * const MyComponent = () => {
+ *    const [foo, setFoo] = React.useState("");
+ *    useOnMountEffect(async (isMountedRef) => {
+ *        const action = async () => {
+ *            const res = await fetch("/foo");
+ *            const text = res.text();
+ *            if (isMountedRef.current) {
+ *                setFoo(text);
+ *            }
+ *        }
+ *
+ *        action();
+ *    });
+ *
+ *    return foo !== "" ? <h1>Loading...</h1> : <h1>{foo}</h1>;
+ * }
  */
-export const useOnMountEffect = (callback: () => void | (() => void)): void => {
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    React.useEffect(callback, []);
+export const useOnMountEffect = (
+    callback: (
+        isMountedRef: React.MutableRefObject<boolean>,
+    ) => void | (() => void),
+): void => {
+    const isMountedRef = React.useRef(true);
+
+    React.useEffect(() => {
+        const cleanup = callback(isMountedRef);
+
+        return () => {
+            cleanup?.();
+            isMountedRef.current = false;
+        };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 };
