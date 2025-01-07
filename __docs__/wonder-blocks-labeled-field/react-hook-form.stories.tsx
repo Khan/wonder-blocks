5646cbc74960d6b3c0a46ad3170ba93c0e27f7ff
Bea Esguerra <beaesguerra@khanacademy.org>
2025-01-07 16:10:51 -0700

    document stories

--- __docs__/wonder-blocks-labeled-field/react-hook-form.stories.tsx
+++ __docs__/wonder-blocks-labeled-field/react-hook-form.stories.tsx
@@ -89,6 +89,22 @@ type StoryArgs = {
     disableFields: boolean;
 };
 
+/**
+ * Example of using the Controller component from React Hook Form with controlled
+ * UI components from Wonder Blocks.
+ *
+ * By default, validation is triggered on submit. To change validation mode,
+ * see other stories or update the story control (you'll need to refresh the page
+ * after though)
+ *
+ * To trigger error messages:
+ * - TextField, TextArea:
+ *   - No value (because it is required)
+ *   - Text is < 3 or > 5 characters
+ * - SingleSelect, MultiSelect, RadioGroup, CheckboxGroup:
+ *   - No selected value (because it is required)
+ *   - Include "Banana" in the selection
+ */
 export const ExampleController = (args: StoryArgs) => {
     const {
         handleSubmit,
@@ -269,6 +285,9 @@ export const ExampleController = (args: StoryArgs) => {
     );
 };
 
+/**
+ * Same example as above but using the useController hook instead.
+ */
 export const UseController = (args: StoryArgs) => {
     const {
         handleSubmit,
@@ -409,17 +428,97 @@ export const UseController = (args: StoryArgs) => {
     );
 };
 
+/**
+ * [React Hook Form modes](https://react-hook-form.com/docs/useform#mode)
+ */
 export const ValidationOnBlur = {
     render: UseController,
     args: {mode: "onBlur"},
 };
 
+/**
+ * [React Hook Form modes](https://react-hook-form.com/docs/useform#mode)
+ */
 export const ValidationOnTouched = {
     render: UseController,
     args: {mode: "onTouched"},
 };
 
+/**
+ * [React Hook Form modes](https://react-hook-form.com/docs/useform#mode)
+ */
 export const ValidationOnChange = {
     render: UseController,
     args: {mode: "onChange"},
+};
+
+/**
+ * When we set `required` as part of the form field rules for React Hook Form,
+ * it does not pass the required message to the field. We need to pass in information
+ * about the field being required to either LabeledField or the field (TextField)
+ * component so that the Labeledfield can show the required indicator (*) and
+ * the field can properly mark input with `aria-required`.
+ *
+ * When we pass in the required message to the field, since the fields also implemet
+ * their own validationg logic around the required prop, there could be a mix of
+ * validation logic occuring.
+ *
+ * In this example, the form is set to validate on blur and TextField is set to
+ * validate onChange (instantValidation=true). Because of this, when you erase
+ * the value, the field is in an error state and the error message isn't shown
+ * yet.
+ *
+ * We could simplify WB components and avoid a mix of validation logic by
+ * removing validation related props from the components and rely on RHF to
+ * handle validation. For the `required` case, we would still have the prop, but
+ * all it would need to do is mark the field as required without doing any validation.
+ */
+export const Required = {
+    render: function RequiredStory(args: StoryArgs) {
+        const {
+            handleSubmit,
+            formState: {errors},
+            control,
+        } = useForm<Inputs>({
+            mode: args.mode,
+            defaultValues,
+        });
+
+        const {field: tfField} = useController({
+            name: "exampleTextField",
+            control,
+            rules: textRules,
+            disabled: args.disableFields,
+        });
+
+        const onSubmit: SubmitHandler<Inputs> = (data) => {
+            console.log("successful submit", data.exampleTextField);
+        };
+
+        const onInvalidSubmit: SubmitErrorHandler<Inputs> = (data) => {
+            console.log("invalid submit", data);
+        };
+
+        return (
+            <form onSubmit={handleSubmit(onSubmit, onInvalidSubmit)}>
+                <View style={{gap: spacing.medium_16}}>
+                    <LabeledField
+                        label="TextField"
+                        description="Helpful description text."
+                        errorMessage={errors.exampleTextField?.message}
+                        field={
+                            <TextField
+                                {...tfField}
+                                required={textRules.required} // NOTE: explicitly map required message from rules since it is not passed by RHF. Required could also be set on LabeledField instead.
+                            />
+                        }
+                    />
+                    <Button type="submit" style={{marginTop: spacing.small_12}}>
+                        Submit
+                    </Button>
+                </View>
+            </form>
+        );
+    },
+    args: {mode: "onBlur"},
 };
