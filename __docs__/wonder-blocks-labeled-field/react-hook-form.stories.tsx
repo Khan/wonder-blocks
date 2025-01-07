288a4407c17ef262737bf8c919cd9e2261609d9e
Bea Esguerra <beaesguerra@khanacademy.org>
2025-01-07 14:33:14 -0700

    useController hook example

--- __docs__/wonder-blocks-labeled-field/react-hook-form.stories.tsx
+++ __docs__/wonder-blocks-labeled-field/react-hook-form.stories.tsx
@@ -4,6 +4,7 @@ import {
     SubmitHandler,
     Controller,
     SubmitErrorHandler,
+    useController,
 } from "react-hook-form";
 import {LabeledField} from "@khanacademy/wonder-blocks-labeled-field";
 import {
@@ -22,7 +23,7 @@ import {spacing} from "@khanacademy/wonder-blocks-tokens";
 import {View} from "@khanacademy/wonder-blocks-core";
 import Button from "@khanacademy/wonder-blocks-button";
 
-export default {title: "Packages / ReactHookForm"};
+export default {title: "Test/ReactHookForm"};
 
 type Inputs = {
     exampleTextField: string;
@@ -33,6 +34,39 @@ type Inputs = {
     exampleCheckboxGroup: string[];
 };
 
+const textRules = {
+    required: "This field is required",
+    maxLength: {value: 5, message: "Max length is 5"},
+    minLength: {value: 3, message: "Min length is 3"},
+};
+const selectedValueRules = {
+    required: "This field is required",
+    validate: (value: string) => {
+        if (value === "banana") {
+            return "Banana is not allowed";
+        }
+        return true;
+    },
+};
+
+const selectedValuesRules = {
+    required: "This field is required",
+    validate: (value: string[]) => {
+        if (value.includes("banana")) {
+            return "Banana is not allowed";
+        }
+        return true;
+    },
+};
+
+const defaultValues = {
+    exampleTextField: "textfield",
+    exampleTextArea: "textarea",
+    exampleSingleSelect: "banana",
+    exampleMultiSelect: ["banana"],
+    exampleCheckboxGroup: ["banana"],
+    exampleRadioGroup: "banana",
+};
 export const ExampleController = (args) => {
     const {
         handleSubmit,
@@ -41,14 +75,7 @@ export const ExampleController = (args) => {
     } = useForm<Inputs>({
         mode: "onTouched",
         // NOTE: comment out defaultValues to see behaviour when empty
-        defaultValues: {
-            exampleTextField: "textfield",
-            exampleTextArea: "textarea",
-            exampleSingleSelect: "banana",
-            exampleMultiSelect: ["banana"],
-            exampleCheckboxGroup: ["banana"],
-            exampleRadioGroup: "banana",
-        },
+        defaultValues,
     });
 
     const onSubmit: SubmitHandler<Inputs> = (data) => {
@@ -65,11 +92,7 @@ export const ExampleController = (args) => {
                 <Controller
                     name="exampleTextField"
                     control={control}
-                    rules={{
-                        required: "This field is required",
-                        maxLength: {value: 5, message: "Max length is 5"},
-                        minLength: {value: 3, message: "Min length is 3"},
-                    }}
+                    rules={textRules}
                     render={({field}) => {
                         return (
                             <LabeledField
@@ -84,11 +107,7 @@ export const ExampleController = (args) => {
                 <Controller
                     name="exampleTextArea"
                     control={control}
-                    rules={{
-                        required: "This field is required",
-                        maxLength: {value: 5, message: "Max length is 5"},
-                        minLength: {value: 3, message: "Min length is 3"},
-                    }}
+                    rules={textRules}
                     render={({field}) => {
                         return (
                             <LabeledField
@@ -103,15 +122,7 @@ export const ExampleController = (args) => {
                 <Controller
                     name="exampleSingleSelect"
                     control={control}
-                    rules={{
-                        required: "This field is required",
-                        validate: (value) => {
-                            if (value === "banana") {
-                                return "Banana is not allowed";
-                            }
-                            return true;
-                        },
-                    }}
+                    rules={selectedValueRules}
                     render={({field}) => {
                         return (
                             <LabeledField
@@ -147,15 +158,7 @@ export const ExampleController = (args) => {
                 <Controller
                     name="exampleMultiSelect"
                     control={control}
-                    rules={{
-                        required: "This field is required",
-                        validate: (value) => {
-                            if (value.includes("banana")) {
-                                return "Banana is not allowed";
-                            }
-                            return true;
-                        },
-                    }}
+                    rules={selectedValuesRules}
                     render={({field}) => {
                         return (
                             <LabeledField
@@ -190,15 +193,7 @@ export const ExampleController = (args) => {
                 <Controller
                     name="exampleRadioGroup"
                     control={control}
-                    rules={{
-                        required: "This field is required",
-                        validate: (value) => {
-                            if (value === "banana") {
-                                return "Banana is not allowed";
-                            }
-                            return true;
-                        },
-                    }}
+                    rules={selectedValueRules}
                     render={({field}) => {
                         return (
                             <RadioGroup
@@ -219,15 +214,7 @@ export const ExampleController = (args) => {
                 <Controller
                     name="exampleCheckboxGroup"
                     control={control}
-                    rules={{
-                        required: "This field is required",
-                        validate: (value) => {
-                            if (value.includes("banana")) {
-                                return "Banana is not allowed";
-                            }
-                            return true;
-                        },
-                    }}
+                    rules={selectedValuesRules}
                     render={({field, ...others}) => {
                         console.log("others", others);
                         return (
@@ -248,6 +235,141 @@ export const ExampleController = (args) => {
                         );
                     }}
                 />
+                <Button type="submit" style={{marginTop: spacing.small_12}}>
+                    Submit
+                </Button>
+            </View>
+        </form>
+    );
+};
+
+export const UseController = () => {
+    const {
+        handleSubmit,
+        formState: {errors},
+        control,
+    } = useForm<Inputs>({
+        mode: "onTouched",
+        // NOTE: comment out defaultValues to see behaviour when empty
+        defaultValues,
+    });
+
+    const {field: tfField} = useController({
+        name: "exampleTextField",
+        control,
+        rules: textRules,
+    });
+
+    const {field: taField} = useController({
+        name: "exampleTextArea",
+        control,
+        rules: textRules,
+    });
+
+    const {field: ssField} = useController({
+        name: "exampleSingleSelect",
+        control,
+        rules: selectedValueRules,
+    });
+
+    const {field: msField} = useController({
+        name: "exampleMultiSelect",
+        control,
+        rules: selectedValuesRules,
+    });
+
+    const {field: rgField} = useController({
+        name: "exampleRadioGroup",
+        control,
+        rules: selectedValueRules,
+    });
+
+    const {field: cbgField} = useController({
+        name: "exampleCheckboxGroup",
+        control,
+        rules: selectedValuesRules,
+    });
+
+    const onSubmit: SubmitHandler<Inputs> = (data) => {
+        console.log("successful submit", data.exampleTextField);
+    };
+
+    const onInvalidSubmit: SubmitErrorHandler<Inputs> = (data) => {
+        console.log("invalid submit", data);
+    };
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit, onInvalidSubmit)}>
+            <View style={{gap: spacing.medium_16}}>
+                <LabeledField
+                    label="TextField"
+                    description="Helpful description text."
+                    errorMessage={errors.exampleTextField?.message}
+                    field={<TextField {...tfField} />}
+                />
+                <LabeledField
+                    label="TextArea"
+                    description="Helpful description text."
+                    errorMessage={errors.exampleTextArea?.message}
+                    field={<TextArea {...taField} />}
+                />
+
+                <LabeledField
+                    label="SingleSelect"
+                    description="Helpful description text."
+                    errorMessage={errors.exampleSingleSelect?.message}
+                    field={
+                        <SingleSelect
+                            placeholder="Choose a fruit"
+                            {...ssField} // NOTE: field includes onBlur, which SingleSelect doesn't support currently
+                            selectedValue={ssField.value} // NOTE: Need to explicitly map since single select takes in selectedValue prop instead of value
+                        >
+                            <OptionItem label="Mango" value="mango" />
+                            <OptionItem label="Strawberry" value="strawberry" />
+                            <OptionItem label="Banana" value="banana" />
+                        </SingleSelect>
+                    }
+                />
+                <LabeledField
+                    label="MultiSelect"
+                    description="Helpful description text."
+                    errorMessage={errors.exampleMultiSelect?.message}
+                    field={
+                        <MultiSelect
+                            {...msField} // NOTE: field includes onBlur, which MultiSelect doesn't support currently
+                            selectedValues={msField.value} // NOTE: Need to explicitly map since single select takes in selectedValue prop instead of value
+                        >
+                            <OptionItem label="Mango" value="mango" />
+                            <OptionItem label="Strawberry" value="strawberry" />
+                            <OptionItem label="Banana" value="banana" />
+                        </MultiSelect>
+                    }
+                />
+
+                <RadioGroup
+                    label="RadioGroup"
+                    description="Helpful description text."
+                    groupName="radio-group"
+                    {...rgField}
+                    selectedValue={rgField.value}
+                    errorMessage={errors.exampleRadioGroup?.message}
+                >
+                    <Choice label="Mango" value="mango" />
+                    <Choice label="Strawberry" value="strawberry" />
+                    <Choice label="Banana" value="banana" />
+                </RadioGroup>
+                <CheckboxGroup
+                    label="CheckboxGroup"
+                    description="Helpful description text."
+                    groupName="checkbox-group"
+                    {...cbgField}
+                    selectedValues={cbgField.value}
+                    errorMessage={errors.exampleCheckboxGroup?.message}
+                >
+                    <Choice label="Mango" value="mango" />
+                    <Choice label="Strawberry" value="strawberry" />
+                    <Choice label="Banana" value="banana" />
+                </CheckboxGroup>
                 <Button type="submit" style={{marginTop: spacing.small_12}}>
                     Submit
                 </Button>
