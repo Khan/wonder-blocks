7800d3978e6ff2e464800d629272d19636c66d8a
Bea Esguerra <beaesguerra@khanacademy.org>
2025-01-08 16:28:49 -0700

    clear field error if value changes to an empty string. this clears an error when the field is cleared (including when using the clear button in the SearchField or backspacing the value until it's clear)

--- packages/wonder-blocks-form/src/hooks/use-field-validation.ts
+++ packages/wonder-blocks-form/src/hooks/use-field-validation.ts
@@ -72,6 +72,7 @@ export const useFieldValidation = ({
     required = false,
     instantValidation = true,
 }: FieldValidationProps) => {
+    const prevValue = React.useRef(value);
     const [errorMessage, setErrorMessage] = React.useState<string | null>(
         // Ensures error is updated on unmounted server-side renders
         // Pass in an initializer function so the validate prop is not called
@@ -140,6 +141,17 @@ export const useFieldValidation = ({
             handleValidation(value);
         }
     });
+
+    React.useEffect(() => {
+        // If it is not required, clear the error when the value is now empty
+        if (!required && prevValue.current !== value && value.length === 0) {
+            setErrorMessage(null);
+            if (onValidate) {
+                onValidate(null);
+            }
+        }
+        prevValue.current = value;
+    }, [onValidate, required, value]);
 
     return {
         errorMessage,
