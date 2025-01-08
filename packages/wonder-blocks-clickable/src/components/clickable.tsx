b05461e32c058918bdd4712eefe8ef32ffc30798
Juan Andrade <juan@khanacademy.org>
2025-01-08 11:46:50 -0500

    docs(changeset): Use semanticColor instead of color

--- packages/wonder-blocks-clickable/src/components/clickable.tsx
+++ packages/wonder-blocks-clickable/src/components/clickable.tsx
@@ -5,7 +5,7 @@ import {__RouterContext} from "react-router";
 
 import {addStyle} from "@khanacademy/wonder-blocks-core";
 import type {AriaProps, StyleType} from "@khanacademy/wonder-blocks-core";
-import {color} from "@khanacademy/wonder-blocks-tokens";
+import {color, semanticColor} from "@khanacademy/wonder-blocks-tokens";
 
 import getClickableBehavior from "../util/get-clickable-behavior";
 import type {ClickableRole, ClickableState} from "./clickable-behavior";
@@ -410,20 +410,22 @@ const styles = StyleSheet.create({
     },
     focused: {
         ":focus": {
-            outline: `solid 2px ${color.blue}`,
+            // TODO(juan): do we need to define a "focused" border color token?
+            outline: `solid 2px ${semanticColor.action.primary.default}`,
         },
     },
     focusedLight: {
-        outline: `solid 2px ${color.white}`,
+        outline: `solid 2px ${semanticColor.border.inverse}`,
     },
     disabled: {
-        color: color.offBlack32,
+        color: semanticColor.text.disabled,
         cursor: "not-allowed",
         ":focus": {
             outline: "none",
         },
         ":focus-visible": {
-            outline: `solid 2px ${color.blue}`,
+            // TODO(juan): do we need to define a "disabled" border color token?
+            outline: `solid 2px ${semanticColor.action.disabled.default}`,
         },
     },
 });
