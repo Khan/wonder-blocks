2f1111aed6c6c080023814e874b3f14e9ea29c46
Juan Andrade <juan@khanacademy.org@khanacademy.org>
2025-01-21 20:10:47 -0500

    [sb-85-a11y] Fix a11y config

--- .storybook/vitest.setup.ts
+++ .storybook/vitest.setup.ts
@@ -1,13 +1,13 @@
 import * as a11yAddonAnnotations from "@storybook/addon-a11y/preview";
 import {beforeAll} from "vitest";
 import {setProjectAnnotations} from "@storybook/react";
-import * as projectAnnotations from "./preview";
+import * as previewAnnotations from "./preview";
 
 // This is an important step to apply the right configuration when testing your stories.
 // More info at: https://storybook.js.org/docs/api/portable-stories/portable-stories-vitest#setprojectannotations
-const project = setProjectAnnotations([
+const annotations = setProjectAnnotations([
+    previewAnnotations,
     a11yAddonAnnotations,
-    projectAnnotations,
 ]);
 
-beforeAll(project.beforeAll);
+beforeAll(annotations.beforeAll);
