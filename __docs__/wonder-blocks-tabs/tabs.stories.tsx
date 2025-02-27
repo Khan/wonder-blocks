36c1944d9d9f27d4709cc50c022304cd08eb35ff
Bea Esguerra <beaesguerra@khanacademy.org>
2025-02-26 17:19:51 -0700

    [tabs-prototype] more use cases

--- __docs__/wonder-blocks-tabs/tabs.stories.tsx
+++ __docs__/wonder-blocks-tabs/tabs.stories.tsx
@@ -1,7 +1,12 @@
 import * as React from "react";
 import packageConfig from "../../packages/wonder-blocks-switch/package.json";
 import ComponentInfo from "../components/component-info";
-import {Test} from "../../packages/wonder-blocks-tabs/src/index";
+import {Tabs, Test} from "../../packages/wonder-blocks-tabs/src/index";
+import {PhosphorIcon} from "@khanacademy/wonder-blocks-icon";
+import {IconMappings} from "../wonder-blocks-icon/phosphor-icon.argtypes";
+import {spacing} from "@khanacademy/wonder-blocks-tokens";
+import Tooltip from "@khanacademy/wonder-blocks-tooltip";
+import {Popover, PopoverContent} from "@khanacademy/wonder-blocks-popover";
 
 export default {
     title: "Packages / Tabs",
@@ -17,4 +22,91 @@ export default {
 
 export const Default = () => {
     return <Test />;
+};
+const tabs = [
+    {
+        id: "tab-1",
+        label: <div>Tab 1</div>,
+        panel: <div>Panel 1</div>,
+    },
+    {
+        id: "tab-2",
+        label: <div>Tab 2</div>,
+        panel: <div>Panel 2</div>,
+    },
+    {
+        id: "tab-3",
+        label: <div>Tab 3</div>,
+        panel: <div>Panel 3</div>,
+    },
+];
+
+export const Example = () => {
+    const [selectedTabId, setSelectedTabId] = React.useState(tabs[0].id);
+    return (
+        <Tabs
+            aria-label="Tools"
+            tabs={tabs}
+            selectedTabId={selectedTabId}
+            onTabChange={setSelectedTabId}
+        />
+    );
+};
+
+export const TabsWithFlexibleContent = () => {
+    const [selectedTabId, setSelectedTabId] = React.useState(tabs[0].id);
+    const tabsWithFlexibleContent = [
+        {
+            id: "tab-1",
+            label: (
+                <div
+                    style={{
+                        display: "flex",
+                        alignItems: "center",
+                        gap: spacing.xxxSmall_4,
+                    }}
+                >
+                    <PhosphorIcon icon={IconMappings.calendar} size="small" />
+                    Tab with icon
+                </div>
+            ),
+            panel: <div>Panel 1</div>,
+        },
+        {
+            id: "tab-2",
+            label: (
+                <Tooltip content="This is a text tooltip">
+                    Tab with Tooltip
+                </Tooltip>
+            ),
+            panel: <div>Panel 2</div>,
+        },
+        {
+            id: "tab-3",
+            label: (
+                <Popover
+                    content={
+                        <PopoverContent
+                            title="Popover title"
+                            content="Content"
+                        />
+                    }
+                    opened={selectedTabId === "tab-3"}
+                >
+                    <div>Tab with Popover</div>
+                </Popover>
+            ),
+            panel: <div>Panel 3</div>,
+        },
+    ];
+    return (
+        <div>
+            <Tabs
+                aria-label="Tools"
+                tabs={tabsWithFlexibleContent}
+                selectedTabId={selectedTabId}
+                onTabChange={setSelectedTabId}
+            />
+        </div>
+    );
 };
