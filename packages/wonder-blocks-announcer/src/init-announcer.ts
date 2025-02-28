32d09b4159bcaef1e4ed9f53ff203dd53f929a7a
Marcy Sutton Todd <marcytodd@khanacademy.org>
2025-02-28 15:32:57 -0800

    [combobox-announcer] Add debounce wait prop to initializer

--- packages/wonder-blocks-announcer/src/init-announcer.ts
+++ packages/wonder-blocks-announcer/src/init-announcer.ts
@@ -1,10 +1,18 @@
 import Announcer from "./announcer";
 
+type InitAnnouncerProps = {
+    debounceThreshold?: number;
+};
+
 /**
  * Utility to inject Announcer on page load.
  * It can be called from useEffect or elsewhere to improve ARIA Live Region performance on the first announcement.
  * @returns {Announcer} The Announcer instance created.
  */
-export function initAnnouncer(): Announcer {
-    return Announcer.getInstance();
+export function initAnnouncer(props?: InitAnnouncerProps): Announcer {
+    const announcer = Announcer.getInstance();
+    if (props?.debounceThreshold !== undefined) {
+        announcer.updateWaitThreshold(props?.debounceThreshold);
+    }
+    return announcer;
 }
