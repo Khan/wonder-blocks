fc33746aa4889dab96b9e56a8e8745e73a425763
Juan Andrade <juan@khanacademy.org@khanacademy.org>
2025-03-04 15:47:25 -0500

    [tokens-sizing] Address feedback

--- packages/wonder-blocks-tokens/src/tokens/sizing.ts
+++ packages/wonder-blocks-tokens/src/tokens/sizing.ts
@@ -6,39 +6,41 @@ const baseline = 8;
 /**
  * Converts a number (px) to a rem value.
  */
-function convertUnitsToRem(value: number): string {
+function pxToRem(value: number): string {
     return `${value / baseline}rem`;
 }
 
 /**
- * Tokens that define the sizing of elements.
+ * Tokens that define the sizing of elements. These values are expressed in
+ * `rem` units.
  *
  * These tokens are multiples of 8px, the baseline for the design system.
  *
  * These tokens can be used for:
  * - Margin and padding
  * - Component sizes
+ * - Typography
  */
 export const sizing = {
-    size_0: convertUnitsToRem(0),
-    size_0125: convertUnitsToRem(1),
-    size_025: convertUnitsToRem(2),
-    size_050: convertUnitsToRem(4),
-    size_075: convertUnitsToRem(6),
-    size_100: convertUnitsToRem(8),
-    size_125: convertUnitsToRem(10),
-    size_150: convertUnitsToRem(12),
-    size_200: convertUnitsToRem(16),
-    size_225: convertUnitsToRem(18),
-    size_250: convertUnitsToRem(20),
-    size_300: convertUnitsToRem(24),
-    size_400: convertUnitsToRem(32),
-    size_500: convertUnitsToRem(40),
-    size_600: convertUnitsToRem(48),
-    size_700: convertUnitsToRem(56),
-    size_800: convertUnitsToRem(64),
-    size_900: convertUnitsToRem(72),
-    size_1000: convertUnitsToRem(80),
-    size_1100: convertUnitsToRem(88),
-    size_1200: convertUnitsToRem(96),
+    size_0: pxToRem(0),
+    size_0125: pxToRem(1),
+    size_025: pxToRem(2),
+    size_050: pxToRem(4),
+    size_075: pxToRem(6),
+    size_100: pxToRem(8),
+    size_125: pxToRem(10),
+    size_150: pxToRem(12),
+    size_200: pxToRem(16),
+    size_225: pxToRem(18),
+    size_250: pxToRem(20),
+    size_300: pxToRem(24),
+    size_400: pxToRem(32),
+    size_500: pxToRem(40),
+    size_600: pxToRem(48),
+    size_700: pxToRem(56),
+    size_800: pxToRem(64),
+    size_900: pxToRem(72),
+    size_1000: pxToRem(80),
+    size_1100: pxToRem(88),
+    size_1200: pxToRem(96),
 } as const;
