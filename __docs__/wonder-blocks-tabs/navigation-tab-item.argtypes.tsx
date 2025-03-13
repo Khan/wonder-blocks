bc94d7d07ee1f271a598b66ce0daee0d1634aa50
Bea Esguerra <beaesguerra@khanacademy.org>
2025-03-13 14:30:56 -0600

    [navigation-tabs/props] add comment about docs

--- __docs__/wonder-blocks-tabs/navigation-tab-item.argtypes.tsx
+++ __docs__/wonder-blocks-tabs/navigation-tab-item.argtypes.tsx
@@ -4,6 +4,9 @@ export default {
     children: {
         table: {
             type: {
+                // We put React.Element<Link> in the docs so consumers know to
+                // use it with a Link. In code, it's marked as React.Element
+                // since TS can't enforce what components are used as children
                 summary: "React.Element<Link> | (linkProps) => React.Element",
             },
         },
