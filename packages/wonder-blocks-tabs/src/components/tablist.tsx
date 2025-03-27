e9160ecd953f7db8ea99aa837c4a3628ab605c42
Bea Esguerra <beaesguerra@khanacademy.org>
2025-03-27 16:33:32 -0600

    [wb-1073-tabs-setup] minor styling

--- packages/wonder-blocks-tabs/src/components/tablist.tsx
+++ packages/wonder-blocks-tabs/src/components/tablist.tsx
@@ -1,10 +1,26 @@
+import {addStyle} from "@khanacademy/wonder-blocks-core";
+import {sizing} from "@khanacademy/wonder-blocks-tokens";
+import {StyleSheet} from "aphrodite";
 import * as React from "react";
 
 type Props = {
     children: React.ReactElement | Array<React.ReactElement>;
 };
 
+const StyledDiv = addStyle("div");
+
 export const Tablist = (props: Props) => {
     const {children} = props;
-    return <div role="tablist">{children}</div>;
+    return (
+        <StyledDiv role="tablist" style={styles.tablist}>
+            {children}
+        </StyledDiv>
+    );
 };
+
+const styles = StyleSheet.create({
+    tablist: {
+        display: "flex",
+        gap: sizing.size_200,
+    },
+});
