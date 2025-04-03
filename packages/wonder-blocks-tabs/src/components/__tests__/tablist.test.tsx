d6ce44647f2653a065383dae4ab1759d82d86aae
Bea Esguerra <beaesguerra@khanacademy.org>
2025-04-03 10:39:44 -0600

    [wb-1909-keyboard-nav] linting

--- packages/wonder-blocks-tabs/src/components/__tests__/tablist.test.tsx
+++ packages/wonder-blocks-tabs/src/components/__tests__/tablist.test.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import {render, screen} from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import {Tablist} from "../tablist";
 
 describe("Tablist", () => {
