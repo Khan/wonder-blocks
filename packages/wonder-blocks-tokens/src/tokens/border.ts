0b710308ee6369326c0103768b574bb544b95dfc
Juan Andrade <juan@khanacademy.org@khanacademy.org>
2025-04-11 18:47:32 -0400

    [tb-border-radius] Replace rem with px for better consistency across different root font-sizes

--- packages/wonder-blocks-tokens/src/tokens/border.ts
+++ packages/wonder-blocks-tokens/src/tokens/border.ts
@@ -1,14 +1,18 @@
-import {sizing} from "./sizing";
+import {sizing, remToPx} from "./sizing";
 
 export const border = {
     /**
      * Corner radius values for borders.
+     *
+     * NOTE: We use fixed values for the corner radius instead of using the
+     * regular `spacing` tokens to ensure that the corner radius is consistent
+     * across different root font sizes.
      */
     radius: {
         radius_0: sizing.size_0,
-        radius_040: sizing.size_040,
-        radius_080: sizing.size_080,
-        radius_120: sizing.size_120,
+        radius_040: remToPx(sizing.size_040),
+        radius_080: remToPx(sizing.size_080),
+        radius_120: remToPx(sizing.size_120),
         radius_full: "50%",
     },
     width: {
