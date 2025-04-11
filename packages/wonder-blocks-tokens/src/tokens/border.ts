363d3cb325e8267028ed2256628d84b38f55c565
Juan Andrade <juan@khanacademy.org@khanacademy.org>
2025-04-11 18:52:06 -0400

    [tb-border-radius] Also convert 0

--- packages/wonder-blocks-tokens/src/tokens/border.ts
+++ packages/wonder-blocks-tokens/src/tokens/border.ts
@@ -9,7 +9,7 @@ export const border = {
      * across different root font sizes.
      */
     radius: {
-        radius_0: sizing.size_0,
+        radius_0: remToPx(sizing.size_0),
         radius_040: remToPx(sizing.size_040),
         radius_080: remToPx(sizing.size_080),
         radius_120: remToPx(sizing.size_120),
