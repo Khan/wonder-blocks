fbc56382b89a7501f87ed0bfab043867ab3e5693
Marcy Sutton Todd <marcytodd@khanacademy.org>
2025-04-23 11:30:00 -0700

    [FEI-4065-rems] Clean up utils from merge

--- packages/wonder-blocks-tokens/src/util/color-utils.ts
+++ packages/wonder-blocks-tokens/src/util/color-utils.ts
@@ -96,24 +96,3 @@ export const mix = (color: string, background: string): string => {
         a: bgObj.a,
     });
 };
-/**
- * The baseline for the size tokens.
- */
-const baseline = 10;
-
-/**
- * Converts a number (px) to a rem value.
- */
-export function pxToRem(value: number): string {
-    return `${value / baseline}rem`;
-}
-
-/**
- * Converts a rem value to a number (px).
- * @param value The rem value to convert (includes the unit).
- * @returns A string with the px value.
- */
-export function remToPx(value: string): string {
-    const num = parseFloat(value);
-    return `${Math.round(num * baseline)}px`;
-}
