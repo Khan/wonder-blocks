8603cb40e771e1a56f89a7256fa1df13c0053802
Juan Andrade <juan@khanacademy.org@khanacademy.org>
2025-04-25 12:59:44 -0400

    [wb-swc] Switch Jest to use SWC

--- config/jest/test.transform.js
+++
@@ -1,7 +0,0 @@
-const babelJest = require("babel-jest").default;
-
-const babelConfig = require("../../build-settings/babel.config")({
-    env: () => true,
-});
-
-module.exports = babelJest.createTransformer(babelConfig);
