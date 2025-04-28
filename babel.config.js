62b587ab3f92c8bc96e667b48f60c7f246b0c9ea
Juan Andrade <juan@khanacademy.org>
2025-04-28 15:46:58 -0400

    [WB-1930] Internal: Switch repo to use SWC (instead of babel) (#2572)
    ## Summary:
    
    - Updates Storybook to use SWC
    - Switch Jest to use SWC
    - Switch ESLint to use SWC
    - Switch Rollup to use SWC
    - Removes `@babel/runtime` and remaining babel instances
    
    Issue: WB-1930
    
    ## Test plan:
    
    Verify that all the checks pass.
    
    Author: jandrade
    
    Reviewers: beaesguerra, jandrade, jeremywiebe, jeresig
    
    Required Reviewers:
    
    Approved By: beaesguerra, jeremywiebe, jeresig
    
    Checks: ✅ 12 checks were successful, ⏭️  2 checks have been skipped
    
    Pull Request URL: https://github.com/Khan/wonder-blocks/pull/2572

--- babel.config.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
-/* eslint-disable import/no-commonjs */
-/**
- * HACK(somewhatabstract):
- * Jest looks for the babel config in the cwd of the project and as such we
- * need a file to tell it where the config really lies or it would use it.
- */
-module.exports = require("./build-settings/babel.config");
