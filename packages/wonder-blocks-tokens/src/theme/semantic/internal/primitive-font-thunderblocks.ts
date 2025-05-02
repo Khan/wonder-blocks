dbedd97d1a5e9496ce4433f87b6af08991cbd337
Marcy Sutton Todd <marcytodd@khanacademy.org>
2025-05-02 11:41:39 -0700

    [font-theming] Add Plus Jakarta Sans font to TB theme

---
+++ packages/wonder-blocks-tokens/src/theme/semantic/internal/primitive-font-thunderblocks.ts
@@ -0,0 +1,50 @@
+import {sizing} from "../../primitive/sizing";
+
+export const fontFamily = {
+    sans: "Plus Jakarta Sans, serif",
+    serif: '"Noto Serif", serif',
+    mono: "Inconsolata, monospace",
+};
+
+export const fontWeight = {
+    light: 300,
+    regular: 400,
+    semi: 600, // only used in TB
+    bold: 700,
+    black: 900,
+};
+
+export const font = {
+    family: {
+        sans: fontFamily.sans,
+        serif: fontFamily.serif,
+        mono: fontFamily.mono,
+    },
+    size: {
+        xxxLarge: sizing.size_360,
+        xxLarge: sizing.size_280,
+        xLarge: sizing.size_240,
+        large: sizing.size_200,
+        xMedium: sizing.size_180, // only used in TB
+        medium: sizing.size_160,
+        small: sizing.size_140,
+        xSmall: sizing.size_120,
+    },
+    lineHeight: {
+        xxxLarge: sizing.size_400,
+        xxLarge: sizing.size_320,
+        xLarge: sizing.size_280,
+        large: sizing.size_240,
+        xMedium: sizing.size_220,
+        medium: sizing.size_200,
+        small: sizing.size_180,
+        xSmall: sizing.size_160,
+    },
+    weight: {
+        light: fontWeight.light,
+        regular: fontWeight.regular,
+        semi: fontWeight.semi,
+        bold: fontWeight.bold,
+        black: fontWeight.black,
+    },
+};
--- packages/wonder-blocks-tokens/src/theme/thunderblocks.ts
+++ packages/wonder-blocks-tokens/src/theme/thunderblocks.ts
@@ -2,7 +2,9 @@ import {mergeTheme} from "@khanacademy/wonder-blocks-theming";
 
 import defaultTheme from "./default";
 import {semanticColor} from "./semantic/semantic-color-thunderblocks";
+import {font} from "./semantic/internal/primitive-font-thunderblocks";
 
 export default mergeTheme(defaultTheme, {
     semanticColor,
+    font,
 });
