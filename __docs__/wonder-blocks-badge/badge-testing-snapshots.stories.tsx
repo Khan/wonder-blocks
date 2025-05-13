3dfdbe6852259fa8e144d21fc00fb233f28941f6
Bea Esguerra <beaesguerra@khanacademy.org>
2025-05-13 10:04:38 -0600

    [implement-badge] adding more scenarios to test

--- __docs__/wonder-blocks-badge/badge-testing-snapshots.stories.tsx
+++ __docs__/wonder-blocks-badge/badge-testing-snapshots.stories.tsx
@@ -6,6 +6,17 @@ import {Badge} from "@khanacademy/wonder-blocks-badge";
 import {IconMappings} from "../wonder-blocks-icon/phosphor-icon.argtypes";
 import {PhosphorIcon} from "@khanacademy/wonder-blocks-icon";
 import {commonStates, StateSheet} from "../components/state-sheet";
+import {ScenariosLayout} from "../components/scenarios-layout";
+import {
+    longText,
+    longTextWithNoWordBreak,
+} from "../components/text-for-testing";
+import {View} from "@khanacademy/wonder-blocks-core";
+import {border, semanticColor, sizing} from "@khanacademy/wonder-blocks-tokens";
+import {
+    multiColoredIcon,
+    singleColoredIcon,
+} from "../components/icons-for-testing";
 
 /**
  * Badges are visual indicators used to display concise information, such as
@@ -70,6 +81,109 @@ export const StateSheetStory: StoryComponentType = {
             >
                 {({props}) => <Badge {...props} />}
             </StateSheet>
+        );
+    },
+};
+
+export const Scenarios: StoryComponentType = {
+    render: () => {
+        const textScenarios = [
+            {
+                name: "Long label",
+                props: {
+                    label: longText,
+                },
+            },
+            {
+                name: "Long label with icon",
+                props: {
+                    label: longText,
+                    icon: <PhosphorIcon icon={IconMappings.cookie} />,
+                },
+            },
+            {
+                name: "Long label with no word break",
+                props: {
+                    label: longTextWithNoWordBreak,
+                },
+            },
+            {
+                name: "Long label with no word break with icon",
+                props: {
+                    label: longTextWithNoWordBreak,
+                    icon: <PhosphorIcon icon={IconMappings.cookie} />,
+                },
+            },
+        ];
+        const scenarios = [
+            {
+                name: "Custom svg icon with label",
+                props: {
+                    label: "Badge",
+                    icon: singleColoredIcon,
+                },
+            },
+            {
+                name: "Custom svg icon",
+                props: {
+                    icon: singleColoredIcon,
+                },
+            },
+            {
+                name: "Custom multi-colored svg icon with label",
+                props: {
+                    label: "Badge",
+                    icon: multiColoredIcon,
+                },
+            },
+            {
+                name: "Custom multi-colored svg icon",
+                props: {
+                    icon: multiColoredIcon, // TODO alt text
+                },
+            },
+            {
+                name: "Custom img tag icon",
+                props: {
+                    icon: (
+                        <img
+                            src={"/favicon.ico"}
+                            alt="Wonder Blocks"
+                            style={{height: "100%", width: "100%"}}
+                        />
+                    ),
+                },
+            },
+            {
+                name: "Custom img tag icon with label",
+                props: {
+                    label: "Badge",
+                    icon: (
+                        <img
+                            src={"/favicon.ico"}
+                            alt="Wonder Blocks"
+                            style={{height: "100%", width: "100%"}}
+                        />
+                    ),
+                },
+            },
+        ];
+        return (
+            <View style={{gap: sizing.size_080}}>
+                <View
+                    style={{
+                        width: "200px",
+                        border: `${border.width.thin} dashed ${semanticColor.border.primary}`,
+                    }}
+                >
+                    <ScenariosLayout scenarios={textScenarios}>
+                        {(props) => <Badge {...props} />}
+                    </ScenariosLayout>
+                </View>
+                <ScenariosLayout scenarios={scenarios}>
+                    {(props) => <Badge {...props} />}
+                </ScenariosLayout>
+            </View>
         );
     },
 };
