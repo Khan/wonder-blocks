7dd2969b21946ef5b2e140da8faaaf442bb407b0
Juan Andrade <juan@khanacademy.org@khanacademy.org>
2025-05-16 10:44:10 -0400

    [icon-button-tb] Add Thunder Blocks theme to IconButton

--- packages/wonder-blocks-icon-button/src/theme/default.ts
+++ packages/wonder-blocks-icon-button/src/theme/default.ts
@@ -1,33 +1,45 @@
-import {border} from "@khanacademy/wonder-blocks-tokens";
+import {border, sizing} from "@khanacademy/wonder-blocks-tokens";
 
-const theme = {
-    border: {
-        offset: {
-            primary: border.width.medium,
-            secondary: `calc(-1 * ${border.width.medium})`,
-            tertiary: `calc(-1 * ${border.width.medium})`,
+export default {
+    iconButton: {
+        root: {
+            border: {
+                offset: {
+                    primary: border.width.medium,
+                    secondary: `calc(-1 * ${border.width.medium})`,
+                    tertiary: `calc(-1 * ${border.width.medium})`,
+                },
+                width: {
+                    primary: {
+                        default: border.width.none,
+                        hover: border.width.medium,
+                        press: border.width.medium,
+                    },
+                    secondary: {
+                        default: border.width.thin,
+                        hover: border.width.medium,
+                        press: border.width.medium,
+                    },
+                    tertiary: {
+                        default: border.width.none,
+                        hover: border.width.medium,
+                        press: border.width.medium,
+                    },
+                },
+                radius: {
+                    default: border.radius.radius_040,
+                    hover: border.radius.radius_040,
+                    press: border.radius.radius_040,
+                },
+            },
         },
-        width: {
-            primary: {
-                default: border.width.none,
-                hover: border.width.medium,
-                press: border.width.medium,
+        icon: {
+            size: {
+                xsmall: sizing.size_160,
+                small: sizing.size_160,
+                medium: sizing.size_240,
+                large: sizing.size_240,
             },
-            secondary: {
-                default: border.width.thin,
-                hover: border.width.medium,
-                press: border.width.medium,
-            },
-            tertiary: {
-                default: border.width.none,
-                hover: border.width.medium,
-                press: border.width.medium,
-            },
-        },
-        radius: {
-            default: border.radius.radius_040,
         },
     },
 };
-
-export default theme;
