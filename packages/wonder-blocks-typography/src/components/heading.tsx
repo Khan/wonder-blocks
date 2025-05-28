960d6f5bb861e3d8234f03ecc7094dde4b4d7413
Marcy Sutton Todd <marcytodd@khanacademy.org>
2025-05-27 18:17:52 -0700

    [WB-1966] Set default tagNames for Headings

--- packages/wonder-blocks-typography/src/components/heading.tsx
+++ packages/wonder-blocks-typography/src/components/heading.tsx
@@ -3,33 +3,42 @@ import {PropsFor, Text} from "@khanacademy/wonder-blocks-core";
 import {font} from "@khanacademy/wonder-blocks-tokens";
 import styles from "../util/styles";
 
+const tagMap = {
+    xxlarge: "h1",
+    xlarge: "h2",
+    large: "h3",
+    medium: "h4",
+    small: "h4",
+} as const;
+
+type HeadingSize = keyof typeof tagMap;
+
 type Props = PropsFor<typeof Text> & {
-    size?: "small" | "medium" | "large" | "xlarge" | "xxlarge";
+    size?: HeadingSize;
     tag?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
     weight?: "semi" | "bold";
 };
 
-const Heading = React.forwardRef(function Heading(
-    {
-        size = "large",
-        weight = "bold",
-        style,
-        children,
-        tag = "h2",
-        ...otherProps
-    }: Props,
-    ref,
-) {
+const Heading = React.forwardRef(function Heading(props: Props, ref) {
+    const {size, weight = "bold", style, children, tag, ...otherProps} = props;
+
+    // Determine final size for style purposes
+    const finalSize = size ?? "large";
+
+    // Resolve tag: prefer explicit `tag`, fallback to size-based tag if size was provided, or "h2" if neither
+    const resolvedTag = tag ?? (size ? tagMap[size] : "h2");
+
     // map props to theme and global token defaults for CSS styles
     const themeHeading = {
-        fontSize: font.heading.size[size],
+        fontSize: font.heading.size[finalSize],
         fontWeight: font.weight[weight],
-        lineHeight: font.heading.lineHeight[size],
+        lineHeight: font.heading.lineHeight[finalSize],
     };
+
     return (
         <Text
             {...otherProps}
-            tag={tag}
+            tag={resolvedTag}
             style={[styles.Heading, themeHeading, style]}
             ref={ref}
         >
