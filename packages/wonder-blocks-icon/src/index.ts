d1ad8754986c38295bff090d76d01d63b4ef571e
Bea Esguerra <beaesguerra@khanacademy.org>
2025-05-28 15:21:06 -0600

    [wb-1963-hook] make useSvgAttributes public and document the utility

--- packages/wonder-blocks-icon/src/index.ts
+++ packages/wonder-blocks-icon/src/index.ts
@@ -3,3 +3,5 @@ export {PhosphorIcon} from "./components/phosphor-icon";
 export type {IconSize, PhosphorIconAsset} from "./types";
 export {GemIcon} from "./components/custom-icon-components/gem-icon";
 export {StreakIcon} from "./components/custom-icon-components/streak-icon";
+
+export {useSvgAttributes} from "./hooks/use-svg-attributes";
