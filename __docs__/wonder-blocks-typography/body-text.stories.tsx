7d2a64658bcb2639b11576bf315d0ab976ecdbf0
Marcy Sutton-Todd <marcytodd@khanacademy.org>
2025-05-29 16:23:02 -0700

    Adjust BodyText and Heading docs and defaults (#2623)
    ## Summary:
    While integrating the new WB Typography components into webapp, I found a few things needing updates for a smooth transition.
    
    Issue: WB-1966
    
    ## Test plan:
    1. Ensure tests pass
    2. Test integration into webapp
    
    Author: marcysutton
    
    Reviewers: jandrade, beaesguerra, marcysutton
    
    Required Reviewers:
    
    Approved By: jandrade, beaesguerra
    
    Checks: ✅ 12 checks were successful, ⏭️  2 checks have been skipped
    
    Pull Request URL: https://github.com/Khan/wonder-blocks/pull/2623

--- __docs__/wonder-blocks-typography/body-text.stories.tsx
+++ __docs__/wonder-blocks-typography/body-text.stories.tsx
@@ -7,25 +7,59 @@ import ComponentInfo from "../components/component-info";
 import TypographyArgTypes from "./typography.argtypes";
 
 import {View} from "@khanacademy/wonder-blocks-core";
-import {BodyText} from "@khanacademy/wonder-blocks-typography";
+import {
+    BodyText,
+    LabelXSmall,
+    LabelSmall,
+    LabelMedium,
+    LabelLarge,
+    Body,
+} from "@khanacademy/wonder-blocks-typography";
 import {spacing} from "@khanacademy/wonder-blocks-tokens";
 import {allModes} from "../../.storybook/modes";
 
 /**
 New component for Thunderblocks body text.
 
-When wrapped in `<ThemeSwitcher theme="classroom">`, this component will use the
+When wrapped in `<ThemeSwitcher theme="classroom">`, `BodyText` will use the
 Plus Jakarta Sans font family.
+
+## Props
+
+### `size`
 
 The `size` prop will select a font size token based on our
 [REM font sizing scale](/?path=/docs/packages-tokens-typography--docs&globals=theme:thunderblocks#size).
 A corresponding line-height token will be automatically selected from our
 [line-height scale](/?path=/docs/packages-tokens-typography--docs&globals=theme:thunderblocks#lineHeight).
 
-The `weight` prop will match a font weight token based on the available
+Each size resolves to the following font-size and automatic line-height using `font.body` tokens:
+
+- xsmall: `sizing.size_120` / `sizing.size_160` (`LabelXSmall`)
+- small: `sizing.size_140` / `sizing.size_180` (`LabelSmall`)
+- medium (default): `sizing.size_160` / `sizing.size_200` (`LabelMedium`, `LabelLarge`, `Body`)
+
+With no `size` prop set, `BodyText` will default to `medium` font size and line height.
+
+### `weight`
+
+The `weight` prop will match a font weight token for Jakarta based on the available
 [font weights](/?path=/docs/packages-tokens-typography--docs&globals=theme:thunderblocks#weight).
 
-Heading text should utilize the `Heading` component.
+- medium (default): `500`
+- semi: `600`
+- bold: `700`
+
+With no `weight` prop set, `BodyText` will default to `medium` weight.
+
+### `tag`
+
+The `tag` prop will set a tagName, such as `tag="span"`.
+
+`BodyText` renders with a `p` tag by default.
+
+> Note: Heading text should utilize the `Heading` component. If the size or weight you're
+looking for doesn't exist in `BodyText`, consider making it a `Heading`!
 **/
 
 export default {
@@ -110,6 +144,43 @@ export const SizesAndWeights = {
     name: "Sizes and weights",
 };
 
+/**
+ * A table showing the conversion from Classic Typography components to BodyText.
+ */
+export const ClassicConversionGuide = {
+    parameters: {
+        chromatic: {
+            // Disabling because all typography components are covered together
+            // in the Typography stories
+            disableSnapshot: true,
+        },
+    },
+    render: () => (
+        <View style={[styles.grid, styles.conversionGuide]}>
+            <View style={styles.row}>
+                <LabelXSmall tag="p">LabelXSmall</LabelXSmall>
+                <BodyText size="xsmall">BodyText size=xsmall</BodyText>
+            </View>
+            <View style={styles.row}>
+                <LabelSmall tag="p">LabelSmall</LabelSmall>
+                <BodyText size="small">BodyText size=small</BodyText>
+            </View>
+            <View style={styles.row}>
+                <LabelMedium tag="p">LabelMedium</LabelMedium>
+                <BodyText>BodyText</BodyText>
+            </View>
+            <View style={styles.row}>
+                <LabelLarge tag="p">LabelLarge</LabelLarge>
+                <BodyText weight="bold">BodyText weight=bold</BodyText>
+            </View>
+            <View style={styles.row}>
+                <Body tag="p">Body</Body>
+                <BodyText>BodyText</BodyText>
+            </View>
+        </View>
+    ),
+};
+
 const styles = StyleSheet.create({
     grid: {
         display: "grid",
@@ -117,6 +188,10 @@ const styles = StyleSheet.create({
         gridTemplateColumns: "max-content max-content max-content",
         rowGap: spacing.medium_16,
         columnGap: spacing.large_24,
+    },
+    conversionGuide: {
+        justifyContent: "flex-start",
+        gridTemplateColumns: "max-content max-content",
     },
     row: {
         display: "contents",
