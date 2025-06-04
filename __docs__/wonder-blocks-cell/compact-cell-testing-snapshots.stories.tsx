b6a5f549188e27a8872a1a05b03e43358db01714
Juan Andrade <juan@khanacademy.org>
2025-06-04 17:05:13 -0400

    [🔥AUDIT🔥] [WB-1958.1] Refactor Cell stories to use StateSheet (#2643)
    🖍 _This is an audit!_ 🖍
    
    ## Summary:
    
    This refactors the Cell stories to use the new `StateSheet` and
    `ScenariosLayout` components and moves some of the previous stories out of
    Chromatic.
    
    This is to prepare for the upcoming Cell changes to support the new TB theme.
    
    Issue: https://khanacademy.atlassian.net/browse/WB-1958
    
    ## Test plan:
    
    Navigate to the Cell stories in Storybook and verify that they render correctly.
    
    Author: jandrade
    
    Auditors: #wonder-blocks
    
    Required Reviewers:
    
    Approved By:
    
    Checks: ✅ 12 checks were successful, ⏭️  2 checks have been skipped
    
    Pull Request URL: https://github.com/Khan/wonder-blocks/pull/2643

---
+++ __docs__/wonder-blocks-cell/compact-cell-testing-snapshots.stories.tsx
@@ -0,0 +1,152 @@
+import * as React from "react";
+import type {Meta, StoryObj} from "@storybook/react";
+
+import {CompactCell} from "@khanacademy/wonder-blocks-cell";
+import {PhosphorIcon} from "@khanacademy/wonder-blocks-icon";
+import {IconMappings} from "../wonder-blocks-icon/phosphor-icon.argtypes";
+import {defaultPseudoStates, StateSheet} from "../components/state-sheet";
+import {
+    longText,
+    longTextWithNoWordBreak,
+} from "../components/text-for-testing";
+import {ScenariosLayout} from "../components/scenarios-layout";
+import {allModes} from "../../.storybook/modes";
+
+const defaultProps = {
+    title: "Title for article item",
+    leftAccessory: <PhosphorIcon icon={IconMappings.calendarBold} />,
+    rightAccessory: <PhosphorIcon icon={IconMappings.caretRightBold} />,
+};
+
+/**
+ * The following stories are used to generate the pseudo states for the
+ * CompactCell component. This is only used for visual testing in Chromatic.
+ */
+export default {
+    title: "Packages / Cell / Testing / Snapshots / CompactCell",
+    component: CompactCell,
+    args: {
+        title: defaultProps.title,
+    },
+    parameters: {
+        backgrounds: {
+            default: "offWhite",
+        },
+        chromatic: {
+            modes: {
+                default: allModes.themeDefault,
+                thunderblocks: allModes.themeThunderBlocks,
+            },
+        },
+    },
+    tags: ["!autodocs"],
+} as Meta<typeof CompactCell>;
+
+type Story = StoryObj<typeof CompactCell>;
+
+const rows = [{name: "Clickable", props: {onClick: () => {}}}];
+
+const columns = [
+    {
+        name: "Default",
+        props: defaultProps,
+    },
+    {
+        name: "Disabled",
+        props: {...defaultProps, disabled: true},
+    },
+    {
+        name: "Selected using active: true",
+        props: {...defaultProps, active: true},
+    },
+];
+
+export const StateSheetStory: Story = {
+    name: "StateSheet",
+    render: (args) => {
+        return (
+            <StateSheet rows={rows} columns={columns} title="Status / Variant">
+                {({props, className}) => (
+                    <CompactCell {...args} {...props} className={className} />
+                )}
+            </StateSheet>
+        );
+    },
+    parameters: {
+        pseudo: defaultPseudoStates,
+    },
+};
+
+/**
+ * The following story shows how the component handles specific scenarios.
+ */
+export const Scenarios: Story = {
+    render() {
+        const scenarios = [
+            {
+                name: "Default",
+                props: defaultProps,
+            },
+            {
+                name: "No Icons",
+                props: {
+                    ...defaultProps,
+                    leftAccessory: undefined,
+                    rightAccessory: undefined,
+                },
+            },
+            {
+                name: "Left Icon Only",
+                props: {
+                    ...defaultProps,
+                    rightAccessory: undefined,
+                },
+            },
+            {
+                name: "Right Icon Only",
+                props: {
+                    ...defaultProps,
+                    leftAccessory: undefined,
+                },
+            },
+            {
+                name: "Long Text",
+                props: {
+                    ...defaultProps,
+                    title: longText,
+                },
+            },
+            {
+                name: "Long Text No Word Break",
+                props: {
+                    ...defaultProps,
+                    title: longTextWithNoWordBreak,
+                },
+            },
+            {
+                name: "Long Text (no icons)",
+                props: {
+                    ...defaultProps,
+                    title: longText,
+                    leftAccessory: undefined,
+                    rightAccessory: undefined,
+                },
+            },
+            {
+                name: "Long Text No Word Break (no icons)",
+                props: {
+                    ...defaultProps,
+                    title: longTextWithNoWordBreak,
+                    leftAccessory: undefined,
+                    rightAccessory: undefined,
+                },
+            },
+        ];
+
+        return (
+            <ScenariosLayout scenarios={scenarios}>
+                {(props) => <CompactCell {...props} />}
+            </ScenariosLayout>
+        );
+    },
+};
