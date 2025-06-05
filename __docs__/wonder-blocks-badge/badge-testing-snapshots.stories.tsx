2f90748cbf93cbf77737d0ce9d17e21fe0d09908
Jeremy Wiebe <jeremy@khanacademy.org>
2025-06-05 09:25:02 -0700

    Merge main: remove reference to deleted KhanmigoTheme button story from 'adding-a-theme.mdx'

--- __docs__/wonder-blocks-badge/badge-testing-snapshots.stories.tsx
+++ __docs__/wonder-blocks-badge/badge-testing-snapshots.stories.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import type {Meta, StoryObj} from "@storybook/react";
+import type {Meta, StoryObj} from "@storybook/react-vite";
 import {StyleSheet} from "aphrodite";
 import ComponentInfo from "../components/component-info";
 import packageConfig from "../../packages/wonder-blocks-form/package.json";
