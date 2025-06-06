2d0b5f0d2ff139d95ab05cc5bfdb905ad547a343
Marcy Sutton Todd <marcytodd@khanacademy.org>
2025-06-06 12:53:41 -0700

    [WB-1973-switch] Remove redundant variables

--- packages/wonder-blocks-switch/src/theme/thunderblocks.ts
+++ packages/wonder-blocks-switch/src/theme/thunderblocks.ts
@@ -1,5 +1,5 @@
 import {mergeTheme} from "@khanacademy/wonder-blocks-theming";
-import {border, spacing} from "@khanacademy/wonder-blocks-tokens";
+import {border} from "@khanacademy/wonder-blocks-tokens";
 import defaultTheme from "./default";
 
 export default mergeTheme(defaultTheme, {
@@ -9,18 +9,7 @@ export default mergeTheme(defaultTheme, {
                 radius: {
                     default: border.radius.radius_080,
                     hover: border.radius.radius_080,
-                    press: border.radius.radius_120,
                 },
-            },
-            sizing: {
-                height: spacing.large_24,
-                width: 40, // Using hardcoded value since there's no matching token
-            },
-        },
-        slider: {
-            sizing: {
-                height: 20, // Using hardcoded value since there's no matching token
-                width: 20, // Using hardcoded value since there's no matching token
             },
         },
     },
