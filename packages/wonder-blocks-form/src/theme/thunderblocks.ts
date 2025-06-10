1f8735a202180700c89784de6b82bc35b881b81d
Marcy Sutton Todd <marcytodd@khanacademy.org>
2025-06-10 11:47:03 -0700

    [WB-1972-checkradio] Initial theming integration

---
+++ packages/wonder-blocks-form/src/theme/thunderblocks.ts
@@ -0,0 +1,22 @@
+import {border, sizing} from "@khanacademy/wonder-blocks-tokens";
+
+export default {
+    root: {
+        border: {
+            width: {
+                default: border.width.thin,
+            },
+        },
+        sizing: {
+            height: sizing.size_240,
+            width: sizing.size_240,
+        },
+    },
+    checkbox: {
+        border: {
+            radius: {
+                default: border.radius.radius_040,
+            },
+        },
+    },
+};
