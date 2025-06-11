933f7bb3d5f97504b874480693f09d09cd256d48
Marcy Sutton Todd <marcytodd@khanacademy.org>
2025-06-11 10:36:36 -0700

    [WB-1972-checkradio] Fine tune theme styles

--- packages/wonder-blocks-form/src/theme/thunderblocks.ts
+++ packages/wonder-blocks-form/src/theme/thunderblocks.ts
@@ -1,33 +1,15 @@
-import {border, sizing, spacing} from "@khanacademy/wonder-blocks-tokens";
+import {border, sizing} from "@khanacademy/wonder-blocks-tokens";
 
 export default {
     inputWrapper: {
-        padding: `${spacing.medium_16}px`,
-        margin: `calc(-${spacing.medium_16}px)`,
+        padding: sizing.size_040,
+        margin: `calc(${sizing.size_040} * -1)`,
     },
     checkbox: {
         border: {
             radius: {
                 default: border.radius.radius_040,
             },
-            width: {
-                default: border.width.thin,
-            },
-        },
-        sizing: {
-            height: sizing.size_240,
-            width: sizing.size_240,
-        },
-    },
-    radio: {
-        border: {
-            width: {
-                default: border.width.thin,
-            },
-        },
-        sizing: {
-            height: sizing.size_240,
-            width: sizing.size_240,
         },
     },
 };
