2f42011ed75b116e0bf250ef7cb1d43e0b424b70
Juan Andrade <juan@khanacademy.org@khanacademy.org>
2025-06-20 16:49:05 -0400

    [tb-modal] Adapt TB theme completely

--- packages/wonder-blocks-modal/src/theme/thunderblocks.ts
+++ packages/wonder-blocks-modal/src/theme/thunderblocks.ts
@@ -1,5 +1,5 @@
 import {mergeTheme} from "@khanacademy/wonder-blocks-theming";
-import {border, sizing} from "@khanacademy/wonder-blocks-tokens";
+import {border, semanticColor, sizing} from "@khanacademy/wonder-blocks-tokens";
 import defaultTheme from "./default";
 
 export default mergeTheme(defaultTheme, {
@@ -8,9 +8,40 @@ export default mergeTheme(defaultTheme, {
             radius: border.radius.radius_120,
         },
     },
+    dialog: {
+        shadow: {
+            default: `0 0 ${sizing.size_080} ${sizing.size_160} ${semanticColor.core.shadow.transparent}`,
+        },
+    },
+    header: {
+        layout: {
+            padding: {
+                block: sizing.size_220,
+                inline: {
+                    default: sizing.size_240,
+                },
+            },
+        },
+    },
+    panel: {
+        layout: {
+            gap: {
+                default: sizing.size_240,
+            },
+        },
+    },
+    footer: {
+        layout: {
+            padding: {
+                inline: sizing.size_160,
+                block: sizing.size_160,
+            },
+        },
+    },
     closeButton: {
         layout: {
-            gap: sizing.size_120,
+            gapRight: sizing.size_120,
+            gapTop: sizing.size_160,
         },
     },
 });
