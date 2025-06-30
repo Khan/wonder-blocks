cac02f2aec5f6658b7d15cda24a48615b18c9db8
Marcy Sutton-Todd <marcytodd@khanacademy.org>
2025-06-30 16:27:45 -0700

    Add testing sheets for Heading/BodyText (#2698)
    ## Summary:
    This PR introduces variant testing sheets for the new Heading and BodyText components.
    
    Issue: WB-1957
    
    ## Test plan:
    1. Review Heading sheet: /?path=/story/packages-typography-testing-snapshots-heading--all-variants-story&globals=theme:thunderblocks
    2. Review BodyText sheet: /?path=/story/packages-typography-testing-snapshots-bodytext--all-variants-story&globals=theme:thunderblocks
    
    Author: marcysutton
    
    Reviewers: beaesguerra, marcysutton
    
    Required Reviewers:
    
    Approved By: beaesguerra
    
    Checks: ✅ 12 checks were successful, ⏭️  2 checks have been skipped
    
    Pull Request URL: https://github.com/Khan/wonder-blocks/pull/2698

---
+++ __docs__/wonder-blocks-typography/body-text-testing-snapshots.stories.tsx
@@ -0,0 +1,57 @@
+import * as React from "react";
+import type {Meta, StoryObj} from "@storybook/react";
+
+import {View} from "@khanacademy/wonder-blocks-core";
+import {BodyText} from "@khanacademy/wonder-blocks-typography";
+import {sizing} from "@khanacademy/wonder-blocks-tokens";
+
+import {AllVariants} from "../components/all-variants";
+import {allModes} from "../../.storybook/modes";
+
+/**
+ * The following stories are used to generate the style combinations for the BodyText
+ * component. This is only used for visual testing in Chromatic.
+ */
+export default {
+    title: "Packages / Typography / Testing / Snapshots / BodyText",
+    component: BodyText,
+    parameters: {
+        chromatic: {
+            modes: {
+                default: allModes.themeDefault,
+                thunderblocks: allModes.themeThunderBlocks,
+            },
+        },
+    },
+    tags: ["!autodocs"],
+} as Meta<typeof BodyText>;
+
+type StoryComponentType = StoryObj<typeof BodyText>;
+
+const weights = ["medium", "semi", "bold"] as const;
+const sizes = ["xsmall", "small", "medium"] as const;
+
+const rows = sizes.map((size) => ({
+    name: size,
+    props: {size},
+}));
+
+const columns = weights.map((weight) => ({
+    name: weight,
+    props: {weight},
+}));
+
+export const AllVariantsStory: StoryComponentType = {
+    name: "All Variants",
+    render: (args) => {
+        return (
+            <AllVariants rows={rows} columns={columns} title="Size / Weight">
+                {({props, name}) => (
+                    <View key={name} style={{gap: sizing.size_120}}>
+                        <BodyText {...props}>{name}</BodyText>
+                    </View>
+                )}
+            </AllVariants>
+        );
+    },
+};
