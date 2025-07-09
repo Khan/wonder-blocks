bdb8497638624744565fbc26e16d664ee846416d
Bea Esguerra <beaesguerra@khanacademy.org>
2025-07-09 14:20:36 -0600

    [wb-2018-other-helper-text] use logical css property instead of margintop

--- packages/wonder-blocks-labeled-field/src/components/labeled-field.tsx
+++ packages/wonder-blocks-labeled-field/src/components/labeled-field.tsx
@@ -436,7 +436,7 @@ const styles = StyleSheet.create({
         fontWeight: theme.error.font.weight,
     },
     errorIcon: {
-        marginTop: sizing.size_010, // This vertically aligns the icon with the text
+        marginBlockStart: sizing.size_010, // This vertically aligns the icon with the text
         color: theme.error.color.foreground,
     },
     required: {
