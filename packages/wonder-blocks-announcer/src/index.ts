1cc270f37d7820eb472098072b0f9df8a1ae929e
Marcy Sutton Todd <marcytodd@khanacademy.org>
2025-07-10 17:31:16 -0700

    [WB-2019-announcer] Expose PolitenessLevel type

--- packages/wonder-blocks-announcer/src/index.ts
+++ packages/wonder-blocks-announcer/src/index.ts
@@ -2,6 +2,7 @@ import {initAnnouncer} from "./init-announcer";
 import {announceMessage} from "./announce-message";
 import {clearMessages} from "./clear-messages";
 import type {AnnounceMessageProps} from "./announce-message";
+import type {PolitenessLevel} from "./util/announcer.types";
 
 export {initAnnouncer, announceMessage, clearMessages};
-export {type AnnounceMessageProps};
+export {type AnnounceMessageProps, type PolitenessLevel};
