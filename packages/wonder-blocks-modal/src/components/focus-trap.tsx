d6059d0c8123ace355f5ac6b5d23c2bd4c406ced
Tim McCabe <timmccabe@khanacademy.org>
2025-07-23 15:27:20 -0400

    Do not try to focus hidden elements in FocusTrap

--- packages/wonder-blocks-modal/src/components/focus-trap.tsx
+++ packages/wonder-blocks-modal/src/components/focus-trap.tsx
@@ -87,6 +87,12 @@ export default class FocusTrap extends React.Component<Props> {
         // Get the list of available focusable elements within the modal.
         const focusableNodes = Array.from(
             modalRootAsHtmlEl.querySelectorAll(FOCUSABLE_ELEMENTS),
+        ).filter((element) =>
+            element.checkVisibility === undefined
+                ? // checkVisibility is not implemented in jsdom, so just assume
+                  // the element is visible
+                  true
+                : element.checkVisibility(),
         );
 
         const nodeIndex = !isLast ? focusableNodes.length - 1 : 0;
