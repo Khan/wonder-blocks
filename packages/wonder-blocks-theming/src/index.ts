dbd12e78b502fb3a60e635e625d9ae53ec957860
Juan Andrade <juan@khanacademy.org@khanacademy.org>
2025-07-23 11:35:48 -0400

    [rm-khanmigo-theme] Add back ThemeSwitcherContext for internal purposes

--- packages/wonder-blocks-theming/src/index.ts
+++ packages/wonder-blocks-theming/src/index.ts
@@ -1,4 +1,5 @@
+export {ThemeSwitcher} from "./components/theme-switcher";
 export {mergeTheme} from "./utils/merge-theme";
 export {type SupportedThemes} from "./types";
+export {ThemeSwitcherContext} from "./utils/theme-switcher-context";
 export {THEME_DATA_ATTRIBUTE} from "./utils/constants";
-export {ThemeSwitcher} from "./components/theme-switcher";
