5a248506320ac72772f229f5d6df6429800f90bb
Bea Esguerra <beaesguerra@khanacademy.org>
2025-08-01 16:43:13 -0600

    [wb-2044] set up a11y for copy button

--- __docs__/components/copy-button.tsx
+++ __docs__/components/copy-button.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import CopyIcon from "@phosphor-icons/core/bold/copy-bold.svg";
 import CheckIcon from "@phosphor-icons/core/bold/check-bold.svg";
 import IconButton from "@khanacademy/wonder-blocks-icon-button";
+import {announceMessage} from "@khanacademy/wonder-blocks-announcer";
 
 type Props = {
     value: string;
@@ -11,9 +12,12 @@ export const CopyButton = (props: Props) => {
     const [showCheckmark, setShowCheckmark] = React.useState(false);
 
     const handleClick = () => {
-        // eslint-disable-next-line no-console -- intentionally log. Once we have toasts, we can replace this with a toast notification.
-        console.log("Copied to clipboard: ", props.value);
         navigator.clipboard.writeText(props.value);
+        // eslint-disable-next-line no-console -- intentionally log. Once we have toasts, we can replace this with a toast notification.
+        console.log("Copied to clipboard:", props.value);
+        announceMessage({message: `Copied to clipboard: ${props.value}`});
+
+        // Temporarily show visual feedback that the copy was successful.
         setShowCheckmark(true);
         setTimeout(() => {
             setShowCheckmark(false);
@@ -27,6 +31,7 @@ export const CopyButton = (props: Props) => {
             size="xsmall"
             kind="tertiary"
             actionType="neutral"
+            aria-label={`Copy to clipboard: ${props.value}`}
         />
     );
 };
