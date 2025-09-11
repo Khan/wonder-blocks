7e963ec595ebbc990c5b2c43e456b86300071aae
Mike Klemarewski <mikeklemarewski@khanacademy.org>
2025-09-11 13:24:26 -0700

    [mk-action-menu-alignment] Allow action menu to support any valid popper placement

--- __docs__/wonder-blocks-dropdown/action-menu.stories.tsx
+++ __docs__/wonder-blocks-dropdown/action-menu.stories.tsx
@@ -389,6 +389,21 @@ export const CustomOpener: StoryComponentType = {
 };
 
 /**
+ * Sometimes you may want to align the dropdown somewhere besides below the
+ * opener. In these cases, you can specify any valid popper placement as the
+ * alignment.
+ */
+export const WithPopperPlacement: StoryComponentType = {
+    name: "With popper placement",
+    args: {
+        alignment: "right-start",
+        opener: ({text}: any) => (
+            <Button endIcon={IconMappings.caretRight}>{text}</Button>
+        ),
+    } as Partial<typeof ActionMenu>,
+};
+
+/**
  * You can use the `lang` attribute to specify the language of the action
  * item(s). This is useful if you want to avoid issues with Screen Readers
  * trying to read the proper language for the rendered text.
