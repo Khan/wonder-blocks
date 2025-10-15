45c4fb59c1dfb60d675bf348a8ea415e2132227c
Marcy Sutton Todd <marcytodd@khanacademy.org>
2025-10-15 14:58:45 -0700

    [wb-1961] Fix modalLauncher docs

--- __docs__/wonder-blocks-modal/modal-launcher.argtypes.ts
+++ __docs__/wonder-blocks-modal/modal-launcher.argtypes.ts
@@ -3,15 +3,8 @@ import type {ArgTypes} from "@storybook/react-vite";
 export default {
     modal: {
         control: {type: undefined},
-        description: `The modal to render. The modal will be rendered inside
-            of a container whose parent is document.body. This allows us to
-            use ModalLauncher within menus and other components that clip
-            their content. If the modal needs to close itself by some other
-            means than tapping the backdrop or the default close button a
-            render callback can be passed. The closeModal function provided
-            to this callback can be called to close the modal.
-            **Note**: Don't call \`closeModal\` while rendering! It should
-            be used to respond to user intearction, like \`onClick\`.`,
+        description:
+            "The modal to render. The modal will be rendered inside of a container whose parent is document.body. This allows us to use ModalLauncher within menus and other components that clip their content. If the modal needs to close itself by some other means than tapping the backdrop or the default close button a render callback can be passed. The closeModal function provided to this callback can be called to close the modal. **Note**: Don't call `closeModal` while rendering! It should be used to respond to user intearction, like `onClick`.",
         table: {
             type: {
                 summary:
@@ -37,10 +30,8 @@ export default {
 
     initialFocusId: {
         control: {type: "text"},
-        description: `The selector for the element that will be focused
-            when the dialog shows. When not set, the first tabbable element
-            within the dialog will be used, which usually is the dismiss button
-            (X).`,
+        description:
+            "The selector for the element that will be focused when the dialog shows. When not set, the first tabbable element within the dialog will be used, which usually is the dismiss button (X).",
         table: {
             type: {summary: "string"},
         },
@@ -48,9 +39,8 @@ export default {
 
     closedFocusId: {
         control: {type: "text"},
-        description: `The selector for the element that will be focused
-            after the dialog closes. When not set, the last element focused
-            outside the modal will be used if it exists.`,
+        description:
+            "The selector for the element that will be focused after the dialog closes. When not set, the last element focused outside the modal will be used if it exists.",
         table: {type: {summary: "string"}},
     },
 
@@ -63,12 +53,8 @@ export default {
 
     opened: {
         control: {type: "boolean"},
-        description: `Renders the modal when true, renders nothing when false.
-            Using this prop makes the component behave as a controlled
-            component. The parent is responsible for managing the
-            opening/closing of the modal when using this prop.  \`onClose\`
-            should always be used and \`children\` should never be used with
-            this prop.  Not doing so will result in an error being thrown.`,
+        description:
+            "Renders the modal when true, renders nothing when false. Using this prop makes the component behave as a controlled component. The parent is responsible for managing the opening/closing of the modal when using this prop. `onClose` should always be used and `children` should never be used with this prop. Not doing so will result in an error being thrown.",
         table: {
             category: "Controlled",
             type: {summary: "boolean"},
@@ -76,11 +62,8 @@ export default {
     },
 
     onClose: {
-        description: `If the parent needs to be notified when the modal is
-            closed, use this prop. You probably want to use this instead of
-            \`onClose\` on the modals themselves, since this will capture a
-            more complete set of close events. \`onClose\` is required when
-            the component is being used as a controlled component.`,
+        description:
+            "If the parent needs to be notified when the modal is closed, use this prop. You probably want to use this instead of `onClose` on the modals themselves, since this will capture a more complete set of close events. `onClose` is required when the component is being used as a controlled component.",
         table: {
             category: "Controlled",
             type: {summary: "() => mixed"},
