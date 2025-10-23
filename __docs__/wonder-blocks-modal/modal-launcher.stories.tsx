f51faab3a112ee659f8cccd16e125cd30c1c4dba
Marcy Sutton Todd <marcytodd@khanacademy.org>
2025-10-23 15:25:39 -0700

    [wb-2103] Try to fix stories

--- __docs__/wonder-blocks-modal/modal-launcher.stories.tsx
+++ __docs__/wonder-blocks-modal/modal-launcher.stories.tsx
@@ -113,6 +113,7 @@ export default {
                 small: allModes.small,
                 large: allModes.large,
             },
+            disableSnapshot: true,
         },
     },
     argTypes: ModalLauncherArgTypes,
@@ -130,13 +131,6 @@ export const Default: StoryComponentType = {
     ),
 };
 
-Default.parameters = {
-    chromatic: {
-        // All the examples for ModalLauncher are behavior based, not visual.
-        disableSnapshot: true,
-    },
-};
-
 export const Simple: StoryComponentType = () => (
     <ModalLauncher modal={DefaultModal}>
         {({openModal}) => (
@@ -146,10 +140,6 @@ export const Simple: StoryComponentType = () => (
 );
 
 Simple.parameters = {
-    chromatic: {
-        // All the examples for ModalLauncher are behavior based, not visual.
-        disableSnapshot: true,
-    },
     docs: {
         description: {
             story: "This is a basic modal launcher. Its child, the button, has access to the `openModal` function via the function-as-child pattern. It passes this into its `onClick` function, which causes the modal to launch when the button is clicked.",
@@ -196,10 +186,6 @@ export const WithLongContentsAndFooter: StoryComponentType = () => {
 };
 
 WithLongContentsAndFooter.parameters = {
-    chromatic: {
-        // All the examples for ModalLauncher are behavior based, not visual.
-        disableSnapshot: true,
-    },
     docs: {
         description: {
             story: "This example demonstrates how to handle long content in modals, especially at high zoom levels. The modal supports two modes: standard (fixed height with overflow hidden) and fullscreen (scrollable content). The fullscreen mode is particularly useful for accessibility, allowing users to read all content even at 400% zoom.",
@@ -249,10 +235,6 @@ export const WithCustomCloseButton: StoryComponentType = () => {
 };
 
 WithCustomCloseButton.parameters = {
-    chromatic: {
-        // All the examples for ModalLauncher are behavior based, not visual.
-        disableSnapshot: true,
-    },
     docs: {
         description: {
             story: 'This is an example of a modal that uses a close button other than the default "X" button in the top right corner. Here, the default "X" close button is not rendered because the `closeButtonVisible` prop on the `<OnePaneDialog>` is set to false. Instead, a custom close button has been added to the modal footer. The `modal` prop on `<ModalLauncher>` can either be a plain modal, or it can be a function that takes a `closeModal` function as a parameter and returns a modal. The latter is what we do in this case. Then the `closeModal` function is passed into the `onClick` prop on the button in the footer.',
@@ -269,10 +251,6 @@ export const WithBackdropDismissDisabled: StoryComponentType = () => (
 );
 
 WithBackdropDismissDisabled.parameters = {
-    chromatic: {
-        // All the examples for ModalLauncher are behavior based, not visual.
-        disableSnapshot: true,
-    },
     docs: {
         description: {
             story: "This is an example in which the modal _cannot_ be dismissed by clicking in in the backdrop. This is done by setting the `backdropDismissEnabled` prop on the `<ModalLauncher>` element to false.",
@@ -320,10 +298,6 @@ export const TriggeringProgrammatically: StoryComponentType = () => {
 };
 
 TriggeringProgrammatically.parameters = {
-    chromatic: {
-        // All the examples for ModalLauncher are behavior based, not visual.
-        disableSnapshot: true,
-    },
     docs: {
         description: {
             story: "Sometimes you'll want to trigger a modal programmatically. This can be done by rendering `<ModalLauncher>` without any children and instead setting its `opened` prop to true. In this situation, `ModalLauncher` is a controlled component which means you'll also have to update `opened` to false in response to the `onClose` callback being triggered. It is necessary to use this method in this example, as `ActionMenu` cannot have a `ModalLauncher` element as a child, (it can only have `Item` elements as children), so launching a modal from a dropdown must be done programatically.",
@@ -390,7 +364,6 @@ export const WithOpenedTrue = () => {
                         <Button onClick={closeModal}>Cancel</Button>
                         <Button
                             onClick={() => {
-                                // Simulate delete
                                 closeModal();
                             }}
                         >
@@ -459,13 +432,6 @@ export const WithOpenedTrue = () => {
     );
 };
 
-WithOpenedTrue.parameters = {
-    chromatic: {
-        // All the examples for ModalLauncher are behavior based, not visual.
-        disableSnapshot: true,
-    },
-};
-
 export const WithClosedFocusId: StoryComponentType = () => {
     const [opened, setOpened] = React.useState(false);
 
@@ -495,10 +461,6 @@ export const WithClosedFocusId: StoryComponentType = () => {
 };
 
 WithClosedFocusId.parameters = {
-    chromatic: {
-        // All the examples for ModalLauncher are behavior based, not visual.
-        disableSnapshot: true,
-    },
     docs: {
         description: {
             story: 'You can use the `closedFocusId` prop on the `ModalLauncher` to specify where to set the focus after the modal has been closed. Imagine the following situation: clicking on a dropdown menu option to open a modal causes the dropdown to close, and so all of the dropdown options are removed from the DOM. This can be a problem because by default, the focus shifts to the previously focused element after a modal is closed; in this case, the element that opened the modal cannot receive focus since it no longer exists in the DOM, so when you close the modal, it doesn\'t know where to focus on the page. When the previously focused element no longer exists, the focus shifts to the page body, which causes a jump to the top of the page. This can make it diffcult to find the original dropdown. A solution to this is to use the `closedFocusId` prop to specify where to set the focus after the modal has been closed. In this example, `closedFocusId` is set to the ID of the button labeled "Focus here after close." If the focus shifts to the button labeled "Top of page (should not receieve focus)," then the focus is on the page body, and the `closedFocusId` did not work.',
@@ -555,10 +517,6 @@ export const WithInitialFocusId: StoryComponentType = () => {
 };
 
 WithInitialFocusId.parameters = {
-    chromatic: {
-        // All the examples for ModalLauncher are behavior based, not visual.
-        disableSnapshot: true,
-    },
     docs: {
         description: {
             story: "Sometimes, you may want a specific element inside the modal to receive focus first. This can be done using the `initialFocusId` prop on the `<ModalLauncher>` element. Just pass in the ID of the element that should receive focus, and it will automatically receieve focus once the modal opens. In this example, the top text input would have received the focus by default, but the bottom text field receives focus instead since its ID is passed into the `initialFocusId` prop.",
@@ -644,7 +602,6 @@ export const FocusManagementPattern: StoryComponentType = () => {
         );
     };
 
-    // Container component to manage state
     const CompletionModalContainer = () => {
         const [selectedItem, setSelectedItem] = React.useState<string | null>(
             null,
@@ -759,10 +716,6 @@ export const FocusTrap: StoryComponentType = () => {
 FocusTrap.storyName = "Navigation with focus trap";
 
 FocusTrap.parameters = {
-    chromatic: {
-        // All the examples for ModalLauncher are behavior based, not visual.
-        disableSnapshot: true,
-    },
     docs: {
         description: {
             story: "All modals have a focus trap, which means that the focus is locked inside the modal. This is done to prevent the user from tabbing out of the modal and losing their place. The focus trap is also used to ensure that the focus is restored to the correct element when the modal is closed. In this example, the focus is trapped inside the modal, and the focus is restored to the button that opened the modal when the modal is closed.\n\nAlso, this example includes a sub-modal that is opened from the first modal so we can test how the focus trap works when multiple modals are open.",
@@ -884,7 +837,9 @@ export const ControlledModalFocusTest: StoryComponentType = () => {
                     />
                 )}
                 opened={opened}
-                onClose={() => setOpened(false)}
+                onClose={() => {
+                    setOpened(false);
+                }}
             />
 
             {/* Wrapped modal that starts opened=true */}
@@ -895,13 +850,7 @@ export const ControlledModalFocusTest: StoryComponentType = () => {
     );
 };
 
-ControlledModalFocusTest.parameters = {
-    chromatic: {
-        // All the examples for ModalLauncher are behavior based, not
-        // visual.
-        disableSnapshot: true,
-    },
-};
+ControlledModalFocusTest.parameters = {};
 
 const WrappedModalExample = ({onClose}: {onClose: () => void}) => {
     return (
@@ -1007,13 +956,6 @@ export const CreatingACustomModal: StoryComponentType = {
                 )}
             </ModalLauncher>
         );
-    },
-    parameters: {
-        chromatic: {
-            // All the examples for ModalLauncher are behavior based, not
-            // visual.
-            disableSnapshot: true,
-        },
     },
 };
 
