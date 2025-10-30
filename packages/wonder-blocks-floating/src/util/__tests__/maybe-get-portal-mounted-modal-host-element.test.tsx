2f1f68d6367feaf47cfd02f31bf829d9465e565c
Juan Andrade <juan@khanacademy.org@khanacademy.org>
2025-10-30 14:35:34 -0400

    [wb-floating-comp-portal] docs(changeset): Adds portal prop and copies maybeGetPortalMountedModalHostElement from WB Modal.

---
+++ packages/wonder-blocks-floating/src/util/__tests__/maybe-get-portal-mounted-modal-host-element.test.tsx
@@ -0,0 +1,133 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import {render, screen} from "@testing-library/react";
+import {userEvent} from "@testing-library/user-event";
+
+import {OnePaneDialog, ModalLauncher} from "@khanacademy/wonder-blocks-modal";
+import {ModalLauncherPortalAttributeName} from "../constants";
+import maybeGetPortalMountedModalHostElement from "../maybe-get-portal-mounted-modal-host-element";
+
+describe("maybeGetPortalMountedModalHostElement", () => {
+    test("when candidate is null, returns null", async () => {
+        // Arrange
+        const candidateElement = null;
+
+        // Act
+        const result = maybeGetPortalMountedModalHostElement(candidateElement);
+
+        // Assert
+        expect(result).toBeFalsy();
+    });
+
+    test("when candidate is not hosted in a modal portal, returns null", async () => {
+        // Arrange
+        const nodes = (
+            <div>
+                <div>
+                    <button>Button</button>
+                </div>
+            </div>
+        );
+        render(nodes);
+        const candidateElement = await screen.findByRole("button");
+
+        // Act
+        const result = maybeGetPortalMountedModalHostElement(candidateElement);
+
+        // Assert
+        expect(result).toBeFalsy();
+        expect(candidateElement).not.toBe(null);
+    });
+
+    describe("hosted in a modal", () => {
+        test("modal is not mounted in a modal portal, returns null", async () => {
+            // Arrange
+            const modalContent = (
+                <div>
+                    Fake modal things
+                    <div>
+                        <button>Candidate</button>
+                    </div>
+                </div>
+            );
+
+            const modal = (
+                <OnePaneDialog
+                    title="Testing"
+                    footer="Footer"
+                    content={modalContent}
+                />
+            );
+
+            render(modal);
+            const candidateElement = await screen.findByRole("button", {
+                name: "Candidate",
+            });
+
+            // Act
+            const result =
+                maybeGetPortalMountedModalHostElement(candidateElement);
+
+            // Assert
+            expect(result).toBeFalsy();
+            expect(candidateElement).not.toBe(null);
+        });
+
+        test("modal is mounted in a modal portal, returns host", (done: any) => {
+            const arrange = async (checkDone: any) => {
+                // Arrange
+                const modalContent = (
+                    <div>
+                        Fake modal things
+                        <div>
+                            <span ref={checkDone}>Candidate</span>
+                        </div>
+                    </div>
+                );
+                const modal = (
+                    <OnePaneDialog
+                        title="Testing"
+                        footer="Footer"
+                        content={modalContent}
+                    />
+                );
+                const launcher = (
+                    <ModalLauncher modal={modal}>
+                        {({openModal}: any) => (
+                            <button onClick={openModal}>Modal</button>
+                        )}
+                    </ModalLauncher>
+                );
+                render(launcher);
+                await userEvent.click(await screen.findByRole("button"));
+            };
+
+            const actAndAssert = (node: any) => {
+                if (node) {
+                    // Act
+                    // eslint-disable-next-line import/no-deprecated
+                    const candidateElement = ReactDOM.findDOMNode(node);
+                    const result =
+                        maybeGetPortalMountedModalHostElement(candidateElement);
+
+                    // Assert
+                    expect(result).toBeTruthy();
+
+                    const modalPortalElement = result;
+                    expect(modalPortalElement).not.toBe(null);
+
+                    const isModalPortal =
+                        modalPortalElement &&
+                        modalPortalElement.hasAttribute(
+                            ModalLauncherPortalAttributeName,
+                        );
+                    expect(isModalPortal).toBeTruthy();
+
+                    done();
+                }
+            };
+
+            arrange(actAndAssert);
+        });
+    });
+});
