1ee952aeaab66f509e23792cb0f8fad1f713182b
Bea Esguerra <beaesguerra@khanacademy.org>
2025-11-03 10:34:17 -0700

    [set-up-syl-dark] set up changing the storybook theme too when the wb theme changes

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -3,6 +3,7 @@
  */
 import {addons} from "storybook/manager-api";
 import wonderBlocksTheme from "./wonder-blocks-theme";
+import {themes} from "storybook/theming";
 
 /**
  * Configures a custom theme to add some minor WB branding to our Storybook
@@ -14,4 +15,13 @@ addons.setConfig({
     sidebar: {
         showRoots: true,
     },
+});
+
+// Listen to theme changes and update the Storybook UI theme dynamically
+addons.register("theme", (api) => {
+    api.on("globalsUpdated", ({globals}) => {
+        const {theme} = globals;
+        const storybookTheme = theme === "syl-dark" ? themes.dark : wonderBlocksTheme;
+        addons.setConfig({theme: storybookTheme});
+    });
 });
