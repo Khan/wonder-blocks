a446ec3423521bc27d86eec6787578d71b6a47c6
Bea Esguerra <beaesguerra@khanacademy.org>
2025-11-24 13:27:44 -0700

    [dark-tokens-with-tb-action-tokens] make ActivityIconButton have white text in syl dark, but instructive default in other themes

--- packages/wonder-blocks-icon-button/src/components/activity-icon-button.tsx
+++ packages/wonder-blocks-icon-button/src/components/activity-icon-button.tsx
@@ -15,6 +15,7 @@ import type {
 } from "../util/icon-button.types";
 
 import {IconButtonUnstyled} from "./icon-button-unstyled";
+import themeTokens from "../theme";
 
 type AriaLabelOnly = {
     /**
@@ -191,7 +192,7 @@ const theme = {
     },
     label: {
         color: {
-            progressive: semanticColor.core.foreground.instructive.default,
+            progressive: themeTokens.activityIconButton.label.color.progressive,
             neutral: semanticColor.core.foreground.neutral.default,
             disabled: semanticColor.core.foreground.disabled.default,
         },
@@ -335,6 +336,7 @@ const _generateStyles = (
             WebkitLineClamp: "2",
             overflow: "hidden",
             wordBreak: "break-word",
+            color: "inherit",
         },
     } as const;
 
