684802c96ac574acc7342b6ae20793442084f858
Marcy Sutton Todd <marcytodd@khanacademy.org>
2025-12-01 14:15:49 -0800

    [modal-docs] Update docs comment

--- __docs__/wonder-blocks-modal/modal-launcher.stories.tsx
+++ __docs__/wonder-blocks-modal/modal-launcher.stories.tsx
@@ -76,7 +76,7 @@ export default {
 
 Intended for use with \`OnePaneDialog\`, \`FlexibleDialog\`, or modal Building Blocks.
 
-For conditionally rendering modals, ensure there is only one \`DrawerLauncher\` in
+For conditionally rendering modals, ensure there is only one \`ModalLauncher\` in
 your component tree. A launcher needs to stay mounted on the current page to
 properly handle the user's keyboard focus on close of modals.
 Read [more details on Confluence](https://khanacademy.atlassian.net/wiki/spaces/FRONTEND/blog/2025/11/24/4454383789/Wonder+Blocks+Modal+Tips+Tricks).
