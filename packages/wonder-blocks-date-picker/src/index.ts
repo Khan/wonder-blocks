7c4fbe8631a03421a48f954c0bae4e7aee2a5a09
Marcy Sutton Todd <marcytodd@khanacademy.org>
2025-12-05 11:10:55 -0800

    [WB-1112] Simplify PR

--- packages/wonder-blocks-date-picker/src/index.ts
+++ packages/wonder-blocks-date-picker/src/index.ts
@@ -1,13 +1,5 @@
-import MaybeNativeDatePicker from "./components/maybe-native-date-picker";
-import DatePickerInput from "./components/date-picker-input";
 import DatePicker from "./components/date-picker";
 import {TemporalLocaleUtils} from "./util/temporal-locale-utils";
 import type {CustomModifiers} from "./util/types";
 
-export {
-    MaybeNativeDatePicker,
-    DatePickerInput,
-    DatePicker,
-    TemporalLocaleUtils,
-    type CustomModifiers,
-};
+export {DatePicker, TemporalLocaleUtils, type CustomModifiers};
