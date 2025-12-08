eff84b8d1e1367972276c0e1688665280d29b1de
Bea Esguerra <beaesguerra@khanacademy.org>
2025-12-08 16:35:52 -0700

    [responsive-tabs] implement styling for custom opener and current nav tab item

--- packages/wonder-blocks-tabs/src/components/navigation-tabs-dropdown.tsx
+++ packages/wonder-blocks-tabs/src/components/navigation-tabs-dropdown.tsx
@@ -1,5 +1,11 @@
 import * as React from "react";
 import {ActionItem, ActionMenu} from "@khanacademy/wonder-blocks-dropdown";
+import caretDown from "@phosphor-icons/core/bold/caret-down-bold.svg";
+import {PhosphorIcon} from "@khanacademy/wonder-blocks-icon";
+import {border, semanticColor, sizing} from "@khanacademy/wonder-blocks-tokens";
+import Button from "@khanacademy/wonder-blocks-button";
+import {StyleSheet} from "aphrodite";
+import okIcon from "@phosphor-icons/core/fill/check-circle-fill.svg";
 import {NavigationTabsProps} from "./types";
 
 function getLabelFromNavigationTabItem(navTabItem: React.ReactElement): string {
@@ -19,17 +25,48 @@ export const NavigationTabsDropdown = React.forwardRef(
         props: NavigationTabsProps,
         ref: React.ForwardedRef<HTMLElement>,
     ) {
-        const children = Array.isArray(props.children)
-            ? props.children
-            : [props.children];
+        const children = React.useMemo(
+            () =>
+                Array.isArray(props.children)
+                    ? props.children
+                    : [props.children],
+            [props.children],
+        );
+
+        const currentTabItemLabel = React.useMemo(() => {
+            const currentTabItem = children.find(
+                (navTabItem: React.ReactElement) => navTabItem.props.current,
+            );
+            return currentTabItem
+                ? getLabelFromNavigationTabItem(currentTabItem)
+                : "";
+        }, [children]);
+
         return (
-            <ActionMenu menuText="Navigation Tabs" {...props}>
+            <ActionMenu
+                menuText="Navigation Tabs"
+                {...props}
+                opener={() => (
+                    <Button
+                        kind="tertiary"
+                        endIcon={caretDown}
+                        style={styles.opener}
+                    >
+                        {currentTabItemLabel}
+                    </Button>
+                )}
+            >
                 {children.map((navTabItem: React.ReactElement) => {
                     return (
                         <ActionItem
                             label={getLabelFromNavigationTabItem(navTabItem)}
                             href={getHrefFromNavigationTabItem(navTabItem)}
                             active={navTabItem.props.current}
+                            rightAccessory={
+                                navTabItem.props.current ? (
+                                    <PhosphorIcon icon={okIcon} size="medium" />
+                                ) : undefined
+                            }
                         />
                     );
                 })}
@@ -37,3 +74,22 @@ export const NavigationTabsDropdown = React.forwardRef(
         );
     },
 );
+
+const styles = StyleSheet.create({
+    opener: {
+        position: "relative",
+        height: "unset",
+        paddingBlockStart: sizing.size_120,
+        paddingBlockEnd: sizing.size_140,
+        paddingInline: sizing.size_180,
+
+        ":after": {
+            borderBlockEnd: `${border.width.thin} solid ${semanticColor.core.border.neutral.subtle}`,
+            content: "''",
+            position: "absolute",
+            left: 0,
+            right: 0,
+            bottom: 0,
+        },
+    },
+});
