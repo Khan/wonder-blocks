607f4932d9a45023cef3559bad5791b1e9ca3855
Bea Esguerra <beaesguerra@khanacademy.org>
2025-12-10 16:53:46 -0700

    [tabs-dropdown] Add TabsDropdown component

--- packages/wonder-blocks-tabs/src/index.ts
+++ packages/wonder-blocks-tabs/src/index.ts
@@ -3,3 +3,4 @@ export {NavigationTabItem} from "./components/navigation-tab-item";
 export {Tabs} from "./components/tabs";
 export type {TabItem, TabRenderProps} from "./components/tabs";
 export {Tab} from "./components/tab";
+export {TabsDropdown} from "./components/tabs-dropdown";
