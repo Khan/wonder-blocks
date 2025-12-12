77af16013824e4b4bc193ba8a083887ff369d743
Bea Esguerra <beaesguerra@khanacademy.org>
2025-12-12 16:28:40 -0700

    [tabs-dropdown] Make TabsDropdown internal only

--- packages/wonder-blocks-tabs/src/index.ts
+++ packages/wonder-blocks-tabs/src/index.ts
@@ -3,4 +3,3 @@ export {NavigationTabItem} from "./components/navigation-tab-item";
 export {Tabs} from "./components/tabs";
 export type {TabItem, TabRenderProps} from "./components/tabs";
 export {Tab} from "./components/tab";
-export {TabsDropdown} from "./components/tabs-dropdown";
