59aba89b971b81239f42fe0c71be633b4891783b
Juan Andrade <juan@khanacademy.org>
2025-12-15 15:47:24 -0500

    [WB-2178.1] Add NodeIconButton component (#2897)
    ## Summary:
    
    Adds a new `NodeIconButton` component for learning path that follows the Chonky
    style and has a full radius.
    
    This component provides a new API for exposing component-level tokens to the
    component, so consumers can customize the component to their needs (if needed).
    
    ## Implementation
    
    1. #2897
    2. #2905
    
    Issue: WB-2178
    
    ## Test plan:
    
    Navigate to the `NodeIconButton` component in the Storybook and verify that it
    looks correct.
    
    `/?path=/docs/packages-iconbutton-nodeiconbutton--docs&globals=theme:thunderblocks`
    
    Also check the snapshots in Chromatic to verify that the component looks correct
    in all states.
    
    Author: jandrade
    
    Reviewers: beaesguerra, jandrade
    
    Required Reviewers:
    
    Approved By: beaesguerra
    
    Checks: ✅ 13 checks were successful, ⏭️  3 checks have been skipped
    
    Pull Request URL: https://github.com/Khan/wonder-blocks/pull/2897

---
+++ packages/wonder-blocks-icon-button/src/components/node-icon-button.tsx
@@ -0,0 +1,315 @@
+import * as React from "react";
+import {StyleSheet} from "aphrodite";
+import {Link} from "react-router-dom-v5-compat";
+
+import {StyleType, View} from "@khanacademy/wonder-blocks-core";
+import {PhosphorIcon} from "@khanacademy/wonder-blocks-icon";
+import {focusStyles} from "@khanacademy/wonder-blocks-styles";
+import {border, semanticColor, sizing} from "@khanacademy/wonder-blocks-tokens";
+
+import type {BaseIconButtonProps} from "../util/icon-button.types";
+
+import {IconButtonUnstyled} from "./icon-button-unstyled";
+
+/**
+ * The object containing the CSS variables that can be overridden to customize
+ * the appearance of the NodeIconButton component.
+ */
+type Tokens = Partial<{
+    "--wb-c-node-icon-button--box-foreground": string;
+    "--wb-c-node-icon-button--box-background": string;
+    "--wb-c-node-icon-button--box-shadow-color": string;
+    "--wb-c-node-icon-button--box-padding": string | number;
+    "--wb-c-node-icon-button--box-shadow-y-rest": string | number;
+    "--wb-c-node-icon-button--box-shadow-y-hover": string | number;
+    "--wb-c-node-icon-button--box-shadow-y-press": string | number;
+    "--wb-c-node-icon-button--icon-size": string | number;
+}>;
+
+type Props = Omit<BaseIconButtonProps, "kind" | "style"> & {
+    /**
+     * The action type of the button. This determines the visual style of
+     * the button. Defaults to `notStarted`.
+     *
+     * - `notStarted` is used for buttons that indicate a not started action.
+     * - `attempted` is used for buttons that indicate an attempted (in progress)
+     *   action.
+     * - `complete` is used for buttons that indicate a complete action.
+     */
+    actionType?: "notStarted" | "attempted" | "complete";
+    /**
+     * The alternative text for the icon button. Required for accessibility.
+     */
+    "aria-label": string;
+
+    /**
+     * The size of the icon button.
+     * One of `small` (24) or `large` (68).
+     * Defaults to `large`.
+     */
+    size?: "small" | "large";
+
+    /**
+     * Custom styles for the elements in the NodeIconButton component.
+     * - `root`: Styles the root element (button)
+     * - `box`: Styles the "chonky" box element
+     * - `icon`: Styles the icon element
+     */
+    styles?: {
+        root?: StyleType;
+        box?: StyleType;
+        icon?: StyleType;
+    };
+};
+
+/**
+ * Node buttons are visual representations of activities along in a Learning
+ * Path. When a represented No  de is a button that launches the activity. Nodes
+ * use the Chonky shadow style.
+ *
+ * ```tsx
+ * import pencilSimpleIcon from "@phosphor-icons/core/regular/pencil-simple.svg";
+ * import {NodeIconButton} from "@khanacademy/wonder-blocks-icon-button";
+ *
+ * <NodeIconButton
+ *     icon={pencilSimpleIcon}
+ *     aria-label="Edit"
+ *     onClick={(e) => console.log("Hello, world!")}
+ * />
+ * ```
+ */
+export const NodeIconButton: React.ForwardRefExoticComponent<
+    Props &
+        React.RefAttributes<typeof Link | HTMLButtonElement | HTMLAnchorElement>
+> = React.forwardRef<
+    typeof Link | HTMLButtonElement | HTMLAnchorElement,
+    Props
+>(function NodeIconButton(props: Props, ref) {
+    const {
+        actionType = "notStarted",
+        disabled = false,
+        icon,
+        size = "large",
+        styles: stylesProp,
+        type = "button",
+        // labeling
+        "aria-label": ariaLabel,
+        ...restProps
+    } = props;
+
+    const [pressed, setPressed] = React.useState(false);
+
+    const buttonStyles = [
+        styles.button,
+        disabled && styles.disabled,
+        !disabled && pressed && styles.pressed,
+        variants.size[size] as StyleType,
+        variants.actionType[actionType] as StyleType,
+        stylesProp?.root,
+    ];
+
+    const chonkyStyles = [
+        styles.chonky,
+        !disabled && pressed && styles.chonkyPressed,
+        stylesProp?.box,
+        disabled && styles.chonkyDisabled,
+    ];
+
+    const handlePress = React.useCallback((isPressing: boolean) => {
+        setPressed(isPressing);
+    }, []);
+
+    const iconElement = React.useMemo(() => {
+        if (typeof icon === "string") {
+            return (
+                <PhosphorIcon
+                    style={[styles.icon, stylesProp?.icon]}
+                    color="currentColor"
+                    icon={icon}
+                />
+            );
+        }
+
+        return React.cloneElement(icon, {
+            style: [styles.icon, stylesProp?.icon],
+        });
+    }, [icon, stylesProp?.icon]);
+
+    return (
+        <IconButtonUnstyled
+            {...restProps}
+            disabled={disabled}
+            onPress={handlePress}
+            ref={ref}
+            style={buttonStyles}
+            type={type}
+            aria-label={ariaLabel}
+        >
+            <>
+                {/* NOTE: Using a regular className to be able to use descendant selectors to account for the hover and press states */}
+                <View style={chonkyStyles} className="chonky">
+                    {iconElement}
+                </View>
+            </>
+        </IconButtonUnstyled>
+    );
+});
+
+const variants: {
+    size: Record<string, Tokens>;
+    actionType: Record<string, Tokens>;
+} = {
+    size: {
+        // Default size.
+        large: {
+            "--wb-c-node-icon-button--icon-size": sizing.size_480,
+            "--wb-c-node-icon-button--box-padding": sizing.size_100,
+            // NOTE: We use px units to prevent a bug in Safari where the shadow
+            // animation flickers when using rem units.
+            "--wb-c-node-icon-button--box-shadow-y-rest": "6px",
+            "--wb-c-node-icon-button--box-shadow-y-hover": "8px",
+            "--wb-c-node-icon-button--box-shadow-y-press": sizing.size_0,
+        },
+        small: {
+            "--wb-c-node-icon-button--icon-size": sizing.size_240,
+            "--wb-c-node-icon-button--box-padding": sizing.size_0,
+            "--wb-c-node-icon-button--box-shadow-y-rest": "2px",
+            "--wb-c-node-icon-button--box-shadow-y-hover": "4px",
+            "--wb-c-node-icon-button--box-shadow-y-press": sizing.size_0,
+        },
+    },
+    actionType: {
+        // Default action type.
+        notStarted: {
+            "--wb-c-node-icon-button--box-foreground":
+                semanticColor.learning.foreground.progress.notStarted.strong,
+            "--wb-c-node-icon-button--box-background":
+                semanticColor.learning.background.progress.notStarted.default,
+            "--wb-c-node-icon-button--box-shadow-color":
+                semanticColor.learning.shadow.progress.notStarted.default,
+        },
+        attempted: {
+            "--wb-c-node-icon-button--box-foreground":
+                semanticColor.learning.foreground.progress.attempted.strong,
+            "--wb-c-node-icon-button--box-background":
+                semanticColor.learning.background.progress.attempted.default,
+            "--wb-c-node-icon-button--box-shadow-color":
+                semanticColor.learning.shadow.progress.attempted.default,
+        },
+        complete: {
+            "--wb-c-node-icon-button--box-foreground":
+                semanticColor.learning.foreground.progress.complete.strong,
+            "--wb-c-node-icon-button--box-background":
+                semanticColor.learning.background.progress.complete.default,
+            "--wb-c-node-icon-button--box-shadow-color":
+                semanticColor.learning.shadow.progress.complete.default,
+        },
+    },
+};
+
+const disabledStatesStyles = {
+    outline: "none",
+    transform: "none",
+};
+const chonkyDisabled = {
+    background: semanticColor.chonky.disabled.background.primary,
+    color: semanticColor.chonky.disabled.foreground.primary,
+    boxShadow: `0 var(--wb-c-node-icon-button--box-shadow-y-rest) 0 0 ${semanticColor.chonky.disabled.shadow.primary}`,
+    transform: "none",
+};
+
+const chonkyPressed = {
+    boxShadow: `0 var(--wb-c-node-icon-button--box-shadow-y-press) 0 0 var(--wb-c-node-icon-button--box-shadow-color)`,
+    transform: `translateY(var(--wb-c-node-icon-button--box-shadow-y-rest))`,
+};
+
+const styles = StyleSheet.create({
+    /**
+     * Button Styles (root)
+     */
+    button: {
+        // theming
+        borderRadius: border.radius.radius_full,
+        // layout
+        flexDirection: "column",
+        alignSelf: "flex-start",
+        justifySelf: "center",
+        gap: sizing.size_020,
+
+        /**
+         * States
+         *
+         * Defined in the following order: hover, active, focus.
+         *
+         * This is important as we want to give more priority to the
+         * :focus-visible styles.
+         */
+        [":is(:hover) .chonky" as any]: {
+            boxShadow: `0 var(--wb-c-node-icon-button--box-shadow-y-hover) 0 0 var(--wb-c-node-icon-button--box-shadow-color)`,
+            transform: `translateY(calc((var(--wb-c-node-icon-button--box-shadow-y-hover) - var(--wb-c-node-icon-button--box-shadow-y-rest)) * -1))`,
+        },
+
+        [":is(:active) .chonky" as any]: chonkyPressed,
+
+        // :focus-visible -> Provide focus styles for keyboard users only.
+        ...focusStyles.focus,
+    },
+    disabled: {
+        cursor: "not-allowed",
+        ...disabledStatesStyles,
+        // Reset hover and active styles on disabled buttons.
+        ":hover": disabledStatesStyles,
+        ":active": disabledStatesStyles,
+        ":focus-visible": {
+            transform: "none",
+        },
+        // Reset hover and active styles on the chonky element.
+        [":is(:hover) .chonky" as any]: {
+            ...chonkyDisabled,
+            ...disabledStatesStyles,
+        },
+        // [":is(:active) .chonky" as any]: chonkyDisabled,
+    },
+    // Enable keyboard support for press styles.
+    pressed: {
+        [".chonky" as any]: chonkyPressed,
+    },
+
+    /**
+     * Chonky Styles (box)
+     */
+    chonky: {
+        // layout
+        borderRadius: border.radius.radius_full,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: "var(--wb-c-node-icon-button--box-padding)",
+        width: "100%",
+        height: "100%",
+        // theming
+        background: "var(--wb-c-node-icon-button--box-background)",
+        color: "var(--wb-c-node-icon-button--box-foreground)",
+        // Gives the button a "chonky" look and feel.
+        marginBlockEnd: "var(--wb-c-node-icon-button--box-shadow-y-rest)",
+        boxShadow: `0 var(--wb-c-node-icon-button--box-shadow-y-rest) 0 0 var(--wb-c-node-icon-button--box-shadow-color)`,
+        // motion
+        transition: "0.12s ease-out",
+        // NOTE: We only want to transition the properties that are being
+        // animated.
+        transitionProperty: "box-shadow, transform, margin-block-end",
+
+        ["@media not (hover: hover)" as any]: {
+            transition: "none",
+        },
+    },
+    chonkyPressed,
+    chonkyDisabled,
+
+    /**
+     * Icon Styles (icon)
+     */
+    icon: {
+        inlineSize: "var(--wb-c-node-icon-button--icon-size)",
+        blockSize: "var(--wb-c-node-icon-button--icon-size)",
+    },
+});
