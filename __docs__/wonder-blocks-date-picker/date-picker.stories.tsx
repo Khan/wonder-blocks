89b88723f9c8c8b377d8159b8ce2cd42c3ebad56
Marcy Sutton Todd <marcytodd@khanacademy.org>
2025-12-17 16:56:29 -0800

    [FEI-7342-locales] Put date in the past for determinism

--- __docs__/wonder-blocks-date-picker/date-picker.stories.tsx
+++ __docs__/wonder-blocks-date-picker/date-picker.stories.tsx
@@ -296,8 +296,8 @@ const DatePickerWithOpenOverlay = (props: Props) => {
 export const OpenCalendarOverlay: Story = {
     render: (args) => <DatePickerWithOpenOverlay {...args} />,
     args: {
-        selectedDate: Temporal.PlainDate.from("2025-12-01"),
-        minDate: Temporal.PlainDate.from("2025-12-01"),
+        selectedDate: Temporal.PlainDate.from("2025-11-01"),
+        minDate: Temporal.PlainDate.from("2025-11-01"),
         maxDate: Temporal.PlainDate.from("2026-12-31"),
         updateDate: () => {},
     },
