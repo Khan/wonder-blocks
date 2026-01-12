987c6791728aca2dbd4b9f532ddb3b27d9abcbd7
Juan Andrade <juan@khanacademy.org@khanacademy.org>
2026-01-12 11:12:12 -0500

    [ab-actiontype] Address feedback

--- packages/wonder-blocks-button/src/components/activity-button.tsx
+++ packages/wonder-blocks-button/src/components/activity-button.tsx
@@ -46,7 +46,6 @@ const ActivityButtonCore: React.ForwardRefExoticComponent<
         ...restProps
     } = props;
 
-    // NOTE: `progressive` is the only action type supported by this component.
     const buttonStyles = _generateStyles(actionType, disabled, kind);
 
     const sharedStyles = [
@@ -181,7 +180,8 @@ export const ActivityButton = React.forwardRef(function ActivityButton(
     const extraClickableProps = beforeNav ? {beforeNav} : {target};
     // Invoke link or button clickable behavior
     const roleForEvents = href ? "link" : "button";
-    // prevent redundant roles for links and buttons, while allowing other roles like `tab` or `menuitem`
+    // prevent redundant roles for links and buttons, while allowing other roles
+    // like `tab` or `menuitem`
     const renderedRole =
         role === "link" || role === "button" ? undefined : role;
 
