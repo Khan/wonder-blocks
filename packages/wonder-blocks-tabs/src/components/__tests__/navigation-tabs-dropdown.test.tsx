273542d3b989c56bd3c7bbaf212447bd8b6dea39
Bea Esguerra <beaesguerra@khanacademy.org>
2026-01-16 15:26:16 -0700

    [navigation-tabs-dropdown] Set up base functionality of ResponsiveTabsDropdown

--- packages/wonder-blocks-tabs/src/components/__tests__/navigation-tabs-dropdown.test.tsx
+++ packages/wonder-blocks-tabs/src/components/__tests__/navigation-tabs-dropdown.test.tsx
@@ -1,1 +1,90 @@
-describe("NavigationTabsDropdown", () => {});
+import * as React from "react";
+import {render, screen} from "@testing-library/react";
+import {userEvent} from "@testing-library/user-event";
+import {NavigationTabsDropdown} from "../navigation-tabs-dropdown";
+
+jest.mock("react-popper", () => ({
+    ...jest.requireActual("react-popper"),
+    Popper: jest.fn().mockImplementation(({children}) => {
+        // Mock `isReferenceHidden` to always return false (or true for testing visibility)
+        return children({
+            ref: jest.fn(),
+            style: {},
+            placement: "bottom",
+            isReferenceHidden: false, // Mocking isReferenceHidden
+        });
+    }),
+}));
+
+const tabs = [
+    {
+        id: "tab-1",
+        label: "Tab 1",
+        href: "/tab-1",
+    },
+    {
+        id: "tab-2",
+        label: "Tab 2",
+        href: "/tab-2",
+    },
+    {
+        id: "tab-3",
+        label: "Tab 3",
+        href: "/tab-3",
+    },
+];
+
+describe("NavigationTabsDropdown", () => {
+    describe("Props", () => {
+        describe("tabs prop", () => {
+            it("should render the tabs when the dropdown is opened", async () => {
+                // Arrange
+                render(<NavigationTabsDropdown tabs={tabs} />);
+
+                // Act
+                await userEvent.click(screen.getByRole("button"));
+
+                // Assert
+                expect(
+                    screen.getByRole("menuitem", {name: "Tab 1"}),
+                ).toBeInTheDocument();
+                expect(
+                    screen.getByRole("menuitem", {name: "Tab 2"}),
+                ).toBeInTheDocument();
+                expect(
+                    screen.getByRole("menuitem", {name: "Tab 3"}),
+                ).toBeInTheDocument();
+            });
+
+            it("should render nothing when there are no tabs", () => {
+                // Arrange
+                // Act
+                const {container} = render(
+                    <NavigationTabsDropdown tabs={[]} />,
+                );
+
+                // Assert
+                expect(container).toBeEmptyDOMElement();
+            });
+
+            it("should render tabs with correct href attributes", async () => {
+                // Arrange
+                render(<NavigationTabsDropdown tabs={tabs} />);
+
+                // Act
+                await userEvent.click(screen.getByRole("button"));
+
+                // Assert
+                expect(
+                    screen.getByRole("menuitem", {name: "Tab 1"}),
+                ).toHaveAttribute("href", "/tab-1");
+                expect(
+                    screen.getByRole("menuitem", {name: "Tab 2"}),
+                ).toHaveAttribute("href", "/tab-2");
+                expect(
+                    screen.getByRole("menuitem", {name: "Tab 3"}),
+                ).toHaveAttribute("href", "/tab-3");
+            });
+        });
+    });
+});
--- packages/wonder-blocks-tabs/src/components/navigation-tabs-dropdown.tsx
+++ packages/wonder-blocks-tabs/src/components/navigation-tabs-dropdown.tsx
@@ -1,8 +1,92 @@
 import * as React from "react";
+import {ActionItem, ActionMenu} from "@khanacademy/wonder-blocks-dropdown";
+import Button from "@khanacademy/wonder-blocks-button";
+import caretDown from "@phosphor-icons/core/bold/caret-down-bold.svg";
+import {StyleSheet} from "aphrodite";
+import {border, semanticColor, sizing} from "@khanacademy/wonder-blocks-tokens";
+import {View} from "@khanacademy/wonder-blocks-core";
+
+type NavigationTabDropdownItem = {
+    /**
+     * The label of the navigation tab
+     */
+    label: string;
+    /**
+     * The id of the navigation tab
+     */
+    id: string;
+    /**
+     * The URL to navigate to
+     */
+    href: string;
+};
+
+type NavigationTabsDropdownProps = {
+    /**
+     * The navigation tabs to render in the dropdown.
+     */
+    tabs: Array<NavigationTabDropdownItem>;
+};
 
 /**
- * A component that renders a dropdown of navigation tabs.
+ * The NavigationTabsDropdown component is used to represent navigation tabs
+ * in an ActionMenu when there is not enough horizontal space to render the
+ * tabs as a horizontal layout. Unlike TabsDropdown, this component uses links
+ * for navigation instead of managing tab panels.
  */
-export const NavigationTabsDropdown = () => {
-    return <div>NavigationTabsDropdown</div>;
+export const NavigationTabsDropdown = (props: NavigationTabsDropdownProps) => {
+    const {tabs} = props;
+
+    if (tabs.length === 0) {
+        return <React.Fragment />;
+    }
+
+    // Use first tab label as default opener text
+    // TODO: update default menuText
+    const menuText = tabs[0]?.label || "Tabs";
+
+    return (
+        <View>
+            <ActionMenu
+                menuText={menuText}
+                opener={() => (
+                    <Button
+                        kind="tertiary"
+                        endIcon={caretDown}
+                        style={styles.opener}
+                    >
+                        {menuText}
+                    </Button>
+                )}
+                style={styles.actionMenu}
+            >
+                {tabs.map((tab: NavigationTabDropdownItem) => {
+                    return (
+                        <ActionItem
+                            key={tab.id}
+                            label={tab.label}
+                            href={tab.href}
+                        />
+                    );
+                })}
+            </ActionMenu>
+        </View>
+    );
 };
+
+const styles = StyleSheet.create({
+    actionMenu: {
+        width: "100%",
+        alignItems: "flex-start",
+        borderBlockEnd: `${border.width.thin} solid ${semanticColor.core.border.neutral.subtle}`,
+    },
+    opener: {
+        position: "relative",
+        height: "unset",
+        paddingBlockStart: sizing.size_120,
+        paddingBlockEnd: sizing.size_140,
+        paddingInline: sizing.size_180,
+        width: "100%",
+        justifyContent: "space-between",
+    },
+});
