b2630f6571b1bc5125653e9807fc2178712db96d
Juan Andrade <juan@khanacademy.org@khanacademy.org>
2026-01-20 11:23:49 -0500

    [floating-popover-2] Fix types

--- packages/wonder-blocks-popover/src/index.ts
+++ packages/wonder-blocks-popover/src/index.ts
@@ -1,8 +1,5 @@
 import Popover from "./components/popover";
-import type {Placement} from "./util/types";
 import PopoverContent from "./components/popover-content";
 import PopoverContentCore from "./components/popover-content-core";
 
 export {Popover, PopoverContent, PopoverContentCore};
-
-export type {Placement};
