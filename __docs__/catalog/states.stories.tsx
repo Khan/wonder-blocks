6cdb0460236833e0fce4c5f6b2ff411123e2c177
Bea Esguerra <beaesguerra@khanacademy.org>
2026-02-06 17:43:04 -0700

    [all-components-stories] set up states stories only for interactive components

--- __docs__/catalog/states.stories.tsx
+++ __docs__/catalog/states.stories.tsx
@@ -1,36 +1,57 @@
-import {AllComponents} from "./catalog.stories";
+import {ComponentList} from "./catalog-component-info";
+import {
+    buttonComponents,
+    feedbackComponents,
+    inputComponents,
+    layoutBlocksComponents,
+    navigationAndMenuComponents,
+} from "./components-config";
 
 export default {
     title: "Catalog / States",
     tags: ["!autodocs"],
 };
 
-export const AllComponentsHover = {
-    name: "Components / Hover",
-    render: AllComponents.render,
+const interactiveComponents = [
+    ...buttonComponents,
+    ...inputComponents,
+    ...feedbackComponents,
+    ...navigationAndMenuComponents,
+    ...layoutBlocksComponents,
+];
+
+export const Hover = {
+    render: ComponentList,
     parameters: {
         pseudo: {
             hover: true,
         },
     },
+    args: {
+        components: interactiveComponents,
+    },
 };
 
-export const AllComponentsFocus = {
-    name: "Components / Focus",
-    render: AllComponents.render,
+export const Focus = {
+    render: ComponentList,
     parameters: {
         pseudo: {
             focusVisible: true,
         },
     },
+    args: {
+        components: interactiveComponents,
+    },
 };
 
-export const AllComponentsPress = {
-    name: "Components / Press",
-    render: AllComponents.render,
+export const Press = {
+    render: ComponentList,
     parameters: {
         pseudo: {
             active: true,
         },
+    },
+    args: {
+        components: interactiveComponents,
     },
 };
