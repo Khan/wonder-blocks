fbb40e702d278a89f257d8bf03003a7404e77e0a
Bea Esguerra <beaesguerra@khanacademy.org>
2026-02-23 16:16:52 -0700

    [wb-syl-dark-tokens] set up sb modes for syl-dark

--- .storybook/modes.ts
+++ .storybook/modes.ts
@@ -19,6 +19,9 @@ export const allModes = {
     themeThunderBlocks: {
         theme: "thunderblocks",
     },
+    themeSylDark: {
+        theme: "syl-dark",
+    },
     // NOTE: This will go away when we fully remove the light variants.
     dark: {
         background: "neutralStrong",
@@ -30,7 +33,13 @@ export const allModes = {
     },
 };
 
+// TODO: Remove in favour of allThemeModesonce fully implemnting sylDark
 export const themeModes = {
     default: allModes.themeDefault,
     thunderblocks: allModes.themeThunderBlocks,
+};
+
+export const allThemeModes = {
+    ...themeModes,
+    sylDark: allModes.themeSylDark,
 };
