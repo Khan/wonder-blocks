e4dfea2ca82804bf9c0b4406255bdd60376347cc
Marcy Sutton Todd <marcytodd@khanacademy.org>
2026-03-12 14:05:31 -0700

    [WB-2019-announcer] Improve modal support in stories

--- packages/wonder-blocks-announcer/src/index.ts
+++ packages/wonder-blocks-announcer/src/index.ts
@@ -1,7 +1,10 @@
 import {initAnnouncer} from "./init-announcer";
 import {announceMessage} from "./announce-message";
 import {clearMessages} from "./clear-messages";
-import {attachAnnouncerToModal, detachAnnouncerFromModal} from "./modal-announcer";
+import {
+    attachAnnouncerToModal,
+    detachAnnouncerFromModal,
+} from "./modal-announcer";
 import type {AnnounceMessageProps} from "./announce-message";
 import type {PolitenessLevel} from "./util/announcer.types";
 
