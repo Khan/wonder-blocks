466ea96caaf276d6e49c9d32fd6bcc54f2dcf939
Marcy Sutton Todd <marcytodd@khanacademy.org>
2026-04-03 15:40:35 -0700

    [WB-2247-bodytext-lint] Make package adjustments for rule dev

--- .eslintrc.js
+++ .eslintrc.js
@@ -48,12 +48,6 @@ module.exports = {
             },
         },
         {
-            files: ["bodytext-tag-eslint-rule/**/*.js"],
-            rules: {
-                "import/no-commonjs": "off",
-            },
-        },
-        {
             files: ["**/*.test.*"],
             rules: {
                 "max-lines": "off",
