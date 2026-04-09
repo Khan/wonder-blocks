dba1c1bf554f006d6be3243b11e779e4c2a5edef
Bea Esguerra <beaesguerra@khanacademy.org>
2026-04-09 15:28:01 -0600

    [add-docs-for-lint-rules-in-sb] specify order of root categories

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -118,11 +118,8 @@ const parameters: Preview["parameters"] = {
     // https://storybook.js.org/docs/react/configure/story-layout
     layout: "padded",
     options: {
-        // Allowing stories to be sorted alphabetically. This means that we will
-        // display the stories (or examples first), then we will display all the
-        // mdx pages under __docs__.
         storySort: {
-            order: ["Components", "**/__docs__/**", "Overview"],
+            order: ["Foundations", "Packages", "Tools", "Catalog", "*"],
         },
     },
     docs: {
