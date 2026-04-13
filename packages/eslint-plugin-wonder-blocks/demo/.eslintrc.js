107d8a02344626189706d4692b2da1b86c836ce0
Bea Esguerra <beaesguerra@khanacademy.org>
2026-04-13 10:21:51 -0600

    [lint-config] no need to define the plugin if we are extending the config from the plugin

--- packages/eslint-plugin-wonder-blocks/demo/.eslintrc.js
+++ packages/eslint-plugin-wonder-blocks/demo/.eslintrc.js
@@ -1,6 +1,5 @@
 module.exports = {
     root: true,
-    plugins: ["@khanacademy/wonder-blocks"],
     parser: "@typescript-eslint/parser",
     extends: ["plugin:@khanacademy/wonder-blocks/strict"],
 };
