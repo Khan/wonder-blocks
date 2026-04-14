4e6cc2142785c9f57db7931c5b03e1dd8c010304
Ben Christel <ben.christel@gmail.com>
2026-04-14 13:10:45 -0700

    Migrate ClickableBehavior typetests

--- packages/wonder-blocks-clickable/src/components/__tests__/clickable-behavior.typestest.tsx
+++ packages/wonder-blocks-clickable/src/components/__tests__/clickable-behavior.typestest.tsx
@@ -1,20 +1,31 @@
 import * as React from "react";
+import {describe, it} from "tstyche";
 
 import ClickableBehavior from "../clickable-behavior";
 
-<ClickableBehavior>
-    {(_, childrenProps) => <div {...childrenProps} />}
-</ClickableBehavior>;
+describe("ClickableBehavior", () => {
+    it("should be usable with no props", () => {
+        <ClickableBehavior>
+            {(_, childrenProps) => <div {...childrenProps} />}
+        </ClickableBehavior>;
+    });
 
-<ClickableBehavior target="_blank">
-    {(_, childrenProps) => <div {...childrenProps} />}
-</ClickableBehavior>;
+    it("should accept target prop", () => {
+        <ClickableBehavior target="_blank">
+            {(_, childrenProps) => <div {...childrenProps} />}
+        </ClickableBehavior>;
+    });
 
-<ClickableBehavior beforeNav={() => Promise.resolve()}>
-    {(_, childrenProps) => <div {...childrenProps} />}
-</ClickableBehavior>;
+    it("should accept beforeNav prop", () => {
+        <ClickableBehavior beforeNav={() => Promise.resolve()}>
+            {(_, childrenProps) => <div {...childrenProps} />}
+        </ClickableBehavior>;
+    });
 
-// @ts-expect-error Types of property 'beforeNav' are incompatible
-<ClickableBehavior target="_blank" beforeNav={() => Promise.resolve()}>
-    {(_, childrenProps) => <div {...childrenProps} />}
-</ClickableBehavior>;
+    it("should reject combining target and beforeNav props", () => {
+        // @ts-expect-error Types of property 'beforeNav' are incompatible
+        <ClickableBehavior target="_blank" beforeNav={() => Promise.resolve()}>
+            {(_, childrenProps) => <div {...childrenProps} />}
+        </ClickableBehavior>;
+    });
+});
