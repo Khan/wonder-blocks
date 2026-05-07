c88e0be5fe8a1df36dd3b2b31fb9e5f67c2c07a2
Marcy Sutton Todd <marcytodd@khanacademy.org>
2026-05-07 13:56:27 -0700

    [WB-2248-bodytext-children] Change rule to recommended

--- packages/eslint-plugin-wonder-blocks/src/configs/strict.ts
+++ packages/eslint-plugin-wonder-blocks/src/configs/strict.ts
@@ -5,7 +5,6 @@ export default {
     rules: {
         ...recommended.rules,
         "@khanacademy/wonder-blocks/no-custom-tab-role": "error",
-        "@khanacademy/wonder-blocks/no-invalid-bodytext-children": "error",
         "@khanacademy/wonder-blocks/no-invalid-bodytext-parent": "error",
     },
 };
