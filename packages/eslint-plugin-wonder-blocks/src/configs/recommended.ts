8f75780e3750db42ab5eb7019dda4463438ef88b
Maddy Andrade <maddy@khanacademy.org@khanacademy.org>
2026-05-11 13:54:41 -0400

    [maddy/rtl-rule-in-plugin] Add docs, demo, and Storybook story

--- packages/eslint-plugin-wonder-blocks/src/configs/recommended.ts
+++ packages/eslint-plugin-wonder-blocks/src/configs/recommended.ts
@@ -2,5 +2,7 @@ export default {
     plugins: ["@khanacademy/wonder-blocks"],
     rules: {
         "@khanacademy/wonder-blocks/no-invalid-bodytext-children": "error",
+        "@khanacademy/wonder-blocks/require-logical-properties-for-rtl":
+            "error",
     },
 };
