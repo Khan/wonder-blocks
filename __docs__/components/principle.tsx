de084d0f8a00a9543d17fe5d9055c1d7d3b008ac
Jonah Goldsaito <jazgold@gmail.com>
2026-06-12 16:33:16 -0400

    Principles updates

---
+++ __docs__/components/principle.tsx
@@ -0,0 +1,74 @@
+import * as React from "react";
+import {StyleSheet} from "aphrodite";
+import {addStyle, View} from "@khanacademy/wonder-blocks-core";
+import {BodyText, Heading} from "@khanacademy/wonder-blocks-typography";
+import {border, semanticColor, sizing} from "@khanacademy/wonder-blocks-tokens";
+
+type PrincipleSetProps = {
+    children: React.ReactNode;
+};
+
+const StyledDiv = addStyle("div");
+
+export function PrincipleSet({
+    children,
+}: PrincipleSetProps): React.ReactElement {
+    return <StyledDiv style={styles.set}>{children}</StyledDiv>;
+}
+
+type Props = {
+    label: string;
+    title: string;
+    description: string;
+};
+
+export default function Principle({
+    label,
+    title,
+    description,
+}: Props): React.ReactElement {
+    return (
+        <View style={styles.container}>
+            <BodyText
+                size="xsmall"
+                weight="bold"
+                tag="span"
+                style={styles.label}
+            >
+                {label}
+            </BodyText>
+            <Heading size="medium" style={styles.title}>
+                {title}
+            </Heading>
+            <BodyText tag="p" style={styles.description}>
+                {description}
+            </BodyText>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    set: {
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fill, minmax(min(100%, 300px), 1fr))",
+        gap: sizing.size_160,
+        padding: sizing.size_160,
+    },
+    container: {
+        backgroundColor: semanticColor.core.background.base.default,
+        border: `${border.width.thin} solid ${semanticColor.core.border.neutral.subtle}`,
+        borderRadius: border.radius.radius_040,
+        paddingInline: sizing.size_400,
+        paddingBlock: sizing.size_560,
+        gap: sizing.size_080,
+    },
+    label: {
+        color: semanticColor.core.foreground.neutral.strong,
+    },
+    title: {
+        color: semanticColor.core.foreground.neutral.strong,
+    },
+    description: {
+        color: semanticColor.core.foreground.neutral.default,
+    },
+});
