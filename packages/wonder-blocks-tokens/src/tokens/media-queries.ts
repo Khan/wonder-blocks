92709c2fce99b7fca2b4f9f8ffc1b22a994e84ce
Marcy Sutton Todd <marcytodd@khanacademy.org>
2026-06-24 10:37:47 -0700

    [media-query-revisit] Use correct values in media query tokens

--- packages/wonder-blocks-tokens/src/tokens/media-queries.ts
+++ packages/wonder-blocks-tokens/src/tokens/media-queries.ts
@@ -13,6 +13,8 @@ const width = {
     mdMin: 682,
     mdMax: 1023,
     lgMin: 1024,
+    lgMax: 1199,
+    xlMin: 1200,
 } as const;
 
 /* Named mediaQuery conditions */
@@ -21,17 +23,18 @@ const mediaQuery = {
     xs: `@media screen and (max-width: ${width.xsMax}px) /* breakpoint.mediaQuery.xs */`,
     sm: `@media screen and (min-width: ${width.smMin}px) and (max-width: ${width.smMax}px) /* breakpoint.mediaQuery.sm */`,
     md: `@media screen and (min-width: ${width.mdMin}px) and (max-width: ${width.mdMax}px) /* breakpoint.mediaQuery.md */`,
-    lg: `@media screen and (min-width: ${width.mdMin}px) and (max-width: ${width.lgMin}px) /* breakpoint.mediaQuery.lg */`,
-    xl: `@media screen and (min-width: ${width.lgMin}px) /* breakpoint.mediaQuery.xl */`,
+    lg: `@media screen and (min-width: ${width.lgMin}px) and (max-width: ${width.lgMax}px) /* breakpoint.mediaQuery.lg */`,
+    xl: `@media screen and (min-width: ${width.xlMin}px) /* breakpoint.mediaQuery.xl */`,
 
     xsOrSmaller: `@media screen and (max-width: ${width.xsMax}px) /* breakpoint.mediaQuery.xsOrSmaller */`,
     smOrSmaller: `@media screen and (max-width: ${width.smMax}px) /* breakpoint.mediaQuery.smOrSmaller */`,
     mdOrSmaller: `@media screen and (max-width: ${width.mdMax}px) /* breakpoint.mediaQuery.mdOrSmaller */`,
-    lgOrSmaller: `@media screen and (max-width: ${width.lgMin}px) /* breakpoint.mediaQuery.lgOrSmaller */`,
+    lgOrSmaller: `@media screen and (max-width: ${width.lgMax}px) /* breakpoint.mediaQuery.lgOrSmaller */`,
 
     smOrLarger: `@media screen and (min-width: ${width.smMin}px) /* breakpoint.mediaQuery.smOrLarger */`,
     mdOrLarger: `@media screen and (min-width: ${width.mdMin}px) /* breakpoint.mediaQuery.mdOrLarger */`,
     lgOrLarger: `@media screen and (min-width: ${width.lgMin}px) /* breakpoint.mediaQuery.lgOrLarger */`,
+    xlOrLarger: `@media screen and (min-width: ${width.xlMin}px) /* breakpoint.mediaQuery.xlOrLarger */`,
 } as const;
 
 export const breakpoint = {
